0feefd1e2de0aafae42818abe9c0718ad22b9292
zextras-vijay <vijay.godhasara@zextras.com>
2022-05-13 11:34:12 +0530

    fix: add sidebar new design

--- src/views/app-view.tsx
+++ src/views/app-view.tsx
@@ -7,20 +7,19 @@ import React, { FC, Suspense } from 'react';
 import { Container, Text } from '@zextras/carbonio-design-system';
 import { Spinner } from '@zextras/carbonio-shell-ui';
 import { useRouteMatch, Switch, Route } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import DomainListPanel from './domain/domain-list-panel';
 import DomainDetailPanel from './domain/domain-detail-panel';
 import {
-	CERTIFICATES_ROUTE_ID,
-	CORE_ROUTE_ID,
-	COS_ROUTE_ID,
+	BACKUP_ROUTE_ID,
 	DOMAINS_ROUTE_ID,
-	FEATURES_ROUTE_ID,
-	SERVER_AND_VOLUMES_ROUTE_ID
+	STORAGES_ROUTE_ID,
+	SUBSCRIPTIONS_ROUTE_ID
 } from '../constants';
 
 const AppView: FC = () => {
 	const { path } = useRouteMatch();
-
+	const [t] = useTranslation();
 	return (
 		<Switch>
 			<Route path={`${path}/${DOMAINS_ROUTE_ID}`}>
@@ -35,50 +34,30 @@ const AppView: FC = () => {
 					</Suspense>
 				</Container>
 			</Route>
-			<Route path={`${path}/${SERVER_AND_VOLUMES_ROUTE_ID}`}>
+			<Route path={`${path}/${STORAGES_ROUTE_ID}`}>
 				<Container orientation="horizontal" mainAlignment="flex-start">
 					<Container width="40%">
-						<Text>Server and Volumes</Text>
+						<Text>{t('label.storages', 'Storages')}</Text>
 					</Container>
 					<Suspense fallback={<Spinner />}>
 						<DomainDetailPanel />
 					</Suspense>
 				</Container>
 			</Route>
-			<Route path={`${path}/${CERTIFICATES_ROUTE_ID}`}>
+			<Route path={`${path}/${SUBSCRIPTIONS_ROUTE_ID}`}>
 				<Container orientation="horizontal" mainAlignment="flex-start">
 					<Container width="40%">
-						<Text>Certificate Sidebar</Text>
+						<Text>{t('label.subscriptions', 'Subscriptions')}</Text>
 					</Container>
 					<Suspense fallback={<Spinner />}>
 						<DomainDetailPanel />
 					</Suspense>
 				</Container>
 			</Route>
-			<Route path={`${path}/${COS_ROUTE_ID}`}>
+			<Route path={`${path}/${BACKUP_ROUTE_ID}`}>
 				<Container orientation="horizontal" mainAlignment="flex-start">
 					<Container width="40%">
-						<Text>COS</Text>
-					</Container>
-					<Suspense fallback={<Spinner />}>
-						<DomainDetailPanel />
-					</Suspense>
-				</Container>
-			</Route>
-			<Route path={`${path}/${CORE_ROUTE_ID}`}>
-				<Container orientation="horizontal" mainAlignment="flex-start">
-					<Container width="40%">
-						<Text>Core</Text>
-					</Container>
-					<Suspense fallback={<Spinner />}>
-						<DomainDetailPanel />
-					</Suspense>
-				</Container>
-			</Route>
-			<Route path={`${path}/${FEATURES_ROUTE_ID}`}>
-				<Container orientation="horizontal" mainAlignment="flex-start">
-					<Container width="40%">
-						<Text>Features</Text>
+						<Text>{t('label.backup', 'Backup')}</Text>
 					</Container>
 					<Suspense fallback={<Spinner />}>
 						<DomainDetailPanel />
--- src/views/secondary-bar/sidebar.tsx
+++ src/views/secondary-bar/sidebar.tsx
@@ -3,24 +3,34 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { FC, useMemo } from 'react';
-import { Accordion } from '@zextras/carbonio-design-system';
+import React, { FC, useCallback, useMemo } from 'react';
+import { Accordion, Container, Row, Text, Divider, Padding } from '@zextras/carbonio-design-system';
 import { useTranslation } from 'react-i18next';
 import { replaceHistory } from '@zextras/carbonio-shell-ui';
 import { useLocation } from 'react-router-dom';
-import { map } from 'lodash';
 import MatomoTracker from '../../matomo-tracker';
 import {
-	CERTIFICATES_ROUTE_ID,
-	CORE_ROUTE_ID,
-	COS_ROUTE_ID,
+	BACKUP_ROUTE_ID,
 	DOMAINS_ROUTE_ID,
-	FEATURES_ROUTE_ID,
 	MANAGE_APP_ID,
-	SERVER_AND_VOLUMES_ROUTE_ID
+	STORAGES_ROUTE_ID,
+	SUBSCRIPTIONS_ROUTE_ID
 } from '../../constants';
 
-const textProps = { fontWeight: '700', color: '#414141' };
+const textProps = { color: 'gray0', weight: 'bold' };
+
+const Header: FC<{ title: string }> = ({ title }) => (
+	<>
+		<Row takeAvwidth="fill" mainAlignment="flex-start" width="100%">
+			<Padding vertical="medium" horizontal="large" width="100%">
+				<Text size="small" color="gray0" weight="bold">
+					{title}
+				</Text>
+			</Padding>
+		</Row>
+		<Divider />
+	</>
+);
 
 const SidebarView: FC = () => {
 	const [t] = useTranslation();
@@ -37,107 +47,39 @@ const SidebarView: FC = () => {
 				trackView: 'Domains'
 			},
 			{
-				id: 'server-and-volumes',
-				route: SERVER_AND_VOLUMES_ROUTE_ID,
-				label: t('label.serverl_and_volumes', 'Server & Volumes'),
+				id: 'storages',
+				route: STORAGES_ROUTE_ID,
+				label: t('label.storages', 'Storages'),
 				icon: 'HardDriveOutline',
-				trackView: 'Server and Volumes'
+				trackView: 'Storages'
 			},
 			{
-				id: 'cos',
-				route: COS_ROUTE_ID,
-				label: t('label.cos', 'CoS'),
-				icon: 'CosOutline',
-				trackView: 'COS'
-			},
-			{
-				id: 'certificates',
-				route: CERTIFICATES_ROUTE_ID,
-				label: t('label.certificates', 'Certificates'),
+				id: 'subscriptions',
+				route: SUBSCRIPTIONS_ROUTE_ID,
+				label: t('label.subscriptions', 'Subscriptions'),
 				icon: 'AwardOutline',
-				trackView: 'Certificates'
-			},
-			{
-				id: 'core',
-				route: CORE_ROUTE_ID,
-				label: t('label.core', 'Core'),
-				icon: 'CoreModeOutline',
-				trackView: 'Core',
-				subSections: [
-					{
-						id: 'core-subscription',
-						route: 'subscription',
-						label: t('label.subscriptions', 'Subscriptions'),
-						icon: 'BarChartOutline',
-						trackView: 'Core/Subscriptions'
-					},
-					{
-						id: 'core-notification',
-						route: 'notification',
-						label: t('label.notification', 'Notification'),
-						icon: 'EmailOutline',
-						trackView: 'Core/Notifications'
-					},
-					{
-						id: 'core-log',
-						route: 'log',
-						label: t('label.log', 'Log'),
-						icon: 'CodeOutline',
-						trackView: 'Core/Logs'
-					},
-					{
-						id: 'core-privacy',
-						route: 'privacy',
-						label: t('label.privacy', 'Privacy'),
-						icon: 'LockOutline',
-						trackView: 'Core/Privacy'
-					}
-				]
-			},
-			{
-				id: 'features',
-				route: FEATURES_ROUTE_ID,
-				label: t('label.features', 'Features'),
-				icon: 'GridOutline',
-				trackView: 'Features',
-				subSections: [
-					{
-						id: 'features-admins',
-						route: 'admins',
-						label: t('label.admins', 'Admins'),
-						icon: 'CrownOutline',
-						trackView: 'Features/Admins'
-					},
-					{
-						id: 'features-backup',
-						route: 'backup',
-						label: t('label.backup', 'Backup'),
-						icon: 'HistoryOutline',
-						trackView: 'Features/Backup'
-					},
-					{
-						id: 'features-activesync',
-						route: 'activesync',
-						label: t('label.active_sync', 'ActiveSync'),
-						icon: 'SmartphoneOutline',
-						trackView: 'Features/ActiveSync'
-					},
-					{
-						id: 'features-storages',
-						route: 'storages',
-						label: t('label.storages', 'Storages'),
-						icon: 'CubeOutline',
-						trackView: 'Features/Storages'
-					}
-				]
+				trackView: 'Subscriptions'
 			}
 		],
 		[t]
 	);
 
-	const items = useMemo(
-		() =>
-			manageViews.map((view) => ({
+	const serviceViews = useMemo(
+		() => [
+			{
+				id: 'backup',
+				route: BACKUP_ROUTE_ID,
+				label: t('label.backup', 'Backup'),
+				icon: 'HistoryOutline',
+				trackView: 'Backup'
+			}
+		],
+		[t]
+	);
+
+	const getAccorionItems = useCallback(
+		(views: any) =>
+			views.map((view: any) => ({
 				id: view.id,
 				label: view.label,
 				icon: view.icon,
@@ -150,23 +92,27 @@ const SidebarView: FC = () => {
 					e.stopPropagation();
 					replaceHistory(`/${view.route}`);
 					matomo.trackPageView(`${view.trackView}`);
-				},
-				items: map(view.subSections, (item) => ({
-					...item,
-					textProps,
-					active: location.pathname === `/${MANAGE_APP_ID}/${view.route}/${item.route}`,
-					disableHover: location.pathname === `/${MANAGE_APP_ID}/${view.route}/${item.route}`,
-					onClick: (e: MouseEvent): void => {
-						e.stopPropagation();
-						replaceHistory(`/${view.route}/${item.route}`);
-						matomo.trackPageView(`${view.trackView}`);
-					}
-				}))
+				}
 			})),
-		[manageViews, location, matomo]
+		[location.pathname, location.search, matomo]
 	);
 
-	return <Accordion items={items} />;
+	const manageItems = useMemo(() => getAccorionItems(manageViews), [manageViews, getAccorionItems]);
+
+	const serviceItems = useMemo(
+		() => getAccorionItems(serviceViews),
+		[serviceViews, getAccorionItems]
+	);
+
+	return (
+		<Container orientation="column" crossAlignment="flex-start" mainAlignment="flex-start">
+			<Header title={t('label.management', 'Management')} />
+			<Accordion items={manageItems} />
+			<Padding top="large" bottom="large"></Padding>
+			<Header title={t('label.services', 'Services')} />
+			<Accordion items={serviceItems} />
+		</Container>
+	);
 };
 
 export default SidebarView;
