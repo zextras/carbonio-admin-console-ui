5ab0229576ea30397fea87c0739f3cfb326f83fb
zextras_kapil <kapil.nadiyapara@zextras.com>
2022-06-16 14:36:21 +0530

    feat: backup view and sidebar added

--- src/views/app-view.tsx
+++ src/views/app-view.tsx
@@ -31,6 +31,8 @@ import BreadCrumb from './components/breadcrumb/breadcrumb-view';
 import BackupApp from './features/backup/BackupApp';
 import CosListPanel from './cos/cos-list-panel';
 import CosDetailPanel from './cos/cos-detail-panel';
+import BackupListPanel from './backup/backup-list-panel';
+import BackupDetailPanel from './backup/backup-detail-panel';
 
 const AppView: FC = () => {
 	const { path } = useRouteMatch();
@@ -106,8 +108,13 @@ const AppView: FC = () => {
 				</Route>
 				<Route path={`/${SERVICES_ROUTE_ID}/${BACKUP_ROUTE_ID}`}>
 					<Container orientation="horizontal" mainAlignment="flex-start">
+						<Container style={{ maxWidth: '265px' }}>
+							<Suspense fallback={<Spinner />}>
+								<BackupListPanel />
+							</Suspense>
+						</Container>
 						<Suspense fallback={<Spinner />}>
-							<BackupApp />
+							<BackupDetailPanel />
 						</Suspense>
 					</Container>
 				</Route>
---
+++ src/views/backup/backup-detail-panel.tsx
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const BackupDetailPanel: FC = () => {
+	const [t] = useTranslation();
+	return (
+		<div>
+			<div>Test Detail</div>
+		</div>
+	);
+};
+export default BackupDetailPanel;
