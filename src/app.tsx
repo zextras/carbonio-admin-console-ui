1325cdab461f9667e0964c19a0865e1bb2f4ecfb
zextras-vijay <vijay.godhasara@zextras.com>
2022-10-11 15:57:39 +0530

    chore: change backup icon

--- src/app.tsx
+++ src/app.tsx
@@ -55,6 +55,7 @@ import { getAllServers } from './services/get-all-servers-service';
 import { useConfigStore } from './store/config/store';
 import { getAllConfig } from './services/get-all-config';
 import { useAuthIsAdvanced } from './store/auth-advanced/store';
+import SvgBackupOutline from './icons/outline/BackupOutline';
 
 const LazyAppView = lazy(() => import('./views/app-view'));
 
@@ -354,29 +355,15 @@ const App: FC = () => {
 		[subscriptionTooltipItems]
 	);
 
-	const backupOutline: FC = useCallback(
-		(props) => (
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
-				<g data-name="Layer 2">
-					<path
-						d="M20.42 10.18L12.71 2.3a1 1 0 00-1.42 0l-7.71 7.89A2 2 0 003 11.62V20a2 2 0 001.89 2h14.22A2 2 0 0021 20v-8.38a2.07 2.07 0 00-.58-1.44zM10 20v-6h4v6zm9 0h-3v-7a1 1 0 00-1-1H9a1 1 0 00-1 1v7H5v-8.42l7-7.15 7 7.19z"
-						data-name="home"
-					/>
-				</g>
-			</svg>
-		),
-		[]
-	);
-
 	const backupPrimaryBar: FC = useCallback(
 		() => (
 			<PrimaryBarIconButton
-				icon={backupOutline}
+				icon={SvgBackupOutline}
 				size="large"
 				onClick={(): void => history.push(`/${SERVICES_ROUTE_ID}/${BACKUP_ROUTE_ID}`)}
 			/>
 		),
-		[backupOutline, history]
+		[history]
 	);
 
 	useEffect(() => {
