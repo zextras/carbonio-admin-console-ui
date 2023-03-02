f41dcdaf9a3f0074943b43f7db3a50ca8d2f225d
zextras_kapil <kapil.nadiyapara@zextras.com>
2023-03-02 16:27:22 +0530

    chore: update translation for deligated send as recipient

--- src/views/domain/manange/accounts/edit-account/add-delegate-section/delegate-setright-section.tsx
+++ src/views/domain/manange/accounts/edit-account/add-delegate-section/delegate-setright-section.tsx
@@ -100,7 +100,7 @@ const DelegateSetRightsSection: FC = () => {
 									<Radio
 										label={t(
 											'account_details.send_as_recepients',
-											`Send as (recipients will see the sender {{targetEmail}})`,
+											`Send as (recipients will display this sender email {{targetEmail}})`,
 											{
 												targetEmail: accountDetail?.zimbraMailDeliveryAddress
 											}
