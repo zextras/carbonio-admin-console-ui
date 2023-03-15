52b222bb0eda28aea197c339c66a7e9af9a01dc8
mananpatel13589 <38516903+mananpatel13589@users.noreply.github.com>
2023-03-15 17:56:11 +0530

    chore: filter input changes

--- src/views/domain/manange/accounts/edit-account/edit-account-delegates-section.tsx
+++ src/views/domain/manange/accounts/edit-account/edit-account-delegates-section.tsx
@@ -54,6 +54,10 @@ import {
 	SEND_READ_MANAGE_MAILS
 } from '../../../../../constants';
 
+const emailRegex =
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars, max-len, no-control-regex
+	/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
 const WizardInSection: FC<any> = ({ wizard, wizardFooter, setToggleWizardSection }) => {
 	const { t } = useTranslation();
 	return (
@@ -923,7 +927,11 @@ const EditAccountDelegatesSection: FC = () => {
 									listArr = pullAt(listArr, pullIndex);
 								}
 								setSimpleSelectedList(listArr);
-								setSelectedAccounts(data);
+								const filterData: any = [];
+								map(data, (ele) => {
+									if (emailRegex.test(ele.label ?? '')) filterData.push(ele);
+								});
+								setSelectedAccounts(filterData);
 
 								setSearchQuery('');
 							}}
