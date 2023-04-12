22d6021b31b216bacafbf827fa904efdc3252074
Vijay Godhasara <101629244+zextras-vijay@users.noreply.github.com>
2023-04-12 16:20:51 +0530

    fix: close account issue (#324)
    Co-authored-by: zextraskapil <94968406+zextraskapil@users.noreply.github.com>

--- src/views/domain/manange/accounts/manage-accounts.tsx
+++ src/views/domain/manange/accounts/manage-accounts.tsx
@@ -561,7 +561,8 @@ const ManageAccounts: FC = () => {
 		[closeAccountDetailDialog]
 	);
 
-	useOutsideClick(tableRef, closeAccountDetailDialog);
+	/** Commented code for fix issue of AC-529 */
+	// useOutsideClick(tableRef, closeAccountDetailDialog);
 
 	useEffect(() => {
 		window.addEventListener('keydown', handleKeyEvent);
