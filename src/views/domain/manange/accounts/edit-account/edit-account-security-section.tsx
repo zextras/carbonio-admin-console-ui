c96ff3a6396e91024d2d26566b20b03cc0adaa24
Manan Patel <manan13589@gmail.com>
2023-04-26 14:50:39 +0530

    chore: email validation from utils

--- src/views/domain/manange/accounts/edit-account/edit-account-security-section.tsx
+++ src/views/domain/manange/accounts/edit-account/edit-account-security-section.tsx
@@ -12,7 +12,6 @@ import {
 	Text,
 	useSnackbar,
 	Table,
-	Divider,
 	ChipInput,
 	Icon,
 	Switch,
@@ -36,10 +35,7 @@ import { useAuthIsAdvanced } from '../../../../../store/auth-advanced/store';
 import ListRow from '../../../../list/list-row';
 import CustomRowFactory from '../../../../app/shared/customTableRowFactory';
 import CustomHeaderFactory from '../../../../app/shared/customTableHeaderFactory';
-
-const emailRegex =
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars, max-len, no-control-regex
-	/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+import { isValidEmail } from '../../../../utility/utils';
 
 const StaticCodesContainer = styled(Row)`
 	max-width: 350px;
@@ -223,7 +219,7 @@ const EditAccountSecuritySection: FC = () => {
 										onChange={(contacts: any): void => {
 											const data: any = [];
 											map(contacts, (contact) => {
-												if (emailRegex.test(contact.label ?? '')) data.push(contact);
+												if (isValidEmail(contact.label ?? '')) data.push(contact);
 											});
 											setSendEmailTo(data);
 										}}
