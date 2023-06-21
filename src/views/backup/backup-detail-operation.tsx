ddc43eb79bdf19fc348b0fd310199e5e5f34a552
Manan Patel <manan13589@gmail.com>
2023-06-21 08:50:49 +0530

    chore: backup admin rights

--- src/views/backup/backup-detail-operation.tsx
+++ src/views/backup/backup-detail-operation.tsx
@@ -17,32 +17,14 @@ import ImportExternalBackup from './actions/import-external-backup';
 import BackupAdvanced from './default-setting/backup-advanced';
 import BackupServerConfig from './default-setting/backup-server-config';
 import ServersList from './default-setting/backup-servers-list';
-import { dumpGlobalConfig } from '../../services/dump-global-config';
+
 import { useBackupStore } from '../../store/backup/store';
 import ServerAdvanced from './server-advanced/server-advanced';
 import BackupConfiguration from './configuration/backup-configuration';
 
 const BackupDetailOperation: FC = () => {
 	const { operation, server }: { operation: string; server: string } = useParams();
-	const globalConfig = useBackupStore((state) => state.globalConfig);
-	const setGlobalConfig = useBackupStore((state) => state.setGlobalConfig);
 	const setBackupSelectedServer = useBackupStore((state) => state.setSelectedBackupServer);
-
-	const getGlobalConfig = useCallback((): void => {
-		const serverName = window.location.hostname;
-		dumpGlobalConfig(serverName).then((data: any) => {
-			if (data?.Body?.response?.content) {
-				const parseData = JSON.parse(data.Body.response.content);
-				if (parseData?.response) {
-					setGlobalConfig(parseData?.response);
-				}
-			}
-		});
-	}, [setGlobalConfig]);
-
-	useEffect(() => {
-		!globalConfig?.privateKeyAlgorithm && getGlobalConfig();
-	}, [getGlobalConfig, globalConfig?.privateKeyAlgorithm]);
 
 	useEffect(() => {
 		if (server) {
