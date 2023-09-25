b995de79971cba8e65dc9bc6aa1fc14ef2bd62d6
zextras_kapil <kapil.nadiyapara@zextras.com>
2023-09-25 10:13:04 +0530

    fix: ac-470: hsm policy error handling added

--- src/views/bucket/hsm/hsm-setting-panel.tsx
+++ src/views/bucket/hsm/hsm-setting-panel.tsx
@@ -549,6 +549,19 @@ const HSMsettingPanel: FC = () => {
 									replace: true
 								});
 							}
+						} else if (info?.error && info?.error?.code === 'MODULE_OR_FEATURE_NOT_LICENSED') {
+							setIsEditSaveInProgress(false);
+							createSnackbar({
+								key: 'error',
+								type: 'error',
+								label: t(
+									'label.storage_hsm_not_licensed',
+									'Cannot complete operation: storages_hsm not licensed.'
+								),
+								autoHideTimeout: 3000,
+								hideButton: true,
+								replace: true
+							});
 						}
 					}
 				})
