5ff1b0c6608973b53c973f6be464b23e2e33f697
Tarun <tarunmeel94@gmail.com>
2023-10-23 13:30:46 +0530

    feat: ac-653: allow Global Admin to list all global and delegated admin in the infrastructure

--- src/views/domain/global-operation.tsx
+++ src/views/domain/global-operation.tsx
@@ -6,10 +6,11 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
-import { THEME, TWO_FACTOR_AUTHENTICATION, DOMAINS } from '../../constants';
+import { THEME, TWO_FACTOR_AUTHENTICATION, DOMAINS, GLOBAL_DELEGATES } from '../../constants';
 import GlobalTheme from './global/global-theme';
 import DomainList from './domain-list/domain-list';
 import GlobalTwoFactorAuthentcation from './global/global-two-factor-auth';
+import GlobalDelegates from './global-delegates';
 
 const GlobalOperations: FC = () => {
 	const [t] = useTranslation();
@@ -25,6 +26,8 @@ const GlobalOperations: FC = () => {
 						return <GlobalTwoFactorAuthentcation />;
 					case DOMAINS:
 						return <DomainList />;
+					case GLOBAL_DELEGATES:
+						return <GlobalDelegates />;
 					default:
 						return null;
 				}
