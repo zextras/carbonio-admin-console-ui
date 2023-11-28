415b265ede49942d627c6e2643f4b1302376ed50
Divyang Dodiya <divyang.dodiya@zextras.com>
2023-11-28 11:36:47 +0530

    fix: ac-871: fixed mailing list subdomain dashed data issue

--- src/views/domain/manange/mailing-list/mailing-list-members-section.tsx
+++ src/views/domain/manange/mailing-list/mailing-list-members-section.tsx
@@ -25,7 +25,7 @@ import CustomHeaderFactory from '../../../app/shared/customTableHeaderFactory';
 import CustomRowFactory from '../../../app/shared/customTableRowFactory';
 import DropDownInput from '../../../components/dropDownInput';
 import ListRow from '../../../list/list-row';
-import { getAllEmailFromString, isValidEmail } from '../../../utility/utils';
+import { getAllEmailFromString } from '../../../utility/utils';
 
 const MailingListMembersSection: FC<any> = () => {
 	const { t } = useTranslation();
@@ -88,9 +88,13 @@ const MailingListMembersSection: FC<any> = () => {
 	const onAdd = useCallback((): void => {
 		if (member !== '') {
 			const specialChars = /[ `'"<>,;]/;
-			const allEmails: any[] = specialChars.test(member) ? getAllEmailFromString(member) : [member];
+			const verifyEmail =
+				/^[_A-Za-z0-9-\\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*(\.[A-Za-z]{2,})$/;
+			const allEmails: string[] = specialChars.test(member)
+				? getAllEmailFromString(member)
+				: [member];
 			if (allEmails !== null && allEmails !== undefined) {
-				const inValidEmailAddress = allEmails.filter((item: any) => !isValidEmail(item));
+				const inValidEmailAddress = allEmails.filter((item: string) => !verifyEmail.test(item));
 				if (inValidEmailAddress && inValidEmailAddress.length > 0) {
 					createSnackbar({
 						key: 'error',
