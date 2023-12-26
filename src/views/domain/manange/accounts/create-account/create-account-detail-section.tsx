bd4fe93f9ed4a2b038273bbd0d5a6d8fe652a940
Tarun Meel <58963927+Tarunmeel94@users.noreply.github.com>
2023-12-26 14:06:52 +0530

    chore: change userName label to use at all interface

--- src/views/domain/manange/accounts/create-account/create-account-detail-section.tsx
+++ src/views/domain/manange/accounts/create-account/create-account-detail-section.tsx
@@ -191,7 +191,7 @@ const CreateAccountDetailSection: FC = () => {
 					<Row width="48%" mainAlignment="flex-start">
 						<Input
 							backgroundColor="gray5"
-							label={t('label.userName_auto_fill', 'username (Auto-fill)')}
+							label={t('label.user_auto_fill', 'user (Auto-fill)')}
 							value={accountDetail?.name}
 							onChange={changeAccName}
 							inputName="name"
@@ -199,10 +199,7 @@ const CreateAccountDetailSection: FC = () => {
 						/>
 						{(accountDetail?.displayName || combineDisplayName) && showAutoFillAlert && (
 							<Text color="error" size="small">
-								{t(
-									'accountDetails.auto_fill_username_is_disabled',
-									'Auto fill username is disabled'
-								)}
+								{t('accountDetails.auto_fill_user_is_disabled', 'Auto fill user is disabled')}
 							</Text>
 						)}
 					</Row>
