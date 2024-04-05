132568616067b87cfc78d2ff429c79285b0d6c9c
Divyang Dodiya <divyang.dodiya@zextras.com>
2024-04-05 12:00:54 +0530

    fix: ac-914: fixed edit account sonar cognitive complexity issue

--- src/views/domain/manange/accounts/edit-account/edit-account.tsx
+++ src/views/domain/manange/accounts/edit-account/edit-account.tsx
@@ -361,113 +361,119 @@ const EditAccount: FC<{
 		[t, accountDetail, createSnackbar]
 	);
 
-	// eslint-disable-next-line sonarjs/cognitive-complexity
-	const modifyAccountReq = useCallback(async () => {
-		const modifiedKeys: any = reduce(
-			accountDetail,
-			function (result, value, key): any {
-				return isEqual(value, initAccountDetail[key]) ? result : [...result, key];
-			},
-			[]
-		);
-		if (deleteAdministrationRights?.length > 0 && modifiedKeys.includes('zimbraIsAdminAccount')) {
-			onDeleteFromList(deleteAdministrationRights);
-		}
-		const modifiedData: any = {};
-		let isPasswordChange = false;
-		remove(modifiedKeys, (ele) => ele === CHANGE_NAME_BOOLEAN);
-		remove(modifiedKeys, (ele) => ele === CHANGE_DISPLAY_NAME_BOOLEAN);
-		remove(modifiedKeys, (ele) => ele === IS_DEFAULT_USER_NAME);
-		if (!accountDetail?.sn?.trim()) {
+	const ErrorSnackbar = useCallback(
+		(label: string, key?: string): void => {
 			createSnackbar({
-				key: 'error',
+				key: key || 'error',
 				type: 'error',
-				label: t('label.surname_required', 'Surname is required'),
+				label,
 				autoHideTimeout: 3000,
 				hideButton: true,
 				replace: true
 			});
-			return;
+		},
+		[createSnackbar]
+	);
+
+	function findModifiedKeys(): string[] {
+		return reduce(
+			accountDetail,
+			(result, value, key): any =>
+				isEqual(value, initAccountDetail[key]) ? result : [...result, key],
+			[]
+		);
+	}
+
+	function handleAdministrationRightsDeletion(modifiedKeys: string[]): any {
+		if (deleteAdministrationRights?.length > 0 && modifiedKeys.includes('zimbraIsAdminAccount')) {
+			onDeleteFromList(deleteAdministrationRights);
 		}
-		// eslint-disable-next-line sonarjs/no-collapsible-if
-		if (accountDetail?.password || accountDetail?.repeatPassword) {
-			if (modifiedKeys.includes('password') || modifiedKeys.includes('repeatPassword')) {
-				if (accountDetail?.password?.length < 6) {
-					createSnackbar({
-						key: 'error',
-						type: 'error',
-						label: t('label.password_lenght_msg', 'Password should be more than 5 character'),
-						autoHideTimeout: 3000,
-						hideButton: true,
-						replace: true
-					});
-					return;
-				}
-				if (accountDetail?.password !== accountDetail?.repeatPassword) {
-					createSnackbar({
-						key: 'error',
-						type: 'error',
-						label: t('label.password_and repeat_password_not_match', 'Passwords do not match'),
-						autoHideTimeout: 3000,
-						hideButton: true,
-						replace: true
-					});
-					return;
-				}
-				setPasswordRequest(initAccountDetail?.zimbraId, accountDetail?.password).then(() => {
-					if (isGlobalAdmin) {
-						flushCache('account', 'id', initAccountDetail?.zimbraId);
-					}
-				});
-				isPasswordChange = true;
-				remove(modifiedKeys, (ele) => ele === 'password' || ele === 'repeatPassword');
+	}
+
+	const handlePasswordChange = useCallback(
+		async (modifiedKeys: string[]): Promise<void> => {
+			if (accountDetail?.password?.length < 6) {
+				ErrorSnackbar(t('label.password_lenght_msg', 'Password should be more than 5 character'));
+				return;
 			}
-		}
-		if (modifiedKeys.includes('uid') || modifiedKeys.includes(DOMAIN_NAME)) {
-			setIsLoading(true);
-			await renameAccountRequest(
-				initAccountDetail?.zimbraId,
-				`${accountDetail?.uid}@${accountDetail?.domainName}`
-			)
-				.then(() => {
-					createSnackbar({
-						key: 'success',
-						type: 'success',
-						label: t(
-							// eslint-disable-next-line sonarjs/no-duplicate-string
-							'label.the_last_changes_has_been_saved_successfully',
-							// eslint-disable-next-line sonarjs/no-duplicate-string
-							'Changes have been saved successfully'
-						),
-						autoHideTimeout: 3000,
-						hideButton: true,
-						replace: true
-					});
-					setIsLoading(false);
-					if (isGlobalAdmin) {
-						flushCache('account', 'id', initAccountDetail?.zimbraId);
-					}
-				})
-				.catch((error) => {
-					createSnackbar({
-						key: 'error',
-						type: 'error',
-						label: error?.message
-							? error?.message
-							: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
-						autoHideTimeout: 3000,
-						hideButton: true,
-						replace: true
-					});
-					setIsLoading(false);
-				});
-			await getAccountList();
-			remove(modifiedKeys, (ele) => ele === UID);
-			if (modifiedKeys.includes(DOMAIN_NAME)) {
-				remove(modifiedKeys, (ele) => ele === DOMAIN_NAME);
-				setShowEditAccountView(false);
+			if (accountDetail?.password !== accountDetail?.repeatPassword) {
+				ErrorSnackbar(t('label.password_and repeat_password_not_match', 'Passwords do not match'));
+				return;
 			}
-		}
+			setPasswordRequest(initAccountDetail?.zimbraId, accountDetail?.password).then(() => {
+				if (isGlobalAdmin) {
+					flushCache('account', 'id', initAccountDetail?.zimbraId);
+				}
+			});
+			remove(modifiedKeys, (ele) => ele === 'password' || ele === 'repeatPassword');
+		},
+		[
+			ErrorSnackbar,
+			accountDetail?.password,
+			accountDetail?.repeatPassword,
+			initAccountDetail?.zimbraId,
+			isGlobalAdmin,
+			t
+		]
+	);
+
+	const handleAccountRename = useCallback(
+		async (modifiedKeys: string[]) => {
+			if (modifiedKeys.includes('uid') || modifiedKeys.includes(DOMAIN_NAME)) {
+				setIsLoading(true);
+				await renameAccountRequest(
+					initAccountDetail?.zimbraId,
+					`${accountDetail?.uid}@${accountDetail?.domainName}`
+				)
+					.then(() => {
+						createSnackbar({
+							key: 'success',
+							type: 'success',
+							label: t(
+								// eslint-disable-next-line sonarjs/no-duplicate-string
+								'label.the_last_changes_has_been_saved_successfully',
+								// eslint-disable-next-line sonarjs/no-duplicate-string
+								'Changes have been saved successfully'
+							),
+							autoHideTimeout: 3000,
+							hideButton: true,
+							replace: true
+						});
+						setIsLoading(false);
+						if (isGlobalAdmin) {
+							flushCache('account', 'id', initAccountDetail?.zimbraId);
+						}
+					})
+					.catch((error) => {
+						ErrorSnackbar(
+							error?.message
+								? error?.message
+								: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.')
+						);
+						setIsLoading(false);
+					});
+				await getAccountList();
+				remove(modifiedKeys, (ele) => ele === UID);
+				if (modifiedKeys.includes(DOMAIN_NAME)) {
+					remove(modifiedKeys, (ele) => ele === DOMAIN_NAME);
+					setShowEditAccountView(false);
+				}
+			}
+		},
+		[
+			ErrorSnackbar,
+			accountDetail?.domainName,
+			accountDetail?.uid,
+			createSnackbar,
+			getAccountList,
+			initAccountDetail?.zimbraId,
+			isGlobalAdmin,
+			setShowEditAccountView,
+			t
+		]
+	);
+
+	const handleCoreAttributesModification = async (modifiedKeys: string[]): Promise<void> => {
 		if (modifiedKeys.includes(ABQ_MODE) || modifiedKeys.includes(BACKUP_ENABLED)) {
 			const body: any = {};
 			if (modifiedKeys.includes(ABQ_MODE)) {
@@ -485,7 +491,7 @@ const EditAccount: FC<{
 				};
 			}
 
-			setCoreAttributes(body)
+			await setCoreAttributes(body)
 				.then(() => {
 					createSnackbar({
 						key: 'success',
@@ -501,112 +507,112 @@ const EditAccount: FC<{
 					setIsLoading(false);
 				})
 				.catch((error) => {
+					ErrorSnackbar(
+						error?.message
+							? error?.message
+							: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.')
+					);
+					setIsLoading(false);
+				});
+			remove(modifiedKeys, (ele) => ele === BACKUP_ENABLED);
+			remove(modifiedKeys, (ele) => ele === ABQ_MODE);
+		}
+	};
+
+	const handleAliasChanges = async (
+		deleteAliasArr: any,
+		addAliasArr: any,
+		modifiedKeys: string[]
+	): Promise<void> => {
+		deleteAliasArr.forEach(async (aliasName: any) => {
+			await deleteAccountAliasRequest(initAccountDetail?.zimbraId, `${aliasName}`)
+				.then(() => {
+					if (isGlobalAdmin) {
+						flushCache('account', 'id', initAccountDetail?.zimbraId);
+					}
+				})
+				.catch((error) => {
 					createSnackbar({
-						key: 'error',
+						key: `error${aliasName}`,
 						type: 'error',
 						label: error?.message
 							? error?.message
 							: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
 						autoHideTimeout: 3000,
 						hideButton: true,
-						replace: true
+						replace: false
 					});
 					setIsLoading(false);
 				});
-			remove(modifiedKeys, (ele) => ele === BACKUP_ENABLED);
-			remove(modifiedKeys, (ele) => ele === ABQ_MODE);
-		}
-		const deleteAliasArr = differenceBy(
-			initAccountDetail.mail.split(','),
-			accountDetail.mail.split(',')
-		);
-		const addAliasArr = differenceBy(
-			accountDetail.mail.split(','),
-			initAccountDetail.mail.split(',')
-		);
-		if (modifiedKeys.includes('mail')) {
-			// eslint-disable-next-line array-callback-return
-			deleteAliasArr.forEach((aliasName) => {
-				deleteAccountAliasRequest(initAccountDetail?.zimbraId, `${aliasName}`)
-					.then(() => {
-						if (isGlobalAdmin) {
-							flushCache('account', 'id', initAccountDetail?.zimbraId);
-						}
-					})
-					.catch((error) => {
-						createSnackbar({
-							key: `error${aliasName}`,
-							type: 'error',
-							label: error?.message
-								? error?.message
-								: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
-							autoHideTimeout: 3000,
-							hideButton: true,
-							replace: false
-						});
-						setIsLoading(false);
-					});
-			});
-
-			// eslint-disable-next-line array-callback-return
-			addAliasArr.forEach((aliasName) => {
-				addAccountAliasRequest(initAccountDetail?.zimbraId, `${aliasName}`)
-					.then(() => {
-						if (isGlobalAdmin) {
-							flushCache('account', 'id', initAccountDetail?.zimbraId);
-						}
-					})
-					.catch((error) => {
-						createSnackbar({
-							key: `error${aliasName}`,
-							type: 'error',
-							label: error?.message
-								? error?.message
-								: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
-							autoHideTimeout: 3000,
-							hideButton: true,
-							replace: false
-						});
-						setIsLoading(false);
-					});
-			});
-
-			remove(modifiedKeys, (ele) => ele === 'mail');
-		}
-
-		if (
-			(modifiedKeys.includes(MOBILE_CALENDAR_FEATURE_SYNC) ||
-				modifiedKeys.includes(MOBILE_CONTACT_FEATURE_SYNC)) &&
-			isAdvanced
-		) {
-			const coreAttrBody: any = {
-				mobileCalendarFeatureSync: {
-					value: accountDetail?.mobileCalendarFeatureSync === 'TRUE' ? 'enabled' : 'disabled',
-					objectName: accountDetail?.name,
-					configType: ACCOUNT
-				},
-				mobileContactFeatureSync: {
-					value: accountDetail?.mobileContactFeatureSync === 'TRUE' ? 'enabled' : 'disabled',
-					objectName: accountDetail?.name,
-					configType: ACCOUNT
-				}
-			};
-			modifyCoreAttributes(coreAttrBody);
-			remove(modifiedKeys, (ele) => ele === MOBILE_CALENDAR_FEATURE_SYNC);
-			remove(modifiedKeys, (ele) => ele === MOBILE_CONTACT_FEATURE_SYNC);
-		}
-		modifiedKeys.forEach((ele: any) => {
-			modifiedData[ele] = accountDetail[ele];
 		});
 
-		if (modifiedKeys && modifiedKeys?.length > 0) {
+		addAliasArr.forEach(async (aliasName: any) => {
+			addAccountAliasRequest(initAccountDetail?.zimbraId, `${aliasName}`)
+				.then(() => {
+					if (isGlobalAdmin) {
+						flushCache('account', 'id', initAccountDetail?.zimbraId);
+					}
+				})
+				.catch((error) => {
+					createSnackbar({
+						key: `error${aliasName}`,
+						type: 'error',
+						label: error?.message
+							? error?.message
+							: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
+						autoHideTimeout: 3000,
+						hideButton: true,
+						replace: false
+					});
+					setIsLoading(false);
+				});
+		});
+
+		remove(modifiedKeys, (ele) => ele === 'mail');
+	};
+
+	const handleMobileSyncFeatures = useCallback(
+		(modifiedKeys: string[]) => {
+			if (
+				(modifiedKeys.includes(MOBILE_CALENDAR_FEATURE_SYNC) ||
+					modifiedKeys.includes(MOBILE_CONTACT_FEATURE_SYNC)) &&
+				isAdvanced
+			) {
+				const coreAttrBody: any = {
+					mobileCalendarFeatureSync: {
+						value: accountDetail?.mobileCalendarFeatureSync === 'TRUE' ? 'enabled' : 'disabled',
+						objectName: accountDetail?.name,
+						configType: ACCOUNT
+					},
+					mobileContactFeatureSync: {
+						value: accountDetail?.mobileContactFeatureSync === 'TRUE' ? 'enabled' : 'disabled',
+						objectName: accountDetail?.name,
+						configType: ACCOUNT
+					}
+				};
+				modifyCoreAttributes(coreAttrBody);
+				remove(modifiedKeys, (ele) => ele === MOBILE_CALENDAR_FEATURE_SYNC);
+				remove(modifiedKeys, (ele) => ele === MOBILE_CONTACT_FEATURE_SYNC);
+			}
+		},
+		[
+			accountDetail?.mobileCalendarFeatureSync,
+			accountDetail?.mobileContactFeatureSync,
+			accountDetail?.name,
+			isAdvanced,
+			modifyCoreAttributes
+		]
+	);
+
+	const handleMainModifiedKeys = useCallback(
+		async (initAccountDetails: any, modifiedData: any) => {
 			setIsLoading(true);
-			modifyAccountRequest(initAccountDetail?.zimbraId, modifiedData)
-				.then((data) => {
+			await modifyAccountRequest(initAccountDetails?.zimbraId, modifiedData)
+				.then(async (data) => {
 					if (data) {
 						// setShowCreateAccountView(false);
 						if (isGlobalAdmin) {
-							flushCache('account', 'id', initAccountDetail?.zimbraId);
+							await flushCache('account', 'id', initAccountDetails?.zimbraId);
 						}
 						createSnackbar({
 							key: 'success',
@@ -622,22 +628,77 @@ const EditAccount: FC<{
 						setInitAccountDetail({ ...accountDetail });
 						setIsLoading(false);
 						getAccountList();
-						getAccountDetail(initAccountDetail?.zimbraId);
+						getAccountDetail(initAccountDetails?.zimbraId);
 					}
 				})
 				.catch((error) => {
-					createSnackbar({
-						key: 'error',
-						type: 'error',
-						label: error?.message
+					ErrorSnackbar(
+						error?.message
 							? error?.message
-							: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
-						autoHideTimeout: 3000,
-						hideButton: true,
-						replace: true
-					});
+							: t('label.something_wrong_error_msg', 'Something went wrong. Please try again.')
+					);
 					setIsLoading(false);
 				});
+		},
+		[
+			ErrorSnackbar,
+			accountDetail,
+			createSnackbar,
+			getAccountDetail,
+			getAccountList,
+			isGlobalAdmin,
+			setInitAccountDetail,
+			t
+		]
+	);
+
+	const modifyAccountReq = useCallback(async () => {
+		const modifiedKeys: string[] = findModifiedKeys();
+		handleAdministrationRightsDeletion(modifiedKeys);
+
+		const modifiedData: any = {};
+		let isPasswordChange = false;
+		remove(modifiedKeys, (ele) => ele === CHANGE_NAME_BOOLEAN);
+		remove(modifiedKeys, (ele) => ele === CHANGE_DISPLAY_NAME_BOOLEAN);
+		remove(modifiedKeys, (ele) => ele === IS_DEFAULT_USER_NAME);
+		if (!accountDetail?.sn?.trim()) {
+			ErrorSnackbar(t('label.surname_required', 'Surname is required'));
+			return;
+		}
+
+		// eslint-disable-next-line sonarjs/no-collapsible-if
+		if (accountDetail?.password || accountDetail?.repeatPassword) {
+			if (modifiedKeys.includes('password') || modifiedKeys.includes('repeatPassword')) {
+				handlePasswordChange(modifiedKeys);
+				isPasswordChange = true;
+			}
+		}
+
+		handleAccountRename(modifiedKeys);
+
+		await handleCoreAttributesModification(modifiedKeys);
+
+		const deleteAliasArr = differenceBy(
+			initAccountDetail.mail.split(','),
+			accountDetail.mail.split(',')
+		);
+		const addAliasArr = differenceBy(
+			accountDetail.mail.split(','),
+			initAccountDetail.mail.split(',')
+		);
+
+		if (modifiedKeys.includes('mail')) {
+			await handleAliasChanges(deleteAliasArr, addAliasArr, modifiedKeys);
+		}
+
+		handleMobileSyncFeatures(modifiedKeys);
+
+		modifiedKeys.forEach((ele: any) => {
+			modifiedData[ele] = accountDetail[ele];
+		});
+
+		if (modifiedKeys && modifiedKeys?.length > 0) {
+			await handleMainModifiedKeys(initAccountDetail, modifiedData);
 		} else {
 			if (addAliasArr.length || deleteAliasArr.length) {
 				setInitAccountDetail({ ...accountDetail });
@@ -658,7 +719,9 @@ const EditAccount: FC<{
 			}
 			setInitAccountDetail({ ...accountDetail });
 		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [
+		ErrorSnackbar,
 		accountDetail,
 		createSnackbar,
 		getAccountDetail,
