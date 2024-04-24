f12227d108a88aea9610374bee8b1475054b86a7
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-04-24 17:16:30 +0200

    chore: clean up code

--- src/views/domain/manange/accounts/edit-account/edit-account-security-section.tsx
+++ src/views/domain/manange/accounts/edit-account/edit-account-security-section.tsx
@@ -798,30 +798,33 @@ const EditAccountSecuritySection: FC<{ handleMatomoTrackerEvent: (value: string)
 						padding={{ all: 'large' }}
 						width="100%"
 					>
-						<Text size="extralarge" weight="bold">
-							{t('label.backup', 'Backup')}
-						</Text>
-						<Row mainAlignment="flex-start" width="100%">
-							<Container
-								height="fit"
-								crossAlignment="flex-start"
-								background="gray6"
-								padding={{ top: 'large' }}
-							>
-								{isAdvanced && (
-									<ListRow>
-										<Container crossAlignment="flex-start">
-											<Switch
-												value={undeleteAllowed}
-												onClick={(): void => onClickUndeleteAllowed()}
-												label={t('cos.allow_restore_message', 'Allow user to restore messages')}
-												iconColor="primary"
-											/>
-										</Container>
-									</ListRow>
-								)}
-							</Container>
-						</Row>
+						{isAdvanced && (
+							<>
+								<Text size="extralarge" weight="bold">
+									{t('label.backup', 'Backup')}
+								</Text>
+								<Row mainAlignment="flex-start" width="100%">
+									<Container
+										height="fit"
+										crossAlignment="flex-start"
+										background="gray6"
+										padding={{ top: 'large' }}
+									>
+										<ListRow>
+											<Container crossAlignment="flex-start">
+												<Switch
+													value={undeleteAllowed}
+													onClick={(): void => onClickUndeleteAllowed()}
+													label={t('cos.allow_restore_message', 'Allow user to restore messages')}
+													iconColor="primary"
+												/>
+											</Container>
+										</ListRow>
+										)
+									</Container>
+								</Row>
+							</>
+						)}
 						<Row mainAlignment="flex-start" width="100%">
 							<Container
 								height="fit"
