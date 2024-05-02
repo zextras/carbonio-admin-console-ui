923c7e64310753b79c4ae4e5990eee9c500aef97
Manan Patel <manan13589@gmail.com>
2024-05-02 15:12:45 +0530

    chore: ac-1018 mta my network attribute inherited chip-input

---
+++ src/views/utility/inherited-components/inherited-chip-input.tsx
@@ -0,0 +1,128 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React, { FC } from 'react';
+
+import {
+	ChipInput,
+	Tooltip,
+	IconCheckbox,
+	Text,
+	Row,
+	Padding,
+	Container
+} from '@zextras/carbonio-design-system';
+import { map } from 'lodash';
+import { useTranslation } from 'react-i18next';
+
+const InheritedChipInput: FC<{
+	subValue: any;
+	inheritedValue: any;
+	background?: any;
+	onChange: any;
+	onChangeReset: any;
+	fromSubValue: any;
+	disabled?: boolean;
+	hasError?: boolean;
+	requireUniqueChips?: boolean;
+	pref?: any;
+	description?: any;
+	placeholder?: any;
+	ChipComponent?: any;
+}> = ({
+	placeholder,
+	requireUniqueChips,
+	subValue,
+	inheritedValue,
+	background = 'gray5',
+	onChange,
+	onChangeReset,
+	fromSubValue,
+	disabled = false,
+	hasError = false,
+	pref = {},
+	description,
+	ChipComponent
+}) => {
+	const [t] = useTranslation();
+	return (
+		<Container orientation="horizontal">
+			<Row takeAvailableSpace>
+				<ChipInput
+					placeholder={placeholder}
+					requireUniqueChips={requireUniqueChips}
+					value={subValue === undefined || !subValue?.length ? inheritedValue || '' : subValue}
+					background={background}
+					onChange={onChange}
+					disabled={disabled}
+					hasError={hasError}
+					ChipComponent={ChipComponent}
+					maxChips={null}
+					CustomIcon={(): any => (
+						<>
+							<Tooltip
+								label={
+									<>
+										<Row>
+											<Text weight="bold">
+												{t('account_details.inherited_value_was', 'The inherited value was')} :
+											</Text>
+											<Text overflow={'break-word'}>{`  ${inheritedValue || ''}`}</Text>
+										</Row>
+										<Padding top="small">
+											<Text weight="bold">
+												{t('account_details.click_to_revert', 'Click to revert.')}
+											</Text>
+										</Padding>
+									</>
+								}
+							>
+								<IconCheckbox
+									icon="RefreshOutline"
+									onClick={onChangeReset}
+									style={{ cursor: 'pointer' }}
+									onChange={(): null => null}
+								/>
+							</Tooltip>
+						</>
+					)}
+					description={description}
+					{...pref}
+				/>
+			</Row>
+			{fromSubValue ? (
+				<Tooltip
+					label={
+						<>
+							<Row>
+								<Text weight="bold">
+									{t('account_details.inherited_value_was', 'The inherited value was')} :
+								</Text>
+								<Text>{`  ${inheritedValue ? map(inheritedValue, 'label') : ''}`}</Text>
+							</Row>
+							<Padding top="small">
+								<Text weight="bold">
+									{t('account_details.click_to_revert', 'Click to revert.')}
+								</Text>
+							</Padding>
+						</>
+					}
+				>
+					<IconCheckbox
+						icon="RefreshOutline"
+						value={false}
+						size="large"
+						onClick={onChangeReset}
+						style={{ cursor: 'pointer' }}
+						onChange={(): null => null}
+					/>
+				</Tooltip>
+			) : (
+				<></>
+			)}
+		</Container>
+	);
+};
+export default InheritedChipInput;
