23d738fd79d79c16a14bc5f49794a2a0ff23b8c0
Keshav Bhatt <keshavnrj@gmail.com>
2024-09-24 21:41:47 +0530

    chore: cleanup test

--- src/views/cos/preferences/tests/COSPreferences.test.tsx
+++ src/views/cos/preferences/tests/COSPreferences.test.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable sonarjs/no-duplicate-string */
-/* eslint-disable prettier/prettier */
 /*
  * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
  *
@@ -29,19 +27,8 @@ jest.mock('../../../../services/flush-cache-service', () => ({
 	flushCache: jest.fn()
 }));
 
-// jest.mock('../../../utility/utils', () => ({
-// 	...jest.requireActual('../../../utility/utils'),
-// 	localeList: jest.fn()
-// }));
-
-// jest
-// 	.spyOn(COSPreferences.prototype, 'localeList')
-// 	.mockImplementation((t: TFunction) => localeList(t));
-
 describe('COSPreferences', () => {
 	const mockSetCos = jest.fn();
-	// const mockCreateSnackbar = jest.fn();
-	const mockLocalesFind = jest.fn();
 
 	beforeEach(() => {
 		jest.resetAllMocks();
@@ -54,16 +41,6 @@ describe('COSPreferences', () => {
 		(useRightsStore as unknown as jest.Mock).mockReturnValue({
 			rights: [{ type: 'COS', all: [{ setAttrs: [{ all: true }] }] }]
 		});
-
-		// const localesItems: SelectItem[] = [
-		// 	{ label: 'English', value: 'en' },
-		// 	{ label: 'Spanish', value: 'es' }
-		// ];
-
-		// (localeList as unknown as jest.Mock).mockReturnValue({
-		// 	locales: localesItems,
-		// 	find: mockLocalesFind
-		// });
 	});
 
 	it('should render the component correctly', () => {
@@ -80,79 +57,4 @@ describe('COSPreferences', () => {
 		expect(screen.getByText('Sending Mails')).toBeInTheDocument();
 		expect(screen.getByText('Contact Options')).toBeInTheDocument();
 	});
-
-	// it('should handle save action', async () => {
-	// 	const { user } = setup(<COSPreferences />);
-	//
-	// 	expect(screen.getByText('Language')).toBeInTheDocument();
-	//
-	// 	await user.click(screen.getByText('English - English'));
-	// 	await user.click(screen.getByText('Spanish'));
-	//
-	// 	const saveButton = screen.getByText('Save');
-	// 	await user.click(saveButton);
-	//
-	// 	// Ensure modifyCos and flushCache are called
-	// 	expect(modifyCos).toHaveBeenCalled();
-	// 	expect(flushCache).toHaveBeenCalledWith('cos', 'id', expect.any(String));
-	//
-	// 	// Ensure the snackbar is shown on success
-	// 	expect(mockCreateSnackbar).toHaveBeenCalledWith(
-	// 		expect.objectContaining({ key: 'success', type: 'success' })
-	// 	);
-	//
-	// 	// Ensure the setCos method is called with the updated COS data
-	// 	expect(mockSetCos).toHaveBeenCalled();
-	// });
-	//
-	// it('should handle cancel action', async () => {
-	// 	const { user } = setup(<COSPreferences />);
-	//
-	// 	const cancelButton = screen.getByText('Cancel');
-	// 	await user.click(cancelButton);
-	//
-	// 	// Check that initial values are reset (in this case check if zimbraPrefLocale gets reset)
-	// 	expect(screen.getByText('en_US')).toBeInTheDocument(); // Based on mock cosInformation
-	// });
-	//
-	// it('should set isDirty flag when changes are made', async () => {
-	// 	const { user } = setup(<COSPreferences />);
-	//
-	// 	// Simulate making changes (e.g., changing the locale or switching a setting)
-	// 	const mailSwitch = screen.getByLabelText('User can specify forwarding address');
-	// 	await user.click(mailSwitch);
-	//
-	// 	// Check that the SaveCancelBar becomes enabled due to the dirty state
-	// 	expect(screen.getByText('Save')).not.toBeDisabled();
-	// 	expect(screen.getByText('Cancel')).not.toBeDisabled();
-	// });
-	//
-	// it('should disable switches when readonlyCOS is true', () => {
-	// 	(useRightsStore as unknown as jest.Mock).mockReturnValue({
-	// 		rights: [{ type: 'COS', all: [{ setAttrs: [{ all: false }] }] }]
-	// 	});
-	//
-	// 	setup(<COSPreferences />);
-	//
-	// 	const mailSwitch = screen.getByLabelText('User can specify forwarding address');
-	// 	expect(mailSwitch).toBeDisabled();
-	// });
-
-	// it('should handle attribute changes in sections', async () => {
-	// 	const { user } = setup(<COSPreferences />);
-	//
-	// 	// Simulate changing an attribute in GeneralOptions (e.g., changing the locale)
-	// 	const localeSelect = screen.getByLabelText('Preferred Language');
-	// 	await user.selectOptions(localeSelect, 'fr_FR');
-	//
-	// 	// Check that the state is updated
-	// 	expect(localeSelect.value).toBe('fr_FR');
-	//
-	// 	// Simulate changing a switch in MailOptions
-	// 	const mailSwitch = screen.getByLabelText('User can specify forwarding address');
-	// 	await user.click(mailSwitch);
-	//
-	// 	// Ensure the switch toggles the value correctly
-	// 	expect(mailSwitch.checked).toBe(true);
-	// });
 });
