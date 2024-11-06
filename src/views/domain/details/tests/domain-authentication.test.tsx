0046277755e3c03bd3ad717a8bebce61cebf5da0
Keshav Bhatt <keshavnrj@gmail.com>
2024-11-06 18:43:42 +0530

    chore: simplify tests by parameterizing them

--- src/views/domain/details/tests/domain-authentication.test.tsx
+++ src/views/domain/details/tests/domain-authentication.test.tsx
@@ -38,120 +38,47 @@ function getDefaultEmptyDomain(): Domain {
 	};
 }
 
-function getDefaultDomain(args?: {
-	zimbraFeatureResetPasswordStatus?: string;
-	zimbraAuthLdapStartTlsEnabled?: string;
-}): Domain {
+function getDefaultDomain(
+	overrides: {
+		zimbraFeatureResetPasswordStatus?: string;
+		zimbraAuthLdapStartTlsEnabled?: string;
+	} = {}
+): Domain {
+	const { zimbraFeatureResetPasswordStatus = 'enabled', zimbraAuthLdapStartTlsEnabled = 'TRUE' } =
+		overrides;
+
 	return {
 		name: 'demo.zextras.io',
 		id: '142f56c1-aaf1-432b-9cfa-448e1b952cf6',
 		a: [
-			{
-				n: 'zimbraGalAccountId',
-				_content: '5b099ec4-1a40-4b98-ba2b-63525d5fc'
-			},
-			{
-				n: 'carbonioNotificationFrom',
-				_content: 'zextras@demo.zextras.io'
-			},
-			{
-				n: 'zimbraDomainName',
-				_content: 'demo.zextras.io'
-			},
-			{
-				n: 'carbonioSearchSpecifiedDomainsByFeature',
-				_content: 'abc.com'
-			},
-			{
-				n: 'objectClass',
-				_content: 'dcObject'
-			},
-			{
-				n: 'objectClass',
-				_content: 'organization'
-			},
-			{
-				n: 'objectClass',
-				_content: 'zimbraDomain'
-			},
-			{
-				n: 'objectClass',
-				_content: 'amavisAccount'
-			},
-			{
-				n: 'carbonioNotificationRecipients',
-				_content: 'zextras@demo.zextras.io'
-			},
-			{
-				n: 'zimbraMailStatus',
-				_content: 'enabled'
-			},
-			{
-				n: 'o',
-				_content: 'demo.zextras.io domain'
-			},
-			{
-				n: 'zimbraNotes',
-				_content: 'aab'
-			},
-			{
-				n: 'zimbraPublicServiceHostname',
-				_content: 'kc-dev5-u22-ce.demo.zextras.io'
-			},
-			{
-				n: 'zimbraDomainStatus',
-				_content: 'active'
-			},
-			{
-				n: 'zimbraDomainDefaultCOSId',
-				_content: '230fafd4-987b-4d2c-a4ba-fb9bc6b71f97'
-			},
-			{
-				n: 'zimbraId',
-				_content: '142f56c1-aaf1-432b-9cfa-448e1b952cf6'
-			},
-			{
-				n: 'zimbraDomainType',
-				_content: 'local'
-			},
-			{
-				n: 'zimbraCreateTimestamp',
-				_content: '20240718072455.621Z'
-			},
-			{
-				n: 'zimbraAggregateQuotaLastUsage',
-				_content: '905962776'
-			},
-			{
-				n: 'dc',
-				_content: 'demo'
-			},
-			{
-				n: 'zimbraFeatureResetPasswordStatus',
-				_content: args?.zimbraFeatureResetPasswordStatus ?? 'enabled'
-			},
-			{
-				n: 'zimbraAuthLdapStartTlsEnabled',
-				_content: args?.zimbraAuthLdapStartTlsEnabled ?? 'TRUE'
-			}
+			{ n: 'zimbraGalAccountId', _content: '5b099ec4-1a40-4b98-ba2b-63525d5fc' },
+			{ n: 'carbonioNotificationFrom', _content: 'zextras@demo.zextras.io' },
+			{ n: 'zimbraDomainName', _content: 'demo.zextras.io' },
+			{ n: 'carbonioSearchSpecifiedDomainsByFeature', _content: 'abc.com' },
+			{ n: 'objectClass', _content: 'dcObject' },
+			{ n: 'objectClass', _content: 'organization' },
+			{ n: 'objectClass', _content: 'zimbraDomain' },
+			{ n: 'objectClass', _content: 'amavisAccount' },
+			{ n: 'carbonioNotificationRecipients', _content: 'zextras@demo.zextras.io' },
+			{ n: 'zimbraMailStatus', _content: 'enabled' },
+			{ n: 'o', _content: 'demo.zextras.io domain' },
+			{ n: 'zimbraNotes', _content: 'aab' },
+			{ n: 'zimbraPublicServiceHostname', _content: 'kc-dev5-u22-ce.demo.zextras.io' },
+			{ n: 'zimbraDomainStatus', _content: 'active' },
+			{ n: 'zimbraDomainDefaultCOSId', _content: '230fafd4-987b-4d2c-a4ba-fb9bc6b71f97' },
+			{ n: 'zimbraId', _content: '142f56c1-aaf1-432b-9cfa-448e1b952cf6' },
+			{ n: 'zimbraDomainType', _content: 'local' },
+			{ n: 'zimbraCreateTimestamp', _content: '20240718072455.621Z' },
+			{ n: 'zimbraAggregateQuotaLastUsage', _content: '905962776' },
+			{ n: 'dc', _content: 'demo' },
+			{ n: 'zimbraFeatureResetPasswordStatus', _content: zimbraFeatureResetPasswordStatus },
+			{ n: 'zimbraAuthLdapStartTlsEnabled', _content: zimbraAuthLdapStartTlsEnabled }
 		]
 	};
 }
 
 describe('Domain Authentication', () => {
 	const setupDomainStore = (): void => {
-		useDomainStore.getState().setCosList([
-			{
-				id: 'e00428a1-0c00-11d9-836a-000d93afea2a',
-				name: 'default',
-				isDefaultCos: true,
-				a: [
-					{ n: 'zimbraId', _content: 'e00428a1-0c00-11d9-836a-000d93afea2a' },
-					{ n: 'zimbraPrefLocale', _content: 'en' },
-					{ n: 'zimbraPrefMessageViewHtmlPreferred', _content: 'TRUE' }
-				]
-			}
-		]);
 		useDomainStore.getState().setDomain(getDefaultDomain());
 	};
 
@@ -162,93 +89,60 @@ describe('Domain Authentication', () => {
 		(useSnackbar as jest.Mock).mockReturnValue(createSnackbarSpy);
 	});
 
-	it('reset password switch must be turned on when zimbraFeatureResetPasswordStatus is enabled', () => {
-		useAuthIsAdvanced.getState().setIsAdvanced(true);
+	describe('Verify Auth section Toggle switches', () => {
+		beforeEach(() => {
+			useAuthIsAdvanced.getState().setIsAdvanced(true);
+		});
 
-		setup(<DomainAuthentication />);
-		expect(screen.getByText('Verify Auth')).toBeInTheDocument();
+		const resetPasswordTests = [
+			{ test: 'turned on', status: 'enabled', expectedIcon: 'icon: ToggleRight' },
+			{ test: 'turned off', status: 'disabled', expectedIcon: 'icon: ToggleLeftOutline' },
+			{ test: 'turned off', status: 'suspended', expectedIcon: 'icon: ToggleLeftOutline' },
+			{ test: 'turned off', status: undefined, expectedIcon: 'icon: ToggleLeftOutline' }
+		];
 
-		const forgotPasswordSwitch = screen.getByTestId('reset-password-switch');
-		expect(within(forgotPasswordSwitch).getByTestId('icon: ToggleRight')).toBeInTheDocument();
-	});
+		test.each(resetPasswordTests)(
+			'reset password switch must be $test when zimbraFeatureResetPasswordStatus is $status',
+			({ status, expectedIcon }) => {
+				useDomainStore
+					.getState()
+					.setDomain(
+						status === undefined
+							? getDefaultEmptyDomain()
+							: getDefaultDomain({ zimbraFeatureResetPasswordStatus: status })
+					);
 
-	it('reset password switch must be turned off when zimbraFeatureResetPasswordStatus is disabled', () => {
-		useAuthIsAdvanced.getState().setIsAdvanced(true);
+				setup(<DomainAuthentication />);
+				expect(screen.getByText('Verify Auth')).toBeInTheDocument();
 
-		useDomainStore
-			.getState()
-			.setDomain(getDefaultDomain({ zimbraFeatureResetPasswordStatus: 'disabled' }));
+				const forgotPasswordSwitch = screen.getByTestId('reset-password-switch');
+				expect(within(forgotPasswordSwitch).getByTestId(expectedIcon)).toBeInTheDocument();
+			}
+		);
 
-		setup(<DomainAuthentication />);
-		expect(screen.getByText('Verify Auth')).toBeInTheDocument();
+		const secureConnectionTests = [
+			{ enabled: 'TRUE', expectedIcon: 'icon: ToggleRight' },
+			{ enabled: 'FALSE', expectedIcon: 'icon: ToggleLeftOutline' },
+			{ enabled: undefined, expectedIcon: 'icon: ToggleLeftOutline' }
+		];
 
-		const forgotPasswordSwitch = screen.getByTestId('reset-password-switch');
-		expect(within(forgotPasswordSwitch).getByTestId('icon: ToggleLeftOutline')).toBeInTheDocument();
-	});
+		test.each(secureConnectionTests)(
+			'enable secure connection switch must be turned $expectedIcon when zimbraAuthLdapStartTlsEnabled is $enabled',
+			({ enabled, expectedIcon }) => {
+				useDomainStore
+					.getState()
+					.setDomain(
+						enabled === undefined
+							? getDefaultEmptyDomain()
+							: getDefaultDomain({ zimbraAuthLdapStartTlsEnabled: enabled })
+					);
 
-	it('reset password switch must be turned off when zimbraFeatureResetPasswordStatus is suspended', () => {
-		useAuthIsAdvanced.getState().setIsAdvanced(true);
+				setup(<DomainAuthentication />);
+				expect(screen.getByText('Verify Auth')).toBeInTheDocument();
 
-		useDomainStore
-			.getState()
-			.setDomain(getDefaultDomain({ zimbraFeatureResetPasswordStatus: 'suspended' }));
-
-		setup(<DomainAuthentication />);
-		expect(screen.getByText('Verify Auth')).toBeInTheDocument();
-
-		const forgotPasswordSwitch = screen.getByTestId('reset-password-switch');
-		expect(within(forgotPasswordSwitch).getByTestId('icon: ToggleLeftOutline')).toBeInTheDocument();
-	});
-
-	it('reset password  switch must be turned off when zimbraFeatureResetPasswordStatus is unset/missing in the domain', () => {
-		useAuthIsAdvanced.getState().setIsAdvanced(true);
-
-		useDomainStore.getState().setDomain(getDefaultEmptyDomain());
-
-		setup(<DomainAuthentication />);
-		expect(screen.getByText('Verify Auth')).toBeInTheDocument();
-
-		const forgotPasswordSwitch = screen.getByTestId('reset-password-switch');
-		expect(within(forgotPasswordSwitch).getByTestId('icon: ToggleLeftOutline')).toBeInTheDocument();
-	});
-
-	it('enable secure connection switch must be turned on when zimbraAuthLdapStartTlsEnabled is TRUE', () => {
-		useAuthIsAdvanced.getState().setIsAdvanced(true);
-
-		setup(<DomainAuthentication />);
-		expect(screen.getByText('Verify Auth')).toBeInTheDocument();
-
-		const enableSecureConnection = screen.getByTestId('enable-secure-connection');
-		expect(within(enableSecureConnection).getByTestId('icon: ToggleRight')).toBeInTheDocument();
-	});
-
-	it('enable secure connection switch must be turned off when zimbraAuthLdapStartTlsEnabled is FALSE', () => {
-		useAuthIsAdvanced.getState().setIsAdvanced(true);
-
-		useDomainStore
-			.getState()
-			.setDomain(getDefaultDomain({ zimbraAuthLdapStartTlsEnabled: 'FALSE' }));
-
-		setup(<DomainAuthentication />);
-		expect(screen.getByText('Verify Auth')).toBeInTheDocument();
-
-		const enableSecureConnection = screen.getByTestId('enable-secure-connection');
-		expect(
-			within(enableSecureConnection).getByTestId('icon: ToggleLeftOutline')
-		).toBeInTheDocument();
-	});
-
-	it('enable secure connection switch must be turned off when zimbraAuthLdapStartTlsEnabled is unset/missing in the domain', () => {
-		useAuthIsAdvanced.getState().setIsAdvanced(true);
-
-		useDomainStore.getState().setDomain(getDefaultEmptyDomain());
-
-		setup(<DomainAuthentication />);
-		expect(screen.getByText('Verify Auth')).toBeInTheDocument();
-
-		const enableSecureConnection = screen.getByTestId('enable-secure-connection');
-		expect(
-			within(enableSecureConnection).getByTestId('icon: ToggleLeftOutline')
-		).toBeInTheDocument();
+				const enableSecureConnection = screen.getByTestId('enable-secure-connection');
+				expect(within(enableSecureConnection).getByTestId(expectedIcon)).toBeInTheDocument();
+			}
+		);
 	});
 });
