ac7f326fb8b7d9bf641661130cb66c72664afc98
Lisa Parma <lisa.parma@zextras.com>
2025-02-21 16:47:02 +0100

    chore: add translation keys

--- src/views/cos/wscSettings/wsc-settings.tsx
+++ src/views/cos/wscSettings/wsc-settings.tsx
@@ -74,22 +74,35 @@ export const WscSettings: FC<{
 		[featuresDetail?.carbonioFeatureChatsEnabled, readonlyFeatures]
 	);
 
+	const durationOptions = useMemo(() => {
+		const timeLimitLabel = t('wsc.section.content.select.timeLimit', 'minute time limit');
+		return [
+			{ value: '0m', label: t('wsc.section.content.select.never', 'Never') },
+			{ value: '5m', label: `5 ${timeLimitLabel}` },
+			{ value: '10m', label: `10 ${timeLimitLabel}` },
+			{ value: '30m', label: `30 ${timeLimitLabel}` }
+		];
+	}, [t]);
+
 	return (
-		<Container height="fit" gap="2rem">
+		<Container height="fit" gap="2rem" style={{ userSelect: 'none' }}>
 			<BoxLayout
-				title={t('', 'General Settings')}
-				description={t('', 'Manage the activation of core features and applications.')}
+				title={t('wsc.section.header.title', 'General Settings')}
+				description={t(
+					'wsc.section.header.description',
+					'Manage the activation of core features and applications.'
+				)}
 			>
 				<SettingLayout
 					description={t(
-						'',
+						'wsc.section.content.description.enableFeature',
 						'Activate to allow messaging, group chats, video calls, and file sharing.'
 					)}
 				>
 					<InheritedSwitch
 						subValue={featuresDetail?.carbonioFeatureChatsEnabled}
 						onChange={changeSwitchOption}
-						label={t('', 'Enable Workstream Collaboration')}
+						label={t('wsc.section.content.toggle.enableFeature', 'Enable Workstream Collaboration')}
 						iconColor="primary"
 						inheritedValue={cosDetail?.carbonioFeatureChatsEnabled}
 						fromSubValue={accSpecificDetail?.carbonioFeatureChatsEnabled}
@@ -101,14 +114,22 @@ export const WscSettings: FC<{
 			<Container orientation="horizontal" height="fit" gap="4rem">
 				<Container mainAlignment="flex-start" width="calc(50% - 2rem)">
 					<BoxLayout
-						title={t('', 'Messaging & Presence')}
-						description={t('', 'Manage the visibility of chats and user status.')}
+						title={t('wsc.section.header.title.messaging', 'Messaging & Presence')}
+						description={t(
+							'wsc.section.header.description.messaging',
+							'Manage the visibility of chats and user status.'
+						)}
 					>
-						<SettingLayout description={t('', 'Show senders when their messages have been read.')}>
+						<SettingLayout
+							description={t(
+								'wsc.section.content.description.readReceipt',
+								'Show senders when their messages have been read.'
+							)}
+						>
 							<InheritedSwitch
 								subValue={featuresDetail?.carbonioWscShowMessageReads}
 								onChange={changeSwitchOption}
-								label={t('', 'Show read receipts')}
+								label={t('wsc.section.content.toggle.readReceipt', 'Show read receipts')}
 								iconColor="primary"
 								inheritedValue={cosDetail?.carbonioWscShowMessageReads}
 								fromSubValue={accSpecificDetail?.carbonioWscShowMessageReads}
@@ -117,11 +138,16 @@ export const WscSettings: FC<{
 								disabled={disableWscSettings}
 							/>
 						</SettingLayout>
-						<SettingLayout description={t('', "Display users' online status or last seen time.")}>
+						<SettingLayout
+							description={t(
+								'wsc.section.content.description.presence',
+								"Display users' online status or last seen time."
+							)}
+						>
 							<InheritedSwitch
 								subValue={featuresDetail?.carbonioWscShowUsersPresence}
 								onChange={changeSwitchOption}
-								label={t('', "Show users' online status")}
+								label={t('wsc.section.content.toggle.presence', "Show users' online status")}
 								iconColor="primary"
 								inheritedValue={cosDetail?.carbonioWscShowUsersPresence}
 								fromSubValue={accSpecificDetail?.carbonioWscShowUsersPresence}
@@ -130,15 +156,15 @@ export const WscSettings: FC<{
 								disabled={disableWscSettings}
 							/>
 						</SettingLayout>
-						<SettingLayout description={t('', 'Set the time limit for deleting a sent message.')}>
+						<SettingLayout
+							description={t(
+								'wsc.section.content.description.deletionLimit',
+								'Set the time limit for deleting a sent message.'
+							)}
+						>
 							<InheritedSelect
-								label={t('', 'Message deletion time limit')}
-								items={[
-									{ value: '0', label: t('', 'Never') },
-									{ value: '5', label: t('', '5 minute time limit') },
-									{ value: '10', label: t('', '10 minute time limit') },
-									{ value: '30', label: t('', '30 minute time limit') }
-								]}
+								label={t('wsc.section.content.select.deletionLimit', 'Message deletion time limit')}
+								items={durationOptions}
 								subValue={featuresDetail?.carbonioWscMessageDeleteTimeLimit}
 								inheritedValue={cosDetail?.carbonioWscMessageDeleteTimeLimit}
 								fromSubValue={accSpecificDetail?.carbonioWscMessageDeleteTimeLimit}
@@ -151,17 +177,15 @@ export const WscSettings: FC<{
 							<Padding top="small" />
 						</SettingLayout>
 						<SettingLayout
-							description={t('', 'Define how long a message can be edited after sending.')}
+							description={t(
+								'wsc.section.content.description.editLimit',
+								'Define how long a message can be edited after sending.'
+							)}
 							descriptionGap
 						>
 							<InheritedSelect
-								label={t('', 'Message editing time limit')}
-								items={[
-									{ value: '0', label: t('', 'Never') },
-									{ value: '5', label: t('', '5 minutes time limit') },
-									{ value: '10', label: t('', '10 minutes time limit') },
-									{ value: '30', label: t('', '30 minutes time limit') }
-								]}
+								label={t('wsc.section.content.select.editLimit', 'Message editing time limit')}
+								items={durationOptions}
 								subValue={featuresDetail?.carbonioWscMessageEditTimeLimit}
 								inheritedValue={cosDetail?.carbonioWscMessageEditTimeLimit}
 								fromSubValue={accSpecificDetail?.carbonioWscMessageEditTimeLimit}
@@ -176,14 +200,25 @@ export const WscSettings: FC<{
 				</Container>
 				<Container mainAlignment="flex-start" width="calc(50% - 2rem)">
 					<BoxLayout
-						title={t('', 'Private and Group Chats')}
-						description={t('', 'Manage Chats creation and Group settings.')}
+						title={t('wsc.section.header.title.chats', 'Private and Group Chats')}
+						description={t(
+							'wsc.section.header.description.chats',
+							'Manage Chats creation and Group settings.'
+						)}
 					>
-						<SettingLayout description={t('', 'Allow users to initiate one-on-one conversations.')}>
+						<SettingLayout
+							description={t(
+								'wsc.section.content.description.createPrivate',
+								'Allow users to initiate one-on-one conversations.'
+							)}
+						>
 							<InheritedSwitch
 								subValue={featuresDetail?.carbonioWscPrivateChatCreation}
 								onChange={changeSwitchOption}
-								label={t('', 'Users can start new private chats')}
+								label={t(
+									'wsc.section.content.toggle.createPrivate',
+									'Users can start new private chats'
+								)}
 								iconColor="primary"
 								inheritedValue={cosDetail?.carbonioWscPrivateChatCreation}
 								fromSubValue={accSpecificDetail?.carbonioWscPrivateChatCreation}
@@ -193,12 +228,15 @@ export const WscSettings: FC<{
 							/>
 						</SettingLayout>
 						<SettingLayout
-							description={t('', 'Allow users to create chats with multiple participants.')}
+							description={t(
+								'wsc.section.content.description.createGroup',
+								'Allow users to create chats with multiple participants.'
+							)}
 						>
 							<InheritedSwitch
 								subValue={featuresDetail?.carbonioWscGroupChatCreation}
 								onChange={changeSwitchOption}
-								label={t('', 'Users can create group chats')}
+								label={t('wsc.section.content.toggle.createGroup', 'Users can create group chats')}
 								iconColor="primary"
 								inheritedValue={cosDetail?.carbonioWscGroupChatCreation}
 								fromSubValue={accSpecificDetail?.carbonioWscGroupChatCreation}
@@ -208,11 +246,17 @@ export const WscSettings: FC<{
 							/>
 						</SettingLayout>
 						<SettingLayout
-							description={t('', 'Set the maximum number of users per group.')}
+							description={t(
+								'wsc.section.content.description.groupMembers',
+								'Set the maximum number of users per group.'
+							)}
 							descriptionGap
 						>
 							<InheritedInput
-								label={t('', 'Maximum number of group members')}
+								label={t(
+									'wsc.section.content.input.groupMembers',
+									'Maximum number of group members'
+								)}
 								subValue={featuresDetail?.carbonioWscMaxGroupMembers}
 								inheritedValue={cosDetail?.carbonioWscMaxGroupMembers}
 								fromSubValue={accSpecificDetail?.carbonioWscMaxGroupMembers}
@@ -224,11 +268,17 @@ export const WscSettings: FC<{
 							/>
 						</SettingLayout>
 						<SettingLayout
-							description={t('', 'Limit the file size for group profile pictures.')}
+							description={t(
+								'wsc.section.content.description.groupPicture',
+								'Limit the file size for group profile pictures.'
+							)}
 							descriptionGap
 						>
 							<InheritedInput
-								label={t('', 'Maximum group picture size in Kb')}
+								label={t(
+									'wsc.section.content.input.groupPicture',
+									'Maximum group picture size in Kb'
+								)}
 								subValue={featuresDetail?.carbonioWscMaxRoomPictureSize}
 								inheritedValue={cosDetail?.carbonioWscMaxRoomPictureSize}
 								fromSubValue={accSpecificDetail?.carbonioWscMaxRoomPictureSize}
@@ -245,14 +295,22 @@ export const WscSettings: FC<{
 			<Container orientation="horizontal" height="fit" gap="4rem">
 				<Container mainAlignment="flex-start" width="calc(50% - 2rem)">
 					<BoxLayout
-						title={t('', 'Calls & Video')}
-						description={t('', 'Configure video calls, recording, and virtual backgrounds.')}
+						title={t('wsc.section.header.title.videocall', 'Calls & Video')}
+						description={t(
+							'wsc.section.header.description.videocall',
+							'Configure video calls, recording, and virtual backgrounds.'
+						)}
 					>
-						<SettingLayout description={t('', 'Activate video call functionality in the feature.')}>
+						<SettingLayout
+							description={t(
+								'wsc.section.content.description.enableVideoCall',
+								'Activate video call functionality in the feature.'
+							)}
+						>
 							<InheritedSwitch
 								subValue={featuresDetail?.carbonioWscVideoCallEnabled}
 								onChange={changeSwitchOption}
-								label={t('', 'Enable video calls')}
+								label={t('wsc.section.content.toggle.enableVideoCall', 'Enable video calls')}
 								iconColor="primary"
 								inheritedValue={cosDetail?.carbonioWscVideoCallEnabled}
 								fromSubValue={accSpecificDetail?.carbonioWscVideoCallEnabled}
@@ -262,11 +320,16 @@ export const WscSettings: FC<{
 							/>
 						</SettingLayout>
 						{isAdvanced && (
-							<SettingLayout description={t('', 'Permit recording of audio and video calls.')}>
+							<SettingLayout
+								description={t(
+									'wsc.section.content.description.enableRecording',
+									'Permit recording of audio and video calls.'
+								)}
+							>
 								<InheritedSwitch
 									subValue={featuresDetail?.carbonioWscRecordingEnabled}
 									onChange={changeSwitchOption}
-									label={t('', 'Allow call recording')}
+									label={t('wsc.section.content.toggle.enableRecording', 'Allow call recording')}
 									iconColor="primary"
 									inheritedValue={cosDetail?.carbonioWscRecordingEnabled}
 									fromSubValue={accSpecificDetail?.carbonioWscRecordingEnabled}
@@ -279,12 +342,18 @@ export const WscSettings: FC<{
 							</SettingLayout>
 						)}
 						<SettingLayout
-							description={t('', 'Allow users to apply virtual backgrounds during video calls.')}
+							description={t(
+								'wsc.section.content.description.enableVirtualBackground',
+								'Allow users to apply virtual backgrounds during video calls.'
+							)}
 						>
 							<InheritedSwitch
 								subValue={featuresDetail?.carbonioWscVirtualBackgroundEnabled}
 								onChange={changeSwitchOption}
-								label={t('', 'Enable virtual background')}
+								label={t(
+									'wsc.section.content.toggle.enableVirtualBackground',
+									'Enable virtual background'
+								)}
 								iconColor="primary"
 								inheritedValue={cosDetail?.carbonioWscVirtualBackgroundEnabled}
 								fromSubValue={accSpecificDetail?.carbonioWscVirtualBackgroundEnabled}
@@ -299,16 +368,25 @@ export const WscSettings: FC<{
 				</Container>
 				<Container mainAlignment="flex-start" width="calc(50% - 2rem)">
 					<BoxLayout
-						title={t('', 'Sharing & Attachments')}
-						description={t('', 'Manage file sharing and attachment limits.')}
+						title={t('wsc.section.header.title.attachments', 'Sharing & Attachments')}
+						description={t(
+							'wsc.section.header.description.attachments',
+							'Manage file sharing and attachment limits.'
+						)}
 					>
 						<SettingLayout
-							description={t('', 'Enable the sending of files, images, and documents.')}
+							description={t(
+								'wsc.section.content.description.enableAttachments',
+								'Enable the sending of files, images, and documents.'
+							)}
 						>
 							<InheritedSwitch
 								subValue={featuresDetail?.carbonioWscAttachmentUpload}
 								onChange={changeSwitchOption}
-								label={t('', 'Users can upload attachments')}
+								label={t(
+									'wsc.section.content.toggle.enableAttachments',
+									'Users can upload attachments'
+								)}
 								iconColor="primary"
 								inheritedValue={cosDetail?.carbonioWscAttachmentUpload}
 								fromSubValue={accSpecificDetail?.carbonioWscAttachmentUpload}
@@ -318,11 +396,17 @@ export const WscSettings: FC<{
 							/>
 						</SettingLayout>
 						<SettingLayout
-							description={t('', 'Define the maximum size for shared files in chats.')}
+							description={t(
+								'wsc.section.content.description.attachmentSize',
+								'Define the maximum size for shared files in chats.'
+							)}
 							descriptionGap
 						>
 							<InheritedInput
-								label={t('', 'Maximum attachment size in Mb')}
+								label={t(
+									'wsc.section.content.input.attachmentSize',
+									'Maximum attachment size in Mb'
+								)}
 								subValue={featuresDetail?.carbonioWscMaxAttachmentSize}
 								inheritedValue={cosDetail?.carbonioWscMaxAttachmentSize}
 								fromSubValue={accSpecificDetail?.carbonioWscMaxAttachmentSize}
