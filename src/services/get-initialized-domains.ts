2aceae45d1bcee92a2f3c856debef30dfb815e90
drazen04 <hangtime23@hotmail.it>
2025-03-13 08:35:02 +0100

    chore: add type for input and response + use async/await

--- src/services/get-initialized-domains.ts
+++ src/services/get-initialized-domains.ts
@@ -4,15 +4,23 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 // TODO:
-//  	- improve the typing of the response
-//  	- improve the typing of the body
 //  	- improve error handling
-//  	- use async await
-export const getInitializedDomains = async (body: { [key: string]: any }): Promise<unknown> =>
-	fetch(`/service/extension/zextras_admin/admin/getInitializedDomains`, {
+
+type GetInitializedDomainsResponse = {
+	domain: [{ name: string; id: string }];
+	searchTotal: number;
+};
+
+export const getInitializedDomains = async (body: {
+	domainName: string;
+}): Promise<GetInitializedDomainsResponse> => {
+	const result = await fetch(`/service/extension/zextras_admin/admin/getInitializedDomains`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify(body)
-	}).then((r) => r.json());
+	});
+
+	return result.json();
+};
