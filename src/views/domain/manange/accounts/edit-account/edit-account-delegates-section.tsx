c785670edb8528ec30957140fa78a61495d7728a
Manuel Garbin <manuel.garbin@studiostorti.com>
2025-03-25 11:12:44 +0100

    fix: fixed add new delegate account on wizard mode

--- src/views/domain/manange/accounts/edit-account/edit-account-delegates-section.tsx
+++ src/views/domain/manange/accounts/edit-account/edit-account-delegates-section.tsx
@@ -401,7 +401,8 @@ const EditAccountDelegatesSection: FC = () => {
 		getIdentitiesList,
 		createSnackbar,
 		t,
-		folderList
+		folderList,
+		accountDetail
 	]);
 
 	const wizardSteps = useMemo(
