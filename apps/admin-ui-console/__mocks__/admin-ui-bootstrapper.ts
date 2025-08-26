06a879e437197fbf5b42a1da4f6c93c67baa0e2e
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-08-26 18:20:13 +0200

    test(admin-ui-bootstrapper): add mock module for testing
    Created a mock implementation of admin-ui-bootstrapper with jest.fn() for
    core exports. This supports unit testing by providing mock functions for
    getSoapFetchRequest, useDomainInformation, useUserSettings, and
    fetchExternalSoap.

---
+++ apps/admin-ui-console/__mocks__/admin-ui-bootstrapper.ts
@@ -0,0 +1,8 @@
+// __mocks__/@zextras/admin-ui-bootstrapper.ts
+export const getSoapFetchRequest = jest.fn();
+export const useDomainInformation = jest.fn();
+export const useUserSettings = jest.fn();
+export const fetchExternalSoap = jest.fn();
+
+// Add any other exports the module has
+export default {};
