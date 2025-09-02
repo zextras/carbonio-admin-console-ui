47720a6907d9712a159a2b9dd95cc646a6bc9cd0
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-09-02 07:15:28 +0200

    feat(composer): support locale-aware TinyMCE editor
    Added locale descriptors and dynamic language support for TinyMCE.
    Updated composer to handle inline image selection and user preferences.

--- apps/admin-ui-bootstrapper/src/globals.d.ts
+++ apps/admin-ui-bootstrapper/src/globals.d.ts
@@ -36,5 +36,4 @@ type cliSettingsNamespace = {
 declare const devUtils: devUtilsNamespace | undefined;
 declare const cliSettings: cliSettingsNamespace | undefined;
 declare module '@zextras/carbonio-design-system';
-declare module 'tinymce';
 declare module '*.svg';
--- apps/admin-ui-bootstrapper/src/store/integrations/composer.tsx
+++ apps/admin-ui-bootstrapper/src/store/integrations/composer.tsx
@@ -3,138 +3,277 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
 
+import type { IAllProps as EditorProps } from '@tinymce/tinymce-react';
 import { Editor } from '@tinymce/tinymce-react';
 import { Container } from '@zextras/carbonio-design-system';
+import styled from 'styled-components';
+import type { EditorOptions, TinyMCE, Ui } from 'tinymce/tinymce';
 // TinyMCE so the global var exists
-// eslint-disable-next-line no-unused-vars, import/order
 import tinymce from 'tinymce/tinymce';
-// this 'expression' prevents webpack from stripping it, maybe there's a better way
-tinymce;
+import 'tinymce/models/dom';
 // Theme
 import 'tinymce/themes/silver';
 // Toolbar icons
 import 'tinymce/icons/default';
 // Editor styles
 import 'tinymce/skins/ui/oxide/skin.min.css';
-
 // importing the plugin js.
 import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/anchor';
 import 'tinymce/plugins/autolink';
-import 'tinymce/plugins/lists';
-import 'tinymce/plugins/link';
-import 'tinymce/plugins/image';
+import 'tinymce/plugins/autoresize';
 import 'tinymce/plugins/charmap';
 import 'tinymce/plugins/code';
-// eslint-disable-next-line import/no-unresolved
-import 'tinymce/plugins/print';
-import 'tinymce/plugins/preview';
-import 'tinymce/plugins/anchor';
-import 'tinymce/plugins/searchreplace';
-import 'tinymce/plugins/visualblocks';
-import 'tinymce/plugins/fullscreen';
-import 'tinymce/plugins/insertdatetime';
-import 'tinymce/plugins/media';
-import 'tinymce/plugins/table';
-// eslint-disable-next-line import/no-unresolved
-import 'tinymce/plugins/paste';
-import 'tinymce/plugins/help';
-import 'tinymce/plugins/wordcount';
-import 'tinymce/plugins/quickbars';
 import 'tinymce/plugins/directionality';
-import 'tinymce/plugins/autoresize';
+import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/help';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/insertdatetime';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/preview';
+import 'tinymce/plugins/quickbars';
+import 'tinymce/plugins/searchreplace';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/visualblocks';
+import 'tinymce/plugins/wordcount';
 
-type ComposerProps = {
+import { useUserSettings } from '../account';
+import { getT } from '../i18n/hooks';
+import { useI18nStore } from '../i18n/store';
+import { SUPPORTED_LOCALES } from '../../constants';
+
+type ComposerProps = EditorProps & {
 	/** The callback invoked when an edit is performed into the editor. `([text, html]) => {}` */
 	onEditorChange?: (values: [string, string]) => void;
 	/** Enable the distraction-free mode */
 	inline?: boolean;
 	/** The initial content of the editor */
-	initialValue?: string;
+	initialValue?: EditorProps['initialValue'];
 	/** The content of the editor (controlled mode) */
-	value?: string;
-	/** The base url to append to the resource urls */
-	baseAssetsUrl?: string;
+	value?: EditorProps['value'];
+	/**
+	 * Callback called when user choose some file from the os.
+	 * If defined, a menu item to add inline images is added to the composer.
+	 */
+	onFileSelect?: (arg: { editor: TinyMCE; files: HTMLInputElement['files'] | undefined }) => void;
+	customInitOptions?: Partial<EditorProps['init']>;
 };
 
-const Composer: FC<ComposerProps> = ({
+export const FileInput = styled.input`
+	display: none;
+`;
+
+const Composer = ({
 	onEditorChange,
+	onFileSelect,
 	inline = false,
 	value,
-	baseAssetsUrl,
 	initialValue,
+	customInitOptions,
 	...rest
-}) => {
-	const [content, setContent] = useState(initialValue);
+}: ComposerProps): React.JSX.Element => {
+	const isControlledMode = useMemo(() => !!onEditorChange, [onEditorChange]);
 
-	const _onEditorChange = useCallback(
-		(newContent: string, editor: any) => {
-			setContent(newContent);
+	const _onEditorChange = useCallback<NonNullable<EditorProps['onEditorChange']>>(
+		(newContent, editor) => {
 			onEditorChange?.([
 				editor.getContent({ format: 'text' }),
 				editor.getContent({ format: 'html' })
 			]);
 		},
-		[setContent, onEditorChange]
+		[onEditorChange]
 	);
 
-	useEffect(() => setContent(value), [value]);
+	const { prefs } = useUserSettings();
+	const defaultStyle = useMemo(
+		() => ({
+			font: prefs?.zimbraPrefHtmlEditorDefaultFontFamily,
+			fontSize: prefs?.zimbraPrefHtmlEditorDefaultFontSize,
+			color: prefs?.zimbraPrefHtmlEditorDefaultFontColor
+		}),
+		[prefs]
+	);
+	const inputRef = useRef<HTMLInputElement>(null);
+	const onFileClick = useCallback(() => {
+		if (inputRef.current) {
+			inputRef.current.value = '';
+			inputRef.current.click();
+		}
+	}, []);
+	const t = getT();
+	const { locale } = useI18nStore.getState();
+	const language = useMemo(() => {
+		const localeObj =
+			locale in SUPPORTED_LOCALES && SUPPORTED_LOCALES[locale as keyof typeof SUPPORTED_LOCALES];
+		return (
+			(localeObj &&
+				(('tinymceLocale' in localeObj && localeObj?.tinymceLocale) || localeObj?.value)) ||
+			locale
+		);
+	}, [locale]);
+	const inlineLabel = useMemo(() => t('label.add_inline_image', 'Add inline image'), [t]);
+
+	const setupCallback = useCallback<NonNullable<EditorOptions['setup']>>(
+		(editor) => {
+			if (onFileSelect)
+				editor.ui.registry.addMenuButton('imageSelector', {
+					icon: 'gallery',
+					tooltip: t('label.select_image', 'Select image'),
+					fetch: (callback) => {
+						const items: Ui.Menu.MenuItemSpec[] = [
+							{
+								type: 'menuitem',
+								text: inlineLabel,
+								onAction: (): void => {
+									onFileClick();
+								}
+							}
+						];
+						callback(items);
+					}
+				});
+		},
+		[inlineLabel, onFileClick, onFileSelect, t]
+	);
+
+	const editorInitConfig = useMemo<EditorProps['init']>(
+		() => ({
+			content_css: `${BASE_PATH}/tinymce/skins/content/default/content.css`,
+			language_url: `${BASE_PATH}tinymce/langs/${language}.js`,
+			language,
+			setup: setupCallback,
+			min_height: 350,
+			auto_focus: true,
+			menubar: false,
+			statusbar: false,
+			branding: false,
+			resize: true,
+			inline,
+			font_size_formats:
+				'8pt 9pt 10pt 11pt 12pt 13pt 14pt 16pt 18pt 24pt 30pt 36pt 48pt 60pt 72pt 96pt',
+			object_resizing: 'img',
+			style_formats: [
+				{
+					title: 'Headers',
+					items: [
+						{ title: 'h1', block: 'h1' },
+						{ title: 'h2', block: 'h2' },
+						{ title: 'h3', block: 'h3' },
+						{ title: 'h4', block: 'h4' },
+						{ title: 'h5', block: 'h5' },
+						{ title: 'h6', block: 'h6' }
+					]
+				},
+				{
+					title: 'Blocks',
+					items: [
+						{ title: 'p', block: 'p' },
+						{ title: 'div', block: 'div' },
+						{ title: 'pre', block: 'pre' }
+					]
+				},
+				{
+					title: 'Containers',
+					items: [
+						{ title: 'section', block: 'section', wrapper: true, merge_siblings: false },
+						{ title: 'article', block: 'article', wrapper: true, merge_siblings: false },
+						{ title: 'blockquote', block: 'blockquote', wrapper: true },
+						{ title: 'hgroup', block: 'hgroup', wrapper: true },
+						{ title: 'aside', block: 'aside', wrapper: true },
+						{ title: 'figure', block: 'figure', wrapper: true }
+					]
+				}
+			],
+			plugins: [
+				'advlist',
+				'autolink',
+				'lists',
+				'link',
+				'image',
+				'charmap',
+				'preview',
+				'anchor',
+				'searchreplace',
+				'code',
+				'fullscreen',
+				'insertdatetime',
+				'media',
+				'table',
+				'code',
+				'help',
+				'quickbars',
+				'directionality',
+				'autoresize',
+				'visualblocks'
+			],
+			toolbar: inline
+				? false
+				: [
+						'fontfamily fontsize styles visualblocks',
+						'bold italic underline strikethrough',
+						'removeformat code',
+						'alignleft aligncenter alignright alignjustify',
+						'forecolor backcolor',
+						'bullist numlist outdent indent',
+						'ltr rtl',
+						'link',
+						'insertfile image',
+						'imageSelector'
+					].join(' | '),
+			quickbars_insert_toolbar: inline ? 'bullist numlist' : '',
+			quickbars_selection_toolbar: inline
+				? 'bold italic underline | forecolor backcolor | removeformat | link'
+				: 'link',
+			contextmenu: '',
+			toolbar_mode: 'wrap',
+			content_style: `body  {  color: ${defaultStyle?.color}; font-size: ${defaultStyle?.fontSize}; font-family: ${defaultStyle?.font}; }`,
+			visualblocks_default_state: false,
+			end_container_on_empty_block: true,
+			relative_urls: false,
+			remove_script_host: false,
+			newline_behavior: 'default',
+			browser_spellcheck: true,
+			convert_unsafe_embeds: true,
+			...customInitOptions
+		}),
+		[
+			language,
+			setupCallback,
+			inline,
+			defaultStyle?.color,
+			defaultStyle?.fontSize,
+			defaultStyle?.font,
+			customInitOptions
+		]
+	);
+
+	const fileInputOnChange = useCallback((): void => {
+		onFileSelect?.({ editor: tinymce, files: inputRef?.current?.files });
+	}, [onFileSelect]);
 
 	return (
 		<Container
 			height="100%"
 			crossAlignment="baseline"
-			mainAlignment="flex-start "
+			mainAlignment="flex-start"
 			style={{ overflowY: 'hidden' }}
 		>
+			<FileInput
+				type="file"
+				ref={inputRef}
+				accept="image/*"
+				onChange={fileInputOnChange}
+				multiple
+			/>
 			<Editor
-				value={content}
-				init={{
-					skin_url: `${baseAssetsUrl}/tinymce/skins/ui/oxide`,
-					content_css: `${baseAssetsUrl}/tinymce/skins/content/default/content.css`,
-					min_height: 350,
-					menubar: false,
-					statusbar: false,
-					branding: false,
-					resize: true,
-					inline,
-					plugins: [
-						'advlist',
-						'autolink',
-						'lists',
-						'link',
-						'image',
-						'charmap',
-						'print',
-						'preview',
-						'anchor',
-						'searchreplace',
-						'code',
-						'fullscreen',
-						'insertdatetime',
-						'media',
-						'table',
-						'paste',
-						'code',
-						'help',
-						'quickbars',
-						'directionality',
-						'autoresize'
-					],
-					toolbar: inline
-						? false
-						: // eslint-disable-next-line max-len
-						  'fontselect fontsizeselect formatselect | bold italic underline strikethrough | removeformat code | alignleft aligncenter alignright alignjustify | forecolor backcolor | bullist numlist outdent indent | ltr rtl',
-					quickbars_insert_toolbar: inline ? 'bullist numlist' : '',
-					quickbars_selection_toolbar: inline
-						? 'bold italic underline | forecolor backcolor | removeformat | quicklink'
-						: 'quicklink',
-					// eslint-disable-next-line sonarjs/no-all-duplicated-branches
-					contextmenu: inline ? '' : '',
-					toolbar_mode: 'wrap'
-				}}
-				onEditorChange={_onEditorChange}
+				initialValue={initialValue}
+				value={value}
+				init={editorInitConfig}
+				onEditorChange={isControlledMode ? _onEditorChange : undefined}
 				{...rest}
 			/>
 		</Container>
