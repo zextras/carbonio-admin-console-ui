c65221e8a2a8bb3f590fc2f71edb6ab398ef44a7
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-09-02 07:12:06 +0200

    chore(prettierrc): unify prettier config across project
    Standardized prettier configuration by adding a root .prettierrc.js and
    updating the admin-ui-bootstrapper config to match. Removed dependency on
    external config in admin-ui-bootstrapper.

--- apps/admin-ui-bootstrapper/.prettierrc.js
+++ apps/admin-ui-bootstrapper/.prettierrc.js
@@ -3,5 +3,14 @@
 // SPDX-License-Identifier: CC0-1.0
 
 module.exports = {
-	...require('@zextras/carbonio-ui-configs/rules/prettier')
-}
+	printWidth: 100,
+	arrowParens: 'always',
+	semi: true,
+	tabWidth: 2,
+	singleQuote: true,
+	trailingComma: 'none',
+	useTabs: true,
+	quoteProps: 'as-needed',
+	jsxSingleQuote: false,
+	bracketSpacing: true,
+};
