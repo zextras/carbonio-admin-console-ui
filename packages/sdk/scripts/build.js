fa5b439311fa0aa9c0eb8ad29c91fb67ef01e976
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-09-05 18:07:59 +0200

    chore(root): update lint scripts and remove preview test
    Update lint scripts in admin-ui-bootstrapper and admin-ui-console for
    consistency. Remove obsolete carbonio-ui-preview test file. Minor code
    style adjustments in sdk build script. Add SPDX header to backup config
    unit test.

--- packages/sdk/scripts/build.js
+++ packages/sdk/scripts/build.js
@@ -4,13 +4,12 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-/* eslint-disable no-console */
 const chalk = require('chalk');
 const webpack = require('webpack');
 const { commitHash } = require('./utils/setup');
 const { setupWebpackBuildConfig } = require('./configs/webpack.build.config');
 const { setupWebpackExternalBuildConfig } = require('./configs/webpack.external.config');
-const {logBuild, printArgs} = require('./utils/console');
+const { logBuild, printArgs } = require('./utils/console');
 const { rmSync } = require('node:fs');
 
 exports.command = 'build';
@@ -35,23 +34,24 @@ exports.builder = {
 	},
 	pkgRel: {
 		desc: 'pkgRel value to pass to the PKGBUILD template',
-		default: '1',
+		default: '1'
 	}
 };
 
-const runExternalBuild = (options, buildSetup) => new Promise((...p) => {
+const runExternalBuild = (options, buildSetup) =>
+	new Promise((...p) => {
 		console.log('Building ', chalk.bold.yellow('external '), chalk.green(options.name));
 		console.log('Using base path ', chalk.green(buildSetup.basePath));
 		const externalConfig = setupWebpackExternalBuildConfig(options, buildSetup);
 		const compilerExternal = webpack(externalConfig);
 		compilerExternal.run(logBuild(p, options));
-});
+	});
 
 exports.handler = async (options) =>
 	new Promise(async (...p) => {
 		printArgs(options, 'Build');
 		const basePath = `/static/iris/${options.name}/${commitHash}/`;
-		rmSync('dist', {recursive: true, force: true});
+		rmSync('dist', { recursive: true, force: true });
 		if (options.external) await runExternalBuild(options, { basePath, commitHash });
 		console.log('Building ', chalk.green(options.name));
 		console.log('Using base path ', chalk.green(basePath));
