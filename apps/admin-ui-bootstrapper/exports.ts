aeb2371323fbb19c3422354da43537fa959e0b86
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-09-19 16:55:54 +0200

    fix(apps/admin-ui-bootstrapper): update imports in exports
    Reorganize and correct import statements in exports.ts. Move specific
    imports from './types' to their respective source modules for clarity
    and accuracy.

--- apps/admin-ui-bootstrapper/exports.ts
+++ apps/admin-ui-bootstrapper/exports.ts
@@ -3,21 +3,19 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import { replaceHistory } from './src/history/hooks';
 import {
 	getSoapFetch,
 	getSoapFetchRequest as getSoapFetchRequestFn,
 	postSoapFetchRequest as postSoapFetchRequestFn,
 	fetchExternalSoap as fetchExternalSoapFn
 } from './src/network/fetch';
+import { usePrimaryBarState } from './src/shell/hooks';
 import { useUserAccount, useUserSettings } from './src/store/account/hooks';
+import { useAllConfig } from './src/store/config';
+import { useDomainInformation } from './src/store/domain-information';
 import { getIntegratedFunction } from './src/store/integrations/getters';
-import {
-	getTags,
-	replaceHistory,
-	usePrimaryBarState,
-	useAllConfig,
-	useDomainInformation
-} from './types';
+import { getTags } from './src/store/tags';
 
 const pkg = { name: 'admin-ui-console' };
 export const soapFetch = getSoapFetch('admin-ui-console');
