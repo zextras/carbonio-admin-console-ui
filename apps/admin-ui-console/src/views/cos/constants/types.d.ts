de8b139d99f30cc094bb67ea1e49efc49bef557e
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-10-07 20:07:15 +0200

    chore(admin-ui-console): remove COS module

--- apps/admin-ui-console/src/views/cos/constants/types.d.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
- *
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-import { SelectItem } from '@zextras/carbonio-design-system';
-
-export type AttributeValue = SelectItem | string | null;
