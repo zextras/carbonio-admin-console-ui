5f0e6ef76469d25018a9f49d049c3adabff2569c
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-10-10 13:59:26 +0200

    test[admin-ui-cos](cos-detail-panel.browser.test): add browser test for COS detail panel

---
+++ apps/admin-ui-cos/tests/browser/cos-detail-panel.browser.test.tsx
@@ -0,0 +1,43 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { page } from '@vitest/browser/context';
+import { createSoapAPIInterceptor, setupBrowserTest } from 'admin-ui-test-utils';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { it, expect, describe } from 'vitest';
+
+import { CosDetailPanel } from '../../src/views/cos/cos-detail-panel';
+
+const mockApiResponse = {
+	cos: [
+		{
+			name: 'firstCOS',
+			id: 'e00428a1-0c00-11d9-836a-000d93afea2a',
+			isDefaultCos: true
+		},
+		{
+			name: 'secondCOS',
+			id: 'f27456a8-0c00-11d9-280a-286d93afea2g',
+			isDefaultCos: true
+		}
+	],
+	searchTotal: 2,
+	more: false
+};
+describe('', () => {
+	it('should render all parts of the component', async () => {
+		createSoapAPIInterceptor('SearchDirectory', {});
+		setupBrowserTest(
+			<Route path="/cos">
+				<CosDetailPanel />
+			</Route>,
+			{ initialRouterEntry: '/cos/cos_list' }
+		);
+
+		await expect.element(page.getByText('COS List')).toBeVisible();
+	});
+});
