e85666385d0857d1b249b8bbbe100ed634894d52
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-10-27 08:40:05 +0100

    chore[apps/admin-ui-cos] (app): lazy load AppView with Suspense

--- apps/admin-ui-cos/src/app.tsx
+++ apps/admin-ui-cos/src/app.tsx
@@ -18,7 +18,7 @@ import {
 import { Icon, useSnackbar } from '@zextras/carbonio-design-system';
 import { find } from 'lodash';
 import moment from 'moment';
-import React, { FC, useCallback, useEffect, useMemo } from 'react';
+import React, { FC, lazy, Suspense, useCallback, useEffect, useMemo } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
@@ -52,13 +52,15 @@ import { useLastLoginTimestamp } from './store/last-login-time-stamp/store';
 import { useMailstoreListStore } from './store/mailstore-list/store';
 import { useRightsStore, Right, Rights } from './store/rights/store';
 import { TrackerProvider } from './tracker/provider';
-
-import AppViewComponent from './views/app-view';
+import { Spinner } from './views/components/spinner';
 import PrimaryBarTooltip from './views/primary-bar-tooltip/primary-bar-tooltip';
 
+const LazyAppView = lazy(() => import('./views/app-view'));
 const AppView: FC = (props) => (
 	<TrackerProvider>
-		<AppViewComponent {...props} />
+		<Suspense fallback={<Spinner />}>
+			<LazyAppView {...props} />
+		</Suspense>
 	</TrackerProvider>
 );
 const PrimaryBarIcon = styled(Icon)`
