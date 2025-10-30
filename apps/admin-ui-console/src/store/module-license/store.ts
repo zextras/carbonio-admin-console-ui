c0429be1265283bfde24e3a1c1f6fb3ccd63dda4
Lucrezia <67391513+yukiLu6@users.noreply.github.com>
2025-10-30 10:31:03 +0100

    fix: removed unused store sections

--- apps/admin-ui-console/src/store/module-license/store.ts
+++ apps/admin-ui-console/src/store/module-license/store.ts
@@ -7,17 +7,13 @@ import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
 type ModuleLicenseState = {
-	moduleLicense: Array<Record<string, string | number | boolean>>;
 	licenseInfo: Record<string, any> | null;
-	setModuleLicense: (v: Array<Record<string, string | number | boolean>>) => void;
 	setLicenseInfo: (v: Record<string, any>) => void;
 };
 
 export const useModuleLicenseStore = create<ModuleLicenseState>()(
 	devtools((set) => ({
-		moduleLicense: [],
 		licenseInfo: null,
-		setModuleLicense: (moduleLicense) => set({ moduleLicense }, false, 'setModuleLicense'),
 		setLicenseInfo: (licenseInfo) => set({ licenseInfo }, false, 'setLicenseInfo'),
 	}))
 );
