d9933aa7744af18927adedfea40397eeca638baa
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-11-27 06:32:57 +0100

    chore: remove unused files

--- apps/admin-ui-subscription/src/views/primary-bar-tooltip/primary-bar-tooltip.tsx
+++ apps/admin-ui-subscription/src/views/primary-bar-tooltip/primary-bar-tooltip.tsx
@@ -3,9 +3,8 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { FC } from 'react';
-
 import { Container, Text, Padding } from '@zextras/carbonio-design-system';
+import React, { FC } from 'react';
 
 const PrimaryBarTooltip: FC<{ items: any[] }> = ({ items }) => (
 	<Container
