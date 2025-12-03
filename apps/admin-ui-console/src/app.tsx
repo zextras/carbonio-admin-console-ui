e84db985e7f9b25ce728a29db0b9dca1013f2bbf
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-12-03 22:15:24 +0100

    chore(admin-ui-console): remove unused code

--- apps/admin-ui-console/src/app.tsx
+++ apps/admin-ui-console/src/app.tsx
@@ -6,36 +6,26 @@
 
 import {
 	addRoute,
-	removeRoute,
 	useAllConfig,
 	useIsAdvanced,
 	useMailstoreServers,
 	useGlobalConfigStore,
 	useAllServers,
 	useBucketServersListStore,
-	useGlobalSettings,
-	useHasAllRights
+	useGlobalSettings
 } from '@zextras/admin-ui-bootstrap';
-import { Button } from '@zextras/carbonio-design-system';
 import React, { FC, lazy, Suspense, useCallback, useEffect, useMemo } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { useHistory } from 'react-router-dom';
-import styled from 'styled-components';
 
 import {
-	BACKUP_ROUTE_ID,
 	CARBONIO_SEND_ANALYTICS,
 	LOG_AND_QUEUES,
-	MANAGE_APP_ID,
 	NOTIFICATION_ROUTE_ID,
 	OPERATIONS_ROUTE_ID,
-	PRIMARY_BAR_BACKUP,
 	PRIMARY_BAR_NOTIFICATIONS,
 	PRIMARY_BAR_OPERATIONS,
-	SERVICES_ROUTE_ID,
 	TRUE
 } from './constants';
-import SvgBackupOutline from './icons/outline/BackupOutline';
 import { TrackerProvider } from './tracker/provider';
 import { Spinner } from './views/components/spinner';
 import PrimaryBarTooltip from './views/primary-bar-tooltip/primary-bar-tooltip';
@@ -50,18 +40,8 @@ const AppView: FC = (props) => (
 	</TrackerProvider>
 );
 
-const PrimaryBarIconButton = styled(Button)`
-	&:hover {
-		background: transparent;
-	}
-	@media (max-width: 60rem) {
-		padding: 0 0 0 0.188rem;
-	}
-`;
-
 const App: FC = () => {
 	const [t] = useTranslation();
-	const history = useHistory();
 	const { data: serverList = [] } = useAllServers();
 	const { setGlobalConfig, setGlobalCarbonioSendAnalytics } = useGlobalConfigStore();
 	const { data: allConfig = [] } = useAllConfig();
@@ -70,7 +50,6 @@ const App: FC = () => {
 		enabled: isAdvanced
 	});
 	const { setAllServersList, setVolumeList } = useBucketServersListStore((state) => state);
-	const hasAllConfigRights = useHasAllRights();
 	const { data: mailstoreServers } = useMailstoreServers();
 
 	useEffect(() => {
@@ -88,23 +67,6 @@ const App: FC = () => {
 		}
 	}, [mailstoreServers, setVolumeList]);
 
-	const managementSection = useMemo(
-		() => ({
-			id: MANAGE_APP_ID,
-			label: t('label.management', 'Management'),
-			position: 3
-		}),
-		[t]
-	);
-	const servicesSection = useMemo(
-		() => ({
-			id: SERVICES_ROUTE_ID,
-			label: t('label.services', 'Services'),
-			position: 4
-		}),
-		[t]
-	);
-
 	const logAndQueuesSection = useMemo(
 		() => ({
 			id: LOG_AND_QUEUES,
@@ -112,37 +74,6 @@ const App: FC = () => {
 			position: 5
 		}),
 		[t]
-	);
-
-	const backupTooltipItems = useMemo(
-		() => [
-			{
-				header: (
-					<>
-						<Trans
-							i18nKey="label.backup_lbl"
-							defaults="<bold>Backup</bold>"
-							components={{ bold: <strong /> }}
-							t={t}
-						/>
-						{'\n\n'}
-						<Trans
-							i18nKey="label.backup_primarybar_tooltip"
-							defaults="Manage your <bold>backup services</bold>, view their <bold>status</bold>, the <bold>servers list</bold> or <bold>import an existing backup</bold>."
-							components={{ bold: <strong /> }}
-							t={t}
-						/>
-					</>
-				),
-				options: []
-			}
-		],
-		[t]
-	);
-
-	const BackupTooltipView: FC = useCallback(
-		() => <PrimaryBarTooltip items={backupTooltipItems} />,
-		[backupTooltipItems]
 	);
 
 	const notificationTooltipItems = useMemo(
@@ -207,42 +138,7 @@ const App: FC = () => {
 		[operationTooltipItem]
 	);
 
-	const backupPrimaryBar: FC = useCallback(
-		() => (
-			<PrimaryBarIconButton
-				// @ts-ignore // Need to fix it with custom soultion
-				icon={SvgBackupOutline}
-				type="ghost"
-				size={'extralarge'}
-				color={'text'}
-				onClick={(): void => history.push(`/${SERVICES_ROUTE_ID}/${BACKUP_ROUTE_ID}`)}
-			/>
-		),
-		[history]
-	);
-
-	const setConfigRightsRoute = useCallback(() => {
-		if (hasAllConfigRights) {
-			if (isAdvanced) {
-				addRoute({
-					route: BACKUP_ROUTE_ID,
-					position: 1,
-					visible: true,
-					label: t('label.backup', 'Backup') || '',
-					primaryBar: backupPrimaryBar,
-					appView: AppView,
-					primarybarSection: { ...servicesSection },
-					tooltip: BackupTooltipView,
-					trackerLabel: PRIMARY_BAR_BACKUP
-				});
-			}
-		} else {
-			removeRoute(BACKUP_ROUTE_ID);
-		}
-	}, [BackupTooltipView, backupPrimaryBar, hasAllConfigRights, isAdvanced, servicesSection, t]);
-
 	useEffect(() => {
-		setConfigRightsRoute();
 		if (isAdvanced) {
 			addRoute({
 				route: NOTIFICATION_ROUTE_ID,
@@ -267,17 +163,7 @@ const App: FC = () => {
 				trackerLabel: PRIMARY_BAR_OPERATIONS
 			});
 		}
-	}, [
-		NotificationTooltipView,
-		OperationTooltipView,
-		hasAllConfigRights,
-		isAdvanced,
-		logAndQueuesSection,
-		managementSection,
-		servicesSection,
-		setConfigRightsRoute,
-		t
-	]);
+	}, [NotificationTooltipView, OperationTooltipView, isAdvanced, logAndQueuesSection, t]);
 
 	// Handle server list changes
 	useEffect(() => {
--- apps/admin-ui-console/src/constants.ts
+++ apps/admin-ui-console/src/constants.ts
@@ -4,29 +4,12 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-export const ADVANCED = 'advanced';
-export const MANAGE_APP_ID = 'manage';
-export const BACKUP_ROUTE_ID = 'backup';
 export const FIRST_PAGE = 1;
 export const DASHBOARD = 'dashboard';
-export const SERVICES_ROUTE_ID = 'services';
-export const SERVER_CONFIG = 'server_config';
-export const IMPORT_EXTERNAL_BACKUP = 'import_an_external_backup';
-export const SERVERS_LIST = 'servers_list';
 export const TRUE = 'TRUE';
 export const LOG_AND_QUEUES = 'logandqueues';
 export const OPERATIONS_ROUTE_ID = 'operations';
 export const CARBONIO_SEND_ANALYTICS = 'carbonioSendAnalytics';
-export const CONFIGURATION_BACKUP = 'configuration_lbl';
-export const ADVANCED_LBL = 'advanced_lbl';
-export const SERVER = 'server';
-export const MANAGE_EXTERNAL_VOLUME = 'manage-external-volume';
-export const MOVE_TO_EXTERNAL_BUCKET = 'move-external-bucket';
-export const MOVE_TO_LOCAL_MOUNT_POINT = 'move-to-local-mountpoint';
-export const S3 = 'S3';
-export const LOCAL_VALUE = 'LOCAL';
-export const MOUNTPOINT = 'mountpoint';
-export const S3_BUCKET = 's3_bucket';
 export const NOTIFICATION_INFORMATION = 'Information';
 export const NOTIFICATION_WARNING = 'Warning';
 export const NOTIFICATION_ERROR = 'Error';
@@ -43,17 +26,9 @@ export const FALSE_OPERTION = 'False';
 export const PM = 'PM';
 export const AM = 'AM';
 export const DESC = 'desc';
-export const LIST_SERVER = 'listServer';
-export const CONFIG = 'config';
-export const BACKUP_BASIC = 'backup_basic';
-export const BACKUP_REALTIME = 'backup_realtime';
-export const IS_DEFAULT_SETTINGS_EXPANDED = 'isDefaultSettingsExpanded';
-export const IS_SERVER_SPECIFICS_EXPANDED = 'isServerSpecificsExpanded';
 export const FINISHED = 'Finished';
 export const EXCEPTION = 'Exception';
-export const PRIMARY_BAR_BACKUP = 'pb_backup';
 export const PRIMARY_BAR_NOTIFICATIONS = 'pb_notifications';
 export const PRIMARY_BAR_OPERATIONS = 'pb_operations';
-export const ZIMBRA_ADMIN_URN = 'urn:zimbraAdmin';
 export const PH_PROJECT_API_KEY = 'phc_fMgU1UPSHulWuJCHXbrjyqoEoXwcb7rZJy69HdD7x2h';
 export const PH_API_HOST = 'https://stats.zextras.tools';
