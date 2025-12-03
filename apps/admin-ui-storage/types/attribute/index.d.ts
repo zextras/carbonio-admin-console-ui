386f89e3d785d4e574974f95d28aa04639569750
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-12-03 10:47:34 +0100

    refactor[apps/admin-ui-storage](index): remove unused type exports
    Removed multiple unused type definition files and their exports from
    index.d.ts to simplify type management in admin-ui-storage.

--- apps/admin-ui-storage/types/attribute/index.d.ts
+++ apps/admin-ui-storage/types/attribute/index.d.ts
@@ -4,12 +4,4 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-export type Attribute = {
-	n: string;
-	_content: string;
-	c?: boolean;
-};
-
 export type objectType = { [key: string]: string };
-
-export type objAll = { [key: string]: string | boolean | number };
