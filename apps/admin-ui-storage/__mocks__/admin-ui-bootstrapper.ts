5fdb20f803cb40f299c2a3582e2d2bd83686d5ec
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-12-05 10:21:05 +0100

    chore: remove unused code

--- apps/admin-ui-storage/__mocks__/admin-ui-bootstrapper.ts
+++
@@ -1,7 +0,0 @@
-import { vi } from 'vitest';
-
-export const getSoapFetchRequest = vi.fn();
-export const useDomainInformation = vi.fn();
-export const useUserSettings = vi.fn();
-export const fetchExternalSoap = vi.fn();
-export const postSoapFetchRequest = vi.fn();
