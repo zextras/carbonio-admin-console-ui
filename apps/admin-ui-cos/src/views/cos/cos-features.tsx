80c149bcca479adb66953907de13433664b47ece
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-12-16 20:30:22 +0100

    chore: update cos features label

--- apps/admin-ui-cos/src/views/cos/cos-features.tsx
+++ apps/admin-ui-cos/src/views/cos/cos-features.tsx
@@ -3,28 +3,25 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import {
-  useCurrentUserRights,
-  useIsAdvanced,
-} from "@zextras/admin-ui-bootstrap";
-import { useSnackbar } from "@zextras/carbonio-design-system";
-import { find, isEqual, reduce } from "lodash-es";
-import { FC, useCallback, useEffect, useMemo, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { useCurrentUserRights, useIsAdvanced } from '@zextras/admin-ui-bootstrap';
+import { useSnackbar } from '@zextras/carbonio-design-system';
+import { find, isEqual, reduce } from 'lodash-es';
+import { FC, useCallback, useEffect, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import {
   COS,
   MOBILE_CALENDAR_FEATURE_SYNC,
   MOBILE_CONTACT_FEATURE_SYNC,
   ZIMBRA_ADMIN_URN,
-} from "../../constants";
-import { flushCache } from "../../services/flush-cache-service";
-import { getCoreAttributes } from "../../services/get-core-attributes";
-import { modifyCos, ModifyCosBody } from "../../services/modify-cos-service";
-import { setCoreAttributes } from "../../services/set-core-attributes";
-import { useCosStore } from "../../store/cos/store";
-import { PageLayout } from "../page-layout";
-import { Features } from "./features";
+} from '../../constants';
+import { flushCache } from '../../services/flush-cache-service';
+import { getCoreAttributes } from '../../services/get-core-attributes';
+import { modifyCos, ModifyCosBody } from '../../services/modify-cos-service';
+import { setCoreAttributes } from '../../services/set-core-attributes';
+import { useCosStore } from '../../store/cos/store';
+import { PageLayout } from '../page-layout';
+import { Features } from './features';
 
 const CosFeatures: FC = () => {
   const [t] = useTranslation();
@@ -33,7 +30,7 @@ const CosFeatures: FC = () => {
   const cosInformation = useCosStore((state) => state.cos?.a);
   const cosName = useCosStore((state) => state.cos?.name);
   const [initCosData, setInitCosData]: any = useState({});
-  const [zimbraId, setZimbraId] = useState<string>("");
+  const [zimbraId, setZimbraId] = useState<string>('');
   const setCos = useCosStore((state) => state.setCos);
   const [cosFeatures, setCosFeatures] = useState<any>({});
   const isAdvanced = useIsAdvanced();
@@ -63,36 +60,29 @@ const CosFeatures: FC = () => {
       {
         configType: COS,
         configName: [cosName],
-        attrName: ["mobileContactFeatureSync", "mobileCalendarFeatureSync"],
+        attrName: ['mobileContactFeatureSync', 'mobileCalendarFeatureSync'],
       },
     ];
     getCoreAttributes(body)
       .then((data) => {
         if (data?.attributes) {
           setSwitchOptionValue(
-            "mobileContactFeatureSync",
-            data?.attributes?.mobileContactFeatureSync[0]?.value === "enabled"
-              ? "TRUE"
-              : "FALSE",
+            'mobileContactFeatureSync',
+            data?.attributes?.mobileContactFeatureSync[0]?.value === 'enabled' ? 'TRUE' : 'FALSE',
           );
           setSwitchOptionValue(
-            "mobileCalendarFeatureSync",
-            data?.attributes?.mobileCalendarFeatureSync[0]?.value === "enabled"
-              ? "TRUE"
-              : "FALSE",
+            'mobileCalendarFeatureSync',
+            data?.attributes?.mobileCalendarFeatureSync[0]?.value === 'enabled' ? 'TRUE' : 'FALSE',
           );
         }
       })
       .catch((error) => {
         createSnackbar({
-          key: "error",
-          severity: "error",
+          key: 'error',
+          severity: 'error',
           label: error?.message
             ? error?.message
-            : t(
-                "label.something_wrong_error_msg",
-                "Something went wrong. Please try again.",
-              ),
+            : t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
           autoHideTimeout: 3000,
           hideButton: true,
           replace: true,
@@ -103,58 +93,22 @@ const CosFeatures: FC = () => {
   const setInitialValues = useCallback(
     (obj: any) => {
       if (obj) {
+        setSwitchOptionValue('carbonioFeatureMailsAppEnabled', obj?.carbonioFeatureMailsAppEnabled);
         setSwitchOptionValue(
-          "carbonioFeatureMailsAppEnabled",
-          obj?.carbonioFeatureMailsAppEnabled,
-        );
-        setSwitchOptionValue(
-          "zimbraFeatureOutOfOfficeReplyEnabled",
+          'zimbraFeatureOutOfOfficeReplyEnabled',
           obj?.zimbraFeatureOutOfOfficeReplyEnabled,
         );
-        setSwitchOptionValue(
-          "zimbraFeatureSignaturesEnabled",
-          obj?.zimbraFeatureSignaturesEnabled,
-        );
-        setSwitchOptionValue(
-          "zimbraFeatureMobileSyncEnabled",
-          obj?.zimbraFeatureMobileSyncEnabled,
-        );
-        setSwitchOptionValue(
-          "zimbraFeatureContactsEnabled",
-          obj?.zimbraFeatureContactsEnabled,
-        );
-        setSwitchOptionValue(
-          "zimbraFeatureCalendarEnabled",
-          obj?.zimbraFeatureCalendarEnabled,
-        );
-        setSwitchOptionValue(
-          "carbonioFeatureFilesAppEnabled",
-          obj?.carbonioFeatureFilesAppEnabled,
-        );
-        setSwitchOptionValue(
-          "carbonioFeatureFilesEnabled",
-          obj?.carbonioFeatureFilesEnabled,
-        );
-        setSwitchOptionValue(
-          "carbonioFeatureTeamEnabled",
-          obj?.carbonioFeatureTeamEnabled,
-        );
-        setSwitchOptionValue(
-          "carbonioFeatureChatsAppEnabled",
-          obj?.carbonioFeatureChatsAppEnabled,
-        );
-        setSwitchOptionValue(
-          "carbonioFeatureTasksEnabled",
-          obj?.carbonioFeatureTasksEnabled,
-        );
-        setSwitchOptionValue(
-          "zimbraFeatureOptionsEnabled",
-          obj?.zimbraFeatureOptionsEnabled,
-        );
-        setSwitchOptionValue(
-          "carbonioFeatureOTPMgmtEnabled",
-          obj?.carbonioFeatureOTPMgmtEnabled,
-        );
+        setSwitchOptionValue('zimbraFeatureSignaturesEnabled', obj?.zimbraFeatureSignaturesEnabled);
+        setSwitchOptionValue('zimbraFeatureMobileSyncEnabled', obj?.zimbraFeatureMobileSyncEnabled);
+        setSwitchOptionValue('zimbraFeatureContactsEnabled', obj?.zimbraFeatureContactsEnabled);
+        setSwitchOptionValue('zimbraFeatureCalendarEnabled', obj?.zimbraFeatureCalendarEnabled);
+        setSwitchOptionValue('carbonioFeatureFilesAppEnabled', obj?.carbonioFeatureFilesAppEnabled);
+        setSwitchOptionValue('carbonioFeatureFilesEnabled', obj?.carbonioFeatureFilesEnabled);
+        setSwitchOptionValue('carbonioFeatureTeamEnabled', obj?.carbonioFeatureTeamEnabled);
+        setSwitchOptionValue('carbonioFeatureChatsAppEnabled', obj?.carbonioFeatureChatsAppEnabled);
+        setSwitchOptionValue('carbonioFeatureTasksEnabled', obj?.carbonioFeatureTasksEnabled);
+        setSwitchOptionValue('zimbraFeatureOptionsEnabled', obj?.zimbraFeatureOptionsEnabled);
+        setSwitchOptionValue('carbonioFeatureOTPMgmtEnabled', obj?.carbonioFeatureOTPMgmtEnabled);
       }
     },
     [setSwitchOptionValue],
@@ -189,14 +143,11 @@ const CosFeatures: FC = () => {
   const modifyCosRequest = (body: ModifyCosBody): void => {
     modifyCos(body)
       .then((data) => {
-        flushCache("cos", "id", body.id._content);
+        flushCache('cos', 'id', body.id._content);
         createSnackbar({
-          key: "success",
-          severity: "success",
-          label: t(
-            "label.change_save_success_msg",
-            "The change has been saved successfully",
-          ),
+          key: 'success',
+          severity: 'success',
+          label: t('label.change_save_success_msg', 'The change has been saved successfully'),
           autoHideTimeout: 3000,
           hideButton: true,
           replace: true,
@@ -208,14 +159,11 @@ const CosFeatures: FC = () => {
       })
       .catch((error) => {
         createSnackbar({
-          key: "error",
-          severity: "error",
+          key: 'error',
+          severity: 'error',
           label: error?.message
             ? error?.message
-            : t(
-                "label.something_wrong_error_msg",
-                "Something went wrong. Please try again.",
-              ),
+            : t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
           autoHideTimeout: 3000,
           hideButton: true,
           replace: true,
@@ -226,25 +174,16 @@ const CosFeatures: FC = () => {
   const modifyCoreAttributes = (body: any): void => {
     setCoreAttributes(body)
       .then((data: any) => {
-        setSwitchOptionValue(
-          "mobileContactFeatureSync",
-          cosFeatures?.mobileContactFeatureSync,
-        );
-        setSwitchOptionValue(
-          "mobileCalendarFeatureSync",
-          cosFeatures?.mobileCalendarFeatureSync,
-        );
+        setSwitchOptionValue('mobileContactFeatureSync', cosFeatures?.mobileContactFeatureSync);
+        setSwitchOptionValue('mobileCalendarFeatureSync', cosFeatures?.mobileCalendarFeatureSync);
       })
       .catch((error) => {
         createSnackbar({
-          key: "error",
-          severity: "error",
+          key: 'error',
+          severity: 'error',
           label: error?.message
             ? error?.message
-            : t(
-                "label.something_wrong_error_msg",
-                "Something went wrong. Please try again.",
-              ),
+            : t('label.something_wrong_error_msg', 'Something went wrong. Please try again.'),
           autoHideTimeout: 3000,
           hideButton: true,
           replace: true,
@@ -260,17 +199,12 @@ const CosFeatures: FC = () => {
       },
     } as ModifyCosBody;
     body.a = Object.keys(cosFeatures)
-      .filter(
-        (ele) =>
-          ele !== MOBILE_CALENDAR_FEATURE_SYNC &&
-          ele !== MOBILE_CONTACT_FEATURE_SYNC,
-      )
+      .filter((ele) => ele !== MOBILE_CALENDAR_FEATURE_SYNC && ele !== MOBILE_CONTACT_FEATURE_SYNC)
       .map((ele) => ({ n: ele, _content: cosFeatures[ele] }));
 
     const modifiedKeys: any = reduce(
       cosFeatures,
-      (result, value, key): any =>
-        isEqual(value, initCosData[key]) ? result : [...result, key],
+      (result, value, key): any => (isEqual(value, initCosData[key]) ? result : [...result, key]),
       [],
     );
     if (
@@ -280,18 +214,12 @@ const CosFeatures: FC = () => {
     ) {
       const coreAttrBody: any = {
         mobileCalendarFeatureSync: {
-          value:
-            cosFeatures.mobileCalendarFeatureSync === "TRUE"
-              ? "enabled"
-              : "disabled",
+          value: cosFeatures.mobileCalendarFeatureSync === 'TRUE' ? 'enabled' : 'disabled',
           objectName: cosName,
           configType: COS,
         },
         mobileContactFeatureSync: {
-          value:
-            cosFeatures.mobileContactFeatureSync === "TRUE"
-              ? "enabled"
-              : "disabled",
+          value: cosFeatures.mobileContactFeatureSync === 'TRUE' ? 'enabled' : 'disabled',
           objectName: cosName,
           configType: COS,
         },
@@ -308,7 +236,7 @@ const CosFeatures: FC = () => {
 
   return (
     <PageLayout
-      title={t("cos.features", "Features")}
+      title={t('label.features', 'Features')}
       onSave={onSave}
       onCancel={onCancel}
       unSavedChanges={isDirty}
