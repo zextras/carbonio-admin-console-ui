947fa9e8a55516bbcd06716a5a39dcfb5c246cbe
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-01-14 18:06:51 +0100

    chore[packages/test-utils] (package): update dependencies and imports
    Add react and styled-components to dependencies. Update imports to use
    local ui-components in test-utils.

--- packages/test-utils/src/jsdom/utils/utils.tsx
+++ packages/test-utils/src/jsdom/utils/utils.tsx
@@ -5,16 +5,16 @@
  */
 
 import {
-	type ByRoleMatcher,
-	type ByRoleOptions,
-	type GetAllBy,
-	queries,
-	queryHelpers,
-	render as testingLibraryRender,
-	type RenderOptions,
-	type RenderResult,
-	screen,
-	within
+  type ByRoleMatcher,
+  type ByRoleOptions,
+  type GetAllBy,
+  queries,
+  queryHelpers,
+  render,
+  type RenderOptions,
+  type RenderResult,
+  screen,
+  within,
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { filter } from 'lodash-es';
@@ -23,140 +23,140 @@ import React, { type ReactElement } from 'react';
 import { Wrapper, WrapperProps } from './wrapper';
 
 export type UserEvent = ReturnType<(typeof userEvent)['setup']> & {
-	readonly rightClick: (target: Element) => Promise<void>;
+  readonly rightClick: (target: Element) => Promise<void>;
 };
 
 /**
  * Matcher function to search a string in more html elements and not just in a single element.
  */
 const queryAllByTextWithMarkup: GetAllBy<[string | RegExp]> = (container, text) =>
-	screen.queryAllByText((_content, element) => {
-		if (element && element instanceof HTMLElement) {
-			const hasText = (singleNode: Element): boolean => {
-				const regExp = RegExp(text);
-				return singleNode.textContent != null && regExp.test(singleNode.textContent);
-			};
-			const childrenDontHaveText = Array.from(element.children).every((child) => !hasText(child));
-			return hasText(element) && childrenDontHaveText;
-		}
-		return false;
-	});
+  screen.queryAllByText((_content, element) => {
+    if (element && element instanceof HTMLElement) {
+      const hasText = (singleNode: Element): boolean => {
+        const regExp = RegExp(text);
+        return singleNode.textContent != null && regExp.test(singleNode.textContent);
+      };
+      const childrenDontHaveText = Array.from(element.children).every((child) => !hasText(child));
+      return hasText(element) && childrenDontHaveText;
+    }
+    return false;
+  });
 
 const getByTextWithMarkupMultipleError = (
-	container: Element | null,
-	text: string | RegExp
+  container: Element | null,
+  text: string | RegExp,
 ): string => `Found multiple elements with text: ${text}`;
 const getByTextWithMarkupMissingError = (
-	container: Element | null,
-	text: string | RegExp
+  container: Element | null,
+  text: string | RegExp,
 ): string => `Unable to find an element with text: ${text}`;
 
 type ByRoleWithIconOptions = ByRoleOptions & {
-	icon: string | RegExp;
+  icon: string | RegExp;
 };
 /**
  * Matcher function to search an icon button through the icon data-testid
  */
 const queryAllByRoleWithIcon: GetAllBy<[ByRoleMatcher, ByRoleWithIconOptions]> = (
-	container,
-	role,
-	{ icon, ...options }
+  container,
+  role,
+  { icon, ...options },
 ) =>
-	filter(
-		screen.queryAllByRole('button', options),
-		(element) => within(element).queryByTestId(icon) !== null
-	);
+  filter(
+    screen.queryAllByRole('button', options),
+    (element) => within(element).queryByTestId(icon) !== null,
+  );
 const getByRoleWithIconMultipleError = (
-	container: Element | null,
-	role: ByRoleMatcher,
-	options: ByRoleWithIconOptions
+  container: Element | null,
+  role: ByRoleMatcher,
+  options: ByRoleWithIconOptions,
 ): string => `Found multiple elements with role ${role} and icon ${options.icon}`;
 const getByRoleWithIconMissingError = (
-	container: Element | null,
-	role: ByRoleMatcher,
-	options: ByRoleWithIconOptions
+  container: Element | null,
+  role: ByRoleMatcher,
+  options: ByRoleWithIconOptions,
 ): string => `Unable to find an element with role ${role} and icon ${options.icon}`;
 
 const [
-	queryByTextWithMarkup,
-	getAllByTextWithMarkup,
-	getByTextWithMarkup,
-	findAllByTextWithMarkup,
-	findByTextWithMarkup
+  queryByTextWithMarkup,
+  getAllByTextWithMarkup,
+  getByTextWithMarkup,
+  findAllByTextWithMarkup,
+  findByTextWithMarkup,
 ] = queryHelpers.buildQueries<[string | RegExp]>(
-	queryAllByTextWithMarkup,
-	getByTextWithMarkupMultipleError,
-	getByTextWithMarkupMissingError
+  queryAllByTextWithMarkup,
+  getByTextWithMarkupMultipleError,
+  getByTextWithMarkupMissingError,
 );
 
 const [
-	queryByRoleWithIcon,
-	getAllByRoleWithIcon,
-	getByRoleWithIcon,
-	findAllByRoleWithIcon,
-	findByRoleWithIcon
+  queryByRoleWithIcon,
+  getAllByRoleWithIcon,
+  getByRoleWithIcon,
+  findAllByRoleWithIcon,
+  findByRoleWithIcon,
 ] = queryHelpers.buildQueries<[ByRoleMatcher, ByRoleWithIconOptions]>(
-	queryAllByRoleWithIcon,
-	getByRoleWithIconMultipleError,
-	getByRoleWithIconMissingError
+  queryAllByRoleWithIcon,
+  getByRoleWithIconMultipleError,
+  getByRoleWithIconMissingError,
 );
 
 const customQueries = {
-	// byTextWithMarkup
-	queryByTextWithMarkup,
-	getAllByTextWithMarkup,
-	getByTextWithMarkup,
-	findAllByTextWithMarkup,
-	findByTextWithMarkup,
-	// byRoleWithIcon
-	queryByRoleWithIcon,
-	getAllByRoleWithIcon,
-	getByRoleWithIcon,
-	findAllByRoleWithIcon,
-	findByRoleWithIcon
+  // byTextWithMarkup
+  queryByTextWithMarkup,
+  getAllByTextWithMarkup,
+  getByTextWithMarkup,
+  findAllByTextWithMarkup,
+  findByTextWithMarkup,
+  // byRoleWithIcon
+  queryByRoleWithIcon,
+  getAllByRoleWithIcon,
+  getByRoleWithIcon,
+  findAllByRoleWithIcon,
+  findByRoleWithIcon,
 };
 
 function customRender(
-	ui: React.ReactElement,
-	{
-		initialRouterEntries = ['/'],
-		...options
-	}: WrapperProps & {
-		options?: Omit<RenderOptions, 'queries' | 'wrapper'>;
-	} = {}
+  ui: React.ReactElement,
+  {
+    initialRouterEntries = ['/'],
+    ...options
+  }: WrapperProps & {
+    options?: Omit<RenderOptions, 'queries' | 'wrapper'>;
+  } = {},
 ): RenderResult<typeof queries & typeof customQueries> {
-	return testingLibraryRender(ui, {
-		wrapper: ({ children }: Pick<WrapperProps, 'children'>) => (
-			<Wrapper initialRouterEntries={initialRouterEntries}>{children}</Wrapper>
-		),
-		queries: { ...queries, ...customQueries },
-		...options
-	});
+  return render(ui, {
+    wrapper: ({ children }: Pick<WrapperProps, 'children'>) => (
+      <Wrapper initialRouterEntries={initialRouterEntries}>{children}</Wrapper>
+    ),
+    queries: { ...queries, ...customQueries },
+    ...options,
+  });
 }
 
 type SetupOptions = Pick<WrapperProps, 'initialRouterEntries'> & {
-	renderOptions?: Omit<RenderOptions, 'queries'>;
-	setupOptions?: Parameters<(typeof userEvent)['setup']>[0];
+  renderOptions?: Omit<RenderOptions, 'queries'>;
+  setupOptions?: Parameters<(typeof userEvent)['setup']>[0];
 };
 
 const setupUserEvent = (options: SetupOptions['setupOptions']): UserEvent => {
-	const user = userEvent.setup(options);
-	const rightClick = (target: Element): Promise<void> =>
-		user.pointer({ target, keys: '[MouseRight]' });
+  const user = userEvent.setup(options);
+  const rightClick = (target: Element): Promise<void> =>
+    user.pointer({ target, keys: '[MouseRight]' });
 
-	return {
-		...user,
-		rightClick
-	};
+  return {
+    ...user,
+    rightClick,
+  };
 };
 
 export const setupTest = (
-	ui: ReactElement,
-	options?: SetupOptions
+  ui: ReactElement,
+  options?: SetupOptions,
 ): { user: UserEvent } & ReturnType<typeof customRender> => ({
-	user: setupUserEvent({ ...options?.setupOptions }),
-	...customRender(ui, {
-		initialRouterEntries: options?.initialRouterEntries,
-		...options?.renderOptions
-	})
+  user: setupUserEvent({ ...options?.setupOptions }),
+  ...customRender(ui, {
+    initialRouterEntries: options?.initialRouterEntries,
+    ...options?.renderOptions,
+  }),
 });
--- packages/test-utils/src/jsdom/utils/wrapper.tsx
+++ packages/test-utils/src/jsdom/utils/wrapper.tsx
@@ -4,52 +4,53 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { ModalManager, SnackbarManager, ThemeProvider } from '@zextras/ui-components';
 import i18next, { type i18n } from 'i18next';
 import React, { useMemo } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 
+import { ModalManager, SnackbarManager, ThemeProvider } from '../../../../ui-components/src/index';
+
 const getAppI18n = (): i18n => {
-	const newI18n = i18next.createInstance();
-	newI18n.init({
-		lng: 'en',
-		fallbackLng: 'en',
-		debug: false,
-		interpolation: {
-			escapeValue: false
-		},
-		resources: { en: { translation: {} } }
-	});
-	return newI18n;
+  const newI18n = i18next.createInstance();
+  newI18n.init({
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: { en: { translation: {} } },
+  });
+  return newI18n;
 };
 
 export type WrapperProps = {
-	children?: React.ReactNode;
-	initialRouterEntries?: string[];
+  children?: React.ReactNode;
+  initialRouterEntries?: string[];
 };
 
 export const I18NextTestProvider = ({
-	children
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }): React.JSX.Element => {
-	const i18nInstance = useMemo(() => getAppI18n(), []);
+  const i18nInstance = useMemo(() => getAppI18n(), []);
 
-	return <I18nextProvider i18n={i18nInstance}>{children}</I18nextProvider>;
+  return <I18nextProvider i18n={i18nInstance}>{children}</I18nextProvider>;
 };
 
 export const Wrapper = ({ initialRouterEntries, children }: WrapperProps): React.JSX.Element => (
-	<MemoryRouter
-		initialEntries={initialRouterEntries}
-		initialIndex={(initialRouterEntries?.length || 1) - 1}
-	>
-		<ThemeProvider>
-			<SnackbarManager>
-				<I18NextTestProvider>
-					<ModalManager>{children}</ModalManager>
-				</I18NextTestProvider>
-			</SnackbarManager>
-		</ThemeProvider>
-	</MemoryRouter>
+  <MemoryRouter
+    initialEntries={initialRouterEntries}
+    initialIndex={(initialRouterEntries?.length || 1) - 1}
+  >
+    <ThemeProvider>
+      <SnackbarManager>
+        <I18NextTestProvider>
+          <ModalManager>{children}</ModalManager>
+        </I18NextTestProvider>
+      </SnackbarManager>
+    </ThemeProvider>
+  </MemoryRouter>
 );
