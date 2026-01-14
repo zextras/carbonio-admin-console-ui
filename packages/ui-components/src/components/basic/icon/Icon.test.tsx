93edbd8207134a5ab4ddd034d1532dd143d9ea2c
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-01-14 18:08:57 +0100

    chore[packages/ui-components] (package): remove test-utils dependency
    Remove test-utils from dependencies in ui-components package.json.

--- packages/ui-components/src/components/basic/icon/Icon.test.tsx
+++ packages/ui-components/src/components/basic/icon/Icon.test.tsx
@@ -4,35 +4,34 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-
 import { screen } from '@testing-library/react';
+import { setupTest } from 'admin-ui-test-utils';
 
-import { setup } from '../../../test-utils';
 import { Icon } from './Icon';
 
 describe('Icon', () => {
-	test('Render an icon', () => {
-		setup(<Icon icon="BulbOutline" />);
-		expect(screen.getByTestId('icon: BulbOutline')).toBeVisible();
-	});
+  test('Render an icon', () => {
+    setupTest(<Icon icon="BulbOutline" />);
+    expect(screen.getByTestId('icon: BulbOutline')).toBeVisible();
+  });
 
-	test('Render an icon with a color of the palette with the variant', () => {
-		setup(<Icon icon="BulbOutline" color="primary.hover" />);
-		expect(screen.getByTestId('icon: BulbOutline')).toBeVisible();
-	});
+  test('Render an icon with a color of the palette with the variant', () => {
+    setupTest(<Icon icon="BulbOutline" color="primary.hover" />);
+    expect(screen.getByTestId('icon: BulbOutline')).toBeVisible();
+  });
 
-	test('Render an icon with a color not of the palette with the variant', () => {
-		setup(<Icon icon="BulbOutline" color="cadetblue.disabled" />);
-		expect(screen.getByTestId('icon: BulbOutline')).toBeVisible();
-	});
+  test('Render an icon with a color not of the palette with the variant', () => {
+    setupTest(<Icon icon="BulbOutline" color="cadetblue.disabled" />);
+    expect(screen.getByTestId('icon: BulbOutline')).toBeVisible();
+  });
 
-	test('Render an icon with a color in the rgb form and a variant', () => {
-		setup(<Icon icon="BulbOutline" color="rgba(100, 50, 50, 0.7).disabled" />);
-		expect(screen.getByTestId('icon: BulbOutline')).toBeVisible();
-	});
+  test('Render an icon with a color in the rgb form and a variant', () => {
+    setupTest(<Icon icon="BulbOutline" color="rgba(100, 50, 50, 0.7).disabled" />);
+    expect(screen.getByTestId('icon: BulbOutline')).toBeVisible();
+  });
 
-	test('Render an icon with a custom color', () => {
-		setup(<Icon icon="BulbOutline" color="rgba(100, 50, 50, 0.7)" />);
-		expect(screen.getByTestId('icon: BulbOutline')).toBeVisible();
-	});
+  test('Render an icon with a custom color', () => {
+    setupTest(<Icon icon="BulbOutline" color="rgba(100, 50, 50, 0.7)" />);
+    expect(screen.getByTestId('icon: BulbOutline')).toBeVisible();
+  });
 });
