3835a1c898e0b04e21412bc8659ffd954a4e1e0f
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-01-14 18:11:35 +0100

    test: fix some tests

--- packages/ui-components/src/components/basic/Link.test.tsx
+++ packages/ui-components/src/components/basic/Link.test.tsx
@@ -4,20 +4,19 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-
 import { screen } from '@testing-library/react';
+import { setupTest } from 'admin-ui-test-utils';
 
-import { setup } from '../../test-utils';
 import { Link } from './Link';
 
 describe('Link', () => {
-	test('Render a Link', () => {
-		const text = 'some content';
-		setup(
-			<Link weight="bold" size="large" color="warning" underlined href="https://test-link.test">
-				{text}
-			</Link>
-		);
-		expect(screen.getByRole('link', { name: text })).toBeVisible();
-	});
+  test('Render a Link', () => {
+    const text = 'some content';
+    setupTest(
+      <Link weight="bold" size="large" color="warning" underlined href="https://test-link.test">
+        {text}
+      </Link>,
+    );
+    expect(screen.getByRole('link', { name: text })).toBeVisible();
+  });
 });
--- packages/ui-components/src/components/display/List/List.test.tsx
+++ packages/ui-components/src/components/display/List/List.test.tsx
@@ -4,66 +4,66 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { screen } from '@testing-library/react';
+import { setupTest } from 'admin-ui-test-utils';
 import React from 'react';
 
-import { setup } from '../../../test-utils';
 import { Container } from '../../layout/Container';
 import { ListItem } from '../ListItem';
 import { List } from './List';
 
 describe('List', () => {
-	test('Render a basic list', () => {
-		const items = [
-			{
-				id: '1',
-				name: 'item 1'
-			},
-			{
-				id: '2',
-				name: 'item 2'
-			}
-		];
+  test('Render a basic list', () => {
+    const items = [
+      {
+        id: '1',
+        name: 'item 1',
+      },
+      {
+        id: '2',
+        name: 'item 2',
+      },
+    ];
 
-		const listItems = items.map((item) => (
-			<ListItem key={item.id}>{(): React.JSX.Element => <div>{item.name}</div>}</ListItem>
-		));
+    const listItems = items.map((item) => (
+      <ListItem key={item.id}>{(): React.JSX.Element => <div>{item.name}</div>}</ListItem>
+    ));
 
-		setup(<List>{listItems}</List>);
+    setupTest(<List>{listItems}</List>);
 
-		expect(screen.getByText('item 1')).toBeVisible();
-		expect(screen.getByText('item 2')).toBeVisible();
-	});
+    expect(screen.getByText('item 1')).toBeVisible();
+    expect(screen.getByText('item 2')).toBeVisible();
+  });
 
-	test('Render a list with a clickable item', async () => {
-		const items = [
-			{
-				id: '1',
-				name: 'item 1',
-				onClick: vi.fn()
-			},
-			{
-				id: '2',
-				name: 'item 2',
-				onClick: vi.fn()
-			}
-		];
+  test('Render a list with a clickable item', async () => {
+    const items = [
+      {
+        id: '1',
+        name: 'item 1',
+        onClick: vi.fn(),
+      },
+      {
+        id: '2',
+        name: 'item 2',
+        onClick: vi.fn(),
+      },
+    ];
 
-		const listItems = items.map((item) => (
-			<ListItem key={item.id}>
-				{(): React.JSX.Element => (
-					<Container key={item.id} onClick={item.onClick}>
-						{item.name}
-					</Container>
-				)}
-			</ListItem>
-		));
+    const listItems = items.map((item) => (
+      <ListItem key={item.id}>
+        {(): React.JSX.Element => (
+          <Container key={item.id} onClick={item.onClick}>
+            {item.name}
+          </Container>
+        )}
+      </ListItem>
+    ));
 
-		const { user } = setup(<List>{listItems}</List>);
+    const { user } = setupTest(<List>{listItems}</List>);
 
-		expect(screen.getByText('item 1')).toBeVisible();
-		expect(screen.getByText('item 2')).toBeVisible();
-		await user.click(screen.getByText('item 1'));
-		expect(items[0].onClick).toHaveBeenCalled();
-		expect(items[1].onClick).not.toHaveBeenCalled();
-	});
+    expect(screen.getByText('item 1')).toBeVisible();
+    expect(screen.getByText('item 2')).toBeVisible();
+    await user.click(screen.getByText('item 1'));
+    expect(items[0].onClick).toHaveBeenCalled();
+    expect(items[1].onClick).not.toHaveBeenCalled();
+  });
 });
