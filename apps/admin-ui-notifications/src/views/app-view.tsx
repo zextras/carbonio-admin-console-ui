4f34a80b12061703c4e78cf9eeb30ffb955ac79d
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-01-30 15:46:24 +0100

    chore[admin-ui-notifications](breadcrumb-view): remove styled-components
    Remove styled-components and related types and plugins from dependencies.
    Refactor breadcrumb and app view to use inline styles and functions instead
    of styled-components. Update global types for CSS modules support.

--- apps/admin-ui-notifications/src/views/app-view.tsx
+++ apps/admin-ui-notifications/src/views/app-view.tsx
@@ -4,24 +4,21 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { usePrimaryBarState } from '@zextras/admin-ui-bootstrap';
-import { Container, ContainerProps } from '@zextras/ui-components';
+import { Container } from '@zextras/ui-components';
 import { FC, Suspense } from 'react';
 import { Route } from 'react-router-dom';
-import styled from 'styled-components';
 
 import { LOG_AND_QUEUES, NOTIFICATION_ROUTE_ID } from '../constants';
 import BreadCrumb from './breadcrumb/breadcrumb-view';
 import NotificationsDetailPanel from './notifications/notifications-detail-panel';
 import NotificationsListPanel from './notifications/notifications-list-panel';
 
-interface ContainerExtendProps extends ContainerProps {
-  isPrimaryBarExpanded?: boolean;
+function getContainerStyle(isPrimaryBarExpanded: boolean) {
+  return {
+    maxWidth: isPrimaryBarExpanded ? '981px' : '1125px',
+    transition: 'width 300ms',
+  };
 }
-
-const DetailViewContainer = styled(Container)<ContainerExtendProps>`
-  max-width: ${({ isPrimaryBarExpanded }): number => (isPrimaryBarExpanded ? 981 : 1125)}px;
-  transition: width 300ms;
-`;
 
 const AppView: FC = () => {
   const isPrimaryBarExpanded = usePrimaryBarState();
@@ -36,11 +33,11 @@ const AppView: FC = () => {
             </Suspense>
           </Container>
           <Container style={{ maxWidth: '100%' }}>
-            <DetailViewContainer isPrimaryBarExpanded={isPrimaryBarExpanded}>
+            <Container style={getContainerStyle(isPrimaryBarExpanded)}>
               <Suspense fallback={<spinner-wc />}>
                 <NotificationsDetailPanel />
               </Suspense>
-            </DetailViewContainer>
+            </Container>
           </Container>
         </Container>
       </Route>
