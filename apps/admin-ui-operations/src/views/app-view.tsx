f1f429a40a9e1926560d4a87e1c731e58aaa4692
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-01-30 15:52:07 +0100

    chore([apps/admin-ui-operations] breadcrumb-view): remove styled-components
    Remove styled-components and related types from dependencies. Refactor
    breadcrumb and operations panels to use CSS modules and inline styles
    instead of styled-components. Update type declarations for CSS modules.

--- apps/admin-ui-operations/src/views/app-view.tsx
+++ apps/admin-ui-operations/src/views/app-view.tsx
@@ -4,48 +4,41 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { usePrimaryBarState } from '@zextras/admin-ui-bootstrap';
-import { Container, ContainerProps } from '@zextras/ui-components';
+import { Container } from '@zextras/ui-components';
 import { FC, Suspense } from 'react';
 import { Route } from 'react-router-dom';
-import styled from 'styled-components';
 
 import { LOG_AND_QUEUES, OPERATIONS_ROUTE_ID } from '../constants';
 import BreadCrumb from './breadcrumb/breadcrumb-view';
 import OperationsDetailPanel from './operations/operations-detail-panel';
 import OperationsListPanel from './operations/operations-list-panel';
 
-interface ContainerExtendProps extends ContainerProps {
-	isPrimaryBarExpanded?: boolean;
-}
-
-const DetailViewContainer = styled(Container)<ContainerExtendProps>`
-	max-width: ${({ isPrimaryBarExpanded }): number => (isPrimaryBarExpanded ? 981 : 1125)}px;
-	transition: width 300ms;
-`;
-
 const AppView: FC = () => {
-	const isPrimaryBarExpanded = usePrimaryBarState();
-	return (
-		<Container height={'fit'}>
-			<BreadCrumb />
-			<Route path={`/${LOG_AND_QUEUES}/${OPERATIONS_ROUTE_ID}`}>
-				<Container orientation="horizontal" mainAlignment="flex-start">
-					<Container style={{ maxWidth: '16.563rem' }}>
-						<Suspense fallback={<spinner-wc />}>
-							<OperationsListPanel />
-						</Suspense>
-					</Container>
-					<Container style={{ maxWidth: '100%' }}>
-						<DetailViewContainer isPrimaryBarExpanded={isPrimaryBarExpanded}>
-							<Suspense fallback={<spinner-wc />}>
-								<OperationsDetailPanel />
-							</Suspense>
-						</DetailViewContainer>
-					</Container>
-				</Container>
-			</Route>
-		</Container>
-	);
+  const isPrimaryBarExpanded = usePrimaryBarState();
+  const detailViewMaxWidth = isPrimaryBarExpanded ? 981 : 1125;
+  return (
+    <Container height={'fit'}>
+      <BreadCrumb />
+      <Route path={`/${LOG_AND_QUEUES}/${OPERATIONS_ROUTE_ID}`}>
+        <Container orientation="horizontal" mainAlignment="flex-start">
+          <Container style={{ maxWidth: '16.563rem' }}>
+            <Suspense fallback={<spinner-wc />}>
+              <OperationsListPanel />
+            </Suspense>
+          </Container>
+          <Container style={{ maxWidth: '100%' }}>
+            <Container
+              style={{ maxWidth: `${detailViewMaxWidth}px`, transition: 'max-width 300ms' }}
+            >
+              <Suspense fallback={<spinner-wc />}>
+                <OperationsDetailPanel />
+              </Suspense>
+            </Container>
+          </Container>
+        </Container>
+      </Route>
+    </Container>
+  );
 };
 
 export default AppView;
