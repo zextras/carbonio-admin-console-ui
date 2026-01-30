cd26cbcbdbb888cdcfbd98db097028f5e79d9286
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-01-30 15:20:30 +0100

    chore(apps/admin-ui-cos): remove inherited-input component
    Delete inherited-input component and its CSS module. Update wsc-settings
    to use InheritedInput from ui-components instead of local implementation.

--- apps/admin-ui-cos/src/wsc/wsc-settings.tsx
+++ apps/admin-ui-cos/src/wsc/wsc-settings.tsx
@@ -4,14 +4,13 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { useIsAdvanced, useLicenseInfo, useUserSettings } from '@zextras/admin-ui-bootstrap';
-import { Banner, Container, Padding } from '@zextras/ui-components';
+import { Banner, Container, InheritedInput, Padding } from '@zextras/ui-components';
 import { ChangeEvent, Dispatch, FC, SetStateAction, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { AccountType } from '../../types/account';
 import { TRUE } from '../constants';
 import { BoxLayout, SettingLayout } from '../views/page-layout';
-import InheritedInput from '../views/utility/inherited-components/inherited-input';
 import InheritedSelect from '../views/utility/inherited-components/inherited-select';
 import InheritedSwitch from '../views/utility/inherited-components/inherited-switch';
 
