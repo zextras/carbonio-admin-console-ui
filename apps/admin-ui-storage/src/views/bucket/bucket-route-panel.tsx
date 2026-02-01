1638360c9acf64b7b6ac9eed3088cd70fdc7fc51
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-02-01 07:41:12 +0100

    fix[admin-ui-storage](breadcrumb-view): migrate to react-router-dom v6
    Update breadcrumb-view and bucket-route-panel to use react-router-dom v6.
    Replace deprecated hooks and components. Remove @types/react-router-dom.

--- apps/admin-ui-storage/src/views/bucket/bucket-route-panel.tsx
+++ apps/admin-ui-storage/src/views/bucket/bucket-route-panel.tsx
@@ -5,29 +5,26 @@
  */
 import { Container } from '@zextras/ui-components';
 import { FC } from 'react';
-import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 import BucketOperation from './bucket-detail-operation';
 
 const BucketRoutePanel: FC = () => {
-	const { path } = useRouteMatch();
-	return (
-		<Container
-			orientation="column"
-			crossAlignment="center"
-			mainAlignment="flex-start"
-			style={{ overflowY: 'hidden' }}
-			background="gray6"
-		>
-			<Switch>
-				<Route exact path={`${path}/:operation`}>
-					<BucketOperation />
-				</Route>
-				<Route exact path={`${path}/:server/:operation`}>
-					<BucketOperation />
-				</Route>
-			</Switch>
-		</Container>
-	);
+  const location = useLocation();
+  const path = location.pathname;
+  return (
+    <Container
+      orientation="column"
+      crossAlignment="center"
+      mainAlignment="flex-start"
+      style={{ overflowY: 'hidden' }}
+      background="gray6"
+    >
+      <Routes>
+        <Route path={`${path}/:operation`} element={<BucketOperation />} />
+        <Route path={`${path}/:server/:operation`} element={<BucketOperation />} />
+      </Routes>
+    </Container>
+  );
 };
 export default BucketRoutePanel;
