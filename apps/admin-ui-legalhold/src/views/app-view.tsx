eed7279ab77b80315e3c61ab25bb0ab1af96dbc9
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-02-03 18:12:39 +0100

    fix[admin-ui-legalhold](app-view): update route to wildcard path
    - Change route path in AppView to use wildcard for legal hold panel
    - Remove unused constants for route path
    - Update container and styling indentation

--- apps/admin-ui-legalhold/src/views/app-view.tsx
+++ apps/admin-ui-legalhold/src/views/app-view.tsx
@@ -7,34 +7,33 @@ import { Container } from '@zextras/ui-components';
 import { FC, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { LEGAL_HOLD_ROUTE_ID, SERVICES_ROUTE_ID } from '../constants';
 import BreadCrumb from './breadcrumb/breadcrumb-view';
 import LegalHoldPanel from './legal-hold/legal-hold-panel';
 
 const AppView: FC = () => {
-	return (
-		<Container height={'fit'}>
-			<BreadCrumb />
-			<Routes>
-				<Route
-					path={`/${SERVICES_ROUTE_ID}/${LEGAL_HOLD_ROUTE_ID}`}
-					element={
-						<Container
-							orientation="horizontal"
-							mainAlignment="flex-start"
-							style={{ overflow: 'hidden' }}
-						>
-							<Container style={{ maxWidth: '100%' }}>
-								<Suspense fallback={<spinner-wc />}>
-									<LegalHoldPanel />
-								</Suspense>
-							</Container>
-						</Container>
-					}
-				/>
-			</Routes>
-		</Container>
-	);
+  return (
+    <Container height={'fit'}>
+      <BreadCrumb />
+      <Routes>
+        <Route
+          path={'/*'}
+          element={
+            <Container
+              orientation="horizontal"
+              mainAlignment="flex-start"
+              style={{ overflow: 'hidden' }}
+            >
+              <Container style={{ maxWidth: '100%' }}>
+                <Suspense fallback={<spinner-wc />}>
+                  <LegalHoldPanel />
+                </Suspense>
+              </Container>
+            </Container>
+          }
+        />
+      </Routes>
+    </Container>
+  );
 };
 
 export default AppView;
