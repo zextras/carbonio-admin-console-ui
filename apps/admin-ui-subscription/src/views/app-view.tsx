11e87eb0dbacbc16152b24d40e8058a354e7790a
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-02-03 18:04:31 +0100

    fix[admin-ui-subscription](app-view): update route path for subscription
    - Change route path in AppView to use root path for subscription
    - Remove unused constants for route path
    - Update DetailViewContainer styling indentation

--- apps/admin-ui-subscription/src/views/app-view.tsx
+++ apps/admin-ui-subscription/src/views/app-view.tsx
@@ -9,42 +9,41 @@ import { FC, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { MANAGE_APP_ID, SUBSCRIPTIONS_ROUTE_ID } from '../constants';
 import BreadCrumb from './breadcrumb/breadcrumb-view';
 import { Subscription } from './subscription/subscription';
 
 interface ContainerExtendProps extends ContainerProps {
-	isPrimaryBarExpanded?: boolean;
+  isPrimaryBarExpanded?: boolean;
 }
 
 const DetailViewContainer = styled(Container)<ContainerExtendProps>`
-	max-width: ${({ isPrimaryBarExpanded }): number => (isPrimaryBarExpanded ? 981 : 1125)}px;
-	transition: width 300ms;
+  max-width: ${({ isPrimaryBarExpanded }): number => (isPrimaryBarExpanded ? 981 : 1125)}px;
+  transition: width 300ms;
 `;
 
 const AppView: FC = () => {
-	const isPrimaryBarExpanded = usePrimaryBarState();
-	return (
-		<Container height={'fit'}>
-			<BreadCrumb />
-			<Routes>
-				<Route
-					path={`/${MANAGE_APP_ID}/${SUBSCRIPTIONS_ROUTE_ID}`}
-					element={
-						<Container orientation="horizontal" mainAlignment="flex-start">
-							<Container style={{ maxWidth: '100%' }}>
-								<DetailViewContainer isPrimaryBarExpanded={isPrimaryBarExpanded}>
-									<Suspense fallback={<spinner-wc />}>
-										<Subscription />
-									</Suspense>
-								</DetailViewContainer>
-							</Container>
-						</Container>
-					}
-				/>
-			</Routes>
-		</Container>
-	);
+  const isPrimaryBarExpanded = usePrimaryBarState();
+  return (
+    <Container height={'fit'}>
+      <BreadCrumb />
+      <Routes>
+        <Route
+          path={'/'}
+          element={
+            <Container orientation="horizontal" mainAlignment="flex-start">
+              <Container style={{ maxWidth: '100%' }}>
+                <DetailViewContainer isPrimaryBarExpanded={isPrimaryBarExpanded}>
+                  <Suspense fallback={<spinner-wc />}>
+                    <Subscription />
+                  </Suspense>
+                </DetailViewContainer>
+              </Container>
+            </Container>
+          }
+        />
+      </Routes>
+    </Container>
+  );
 };
 
 export default AppView;
