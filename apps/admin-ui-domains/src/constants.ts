bab218620daf3ecb61736ba9c5d9f2b3dc5c0b68
Luca Stauble <luca.stauble@gmail.com>
2026-02-13 17:28:15 +0100

    feat: add service for retrieving account total quota

--- apps/admin-ui-domains/src/constants.ts
+++ apps/admin-ui-domains/src/constants.ts
@@ -194,3 +194,8 @@ export const SYSTEM_ACCOUNT_FLAG = 'systemAccount';
 export const ADMIN_GROUP_FLAG = 'adminGroup';
 export const PRIMARY_COLOR_CODE_EX = 'ex. #225CA8';
 export const ZIMBRA_ADMIN_URN = 'urn:zimbraAdmin';
+
+// Storages API constants\
+export const STORAGES_API_BASE_URL = '/services/storages/admin';
+export const STORAGES_API_VERSION = '2';
+export const STORAGES_API_VERSION_HEADER = 'X-API-Version';
