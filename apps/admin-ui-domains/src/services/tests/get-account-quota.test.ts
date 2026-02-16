62ee011a1166914f0ded09977fa4cd604f41038f
Luca Stauble <luca.stauble@gmail.com>
2026-02-16 10:43:28 +0100

    test: fix test cases

--- apps/admin-ui-domains/src/services/tests/get-account-quota.test.ts
+++ apps/admin-ui-domains/src/services/tests/get-account-quota.test.ts
@@ -10,34 +10,7 @@ import { createAPIInterceptor } from '../../../../../packages/test-utils/src/ind
 import { getAccountQuota, GetAccountQuotaRawResponse } from '../get-account-quota';
 
 describe('getAccountQuota', () => {
-  it('should call the correct API endpoint with the correct version header', async () => {
-    const accountId = '12345';
-    const apiResponse: GetAccountQuotaRawResponse = {
-      total: {
-        used: 500000000,
-        computedLimit: 1000000000,
-      },
-      modules: {
-        mailbox: { used: 0 },
-        files: { used: 0 },
-        wsc: { used: 0 },
-      },
-    };
-
-    const apiInterceptor = createAPIInterceptor(
-      'get',
-      `/services/storages/admin/quota/accounts/${accountId}`,
-      () => {
-        return HttpResponse.json(apiResponse, { status: 200 });
-      },
-    );
-
-    await getAccountQuota(accountId);
-
-    expect(apiInterceptor.getCalledTimes()).toBe(1);
-  });
-
-  it('should return total quota for an account if the API request is successful', async () => {
+    it('should call the correct API endpoint with the correct version header', async () => {
     const accountId = '12345';
 
     const apiInterceptor = createAPIInterceptor(
@@ -54,7 +27,38 @@ describe('getAccountQuota', () => {
     expect(apiInterceptor.getLastRequest().headers.get('X-API-Version')).toBe('2');
   });
 
-  it('should return an error message if the API request fails', async () => {
+  it('should return total quota for an account if the API request is successful', async () => {
+    const accountId = '12345';
+    const apiResponse: GetAccountQuotaRawResponse = {
+      total: {
+        used: 500000000,
+        computedLimit: 1000000000,
+      },
+      modules: {
+        mailbox: { used: 0 },
+        files: { used: 0 },
+        wsc: { used: 0 },
+      },
+    };
+
+    createAPIInterceptor(
+      'get',
+      `/services/storages/admin/quota/accounts/${accountId}`,
+      () => {
+        return HttpResponse.json(apiResponse, { status: 200 });
+      },
+    );
+
+    const result = await getAccountQuota(accountId);
+
+    expect(result).toEqual({
+      type: 'success',
+      totalComputedLimit: apiResponse.total.computedLimit,
+      totalUsed: apiResponse.total.used,
+    });
+  });
+
+  it('should return an error if the API request fails', async () => {
     const accountId = '12345';
 
     const apiInterceptor = createAPIInterceptor(
