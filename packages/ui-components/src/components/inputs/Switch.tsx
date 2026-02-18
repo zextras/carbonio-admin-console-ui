f1d307ce9117e6d9e186e71d6df4bffbad1623d0
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-02-18 09:38:51 +0100

    fix[packages/ui-components] (Switch): replace Icon with icon-wc
    - Replace usage of Icon component with icon-wc web component in Switch
    - Update styled-components selectors to target icon-wc
    - Remove unused CustomIcon styled component

--- packages/ui-components/src/components/inputs/Switch.tsx
+++ packages/ui-components/src/components/inputs/Switch.tsx
@@ -10,131 +10,128 @@ import styled, { css, SimpleInterpolation } from 'styled-components';
 import { useCheckbox } from '../../hooks/useCheckbox';
 import { useCombinedRefs } from '../../hooks/useCombinedRefs';
 import { AnyColor, PaletteColor } from '../../types/utils';
-import { Icon } from '../basic/icon/Icon';
 import { Text } from '../basic/text/Text';
 import { Container, ContainerProps } from '../layout/Container';
 import { Padding } from '../layout/Padding';
 
 type SwitchSize = 'medium' | 'small';
 
-const CustomIcon = styled(Icon)``;
-
 const IconWrapper = styled.div<{
-	$disabled: boolean;
-	$iconColor: AnyColor;
+  $disabled: boolean;
+  $iconColor: AnyColor;
 }>`
-	position: relative;
-	display: flex;
-	align-items: center;
-	${({ theme, $disabled, $iconColor }): SimpleInterpolation =>
-		!$disabled &&
-		css`
-			&:focus {
-				outline: none;
-				> ${CustomIcon} {
-					color: ${theme.palette[$iconColor as keyof typeof theme.palette].focus};
-				}
-			}
-			&:hover {
-				outline: none;
-				> ${CustomIcon} {
-					color: ${theme.palette[$iconColor as keyof typeof theme.palette].hover};
-				}
-			}
-			&:active {
-				outline: none;
-				> ${CustomIcon} {
-					color: ${theme.palette[$iconColor as keyof typeof theme.palette].active};
-				}
-			}
-		`};
+  position: relative;
+  display: flex;
+  align-items: center;
+  ${({ theme, $disabled, $iconColor }): SimpleInterpolation =>
+    !$disabled &&
+    css`
+      &:focus {
+        outline: none;
+        > icon-wc {
+          color: ${theme.palette[$iconColor as keyof typeof theme.palette].focus};
+        }
+      }
+      &:hover {
+        outline: none;
+        > icon-wc {
+          color: ${theme.palette[$iconColor as keyof typeof theme.palette].hover};
+        }
+      }
+      &:active {
+        outline: none;
+        > icon-wc {
+          color: ${theme.palette[$iconColor as keyof typeof theme.palette].active};
+        }
+      }
+    `};
 `;
 
 const CustomText = styled(Text)`
-	user-select: none;
-	line-height: 1.5;
+  user-select: none;
+  line-height: 1.5;
 `;
 
 type SwitchProps = Omit<ContainerProps, 'onChange' | 'onClick'> & {
-	/** status of the Switch */
-	defaultChecked?: boolean;
-	/** Switch value */
-	value?: boolean;
-	/** Switch text */
-	label?: string;
-	/** Switch padding */
-	padding?: ContainerProps['padding'];
-	/** whether to disable the Switch or not */
-	disabled?: boolean;
-	/** click callback */
-	onClick?: (event: Event) => void;
-	/** change callback */
-	onChange?: (checked: boolean) => void;
-	/** available sizes */
-	size?: SwitchSize;
-	/** icon color */
-	iconColor?: PaletteColor;
-	ref?: React.Ref<HTMLDivElement>;
+  /** status of the Switch */
+  defaultChecked?: boolean;
+  /** Switch value */
+  value?: boolean;
+  /** Switch text */
+  label?: string;
+  /** Switch padding */
+  padding?: ContainerProps['padding'];
+  /** whether to disable the Switch or not */
+  disabled?: boolean;
+  /** click callback */
+  onClick?: (event: Event) => void;
+  /** change callback */
+  onChange?: (checked: boolean) => void;
+  /** available sizes */
+  size?: SwitchSize;
+  /** icon color */
+  iconColor?: PaletteColor;
+  ref?: React.Ref<HTMLDivElement>;
 };
 
 const Switch = ({
-	defaultChecked = false,
-	value,
-	label,
-	padding,
-	disabled = false,
-	onClick,
-	onChange,
-	size = 'medium',
-	iconColor = 'gray0',
-	ref,
-	...rest
+  defaultChecked = false,
+  value,
+  label,
+  padding,
+  disabled = false,
+  onClick,
+  onChange,
+  size = 'medium',
+  iconColor = 'gray0',
+  ref,
+  ...rest
 }: SwitchProps) => {
-	const innerRef = useRef<HTMLDivElement>(null);
-	const ckbRef = useCombinedRefs<HTMLDivElement>(ref, innerRef);
-	const checked = useCheckbox({
-		ref: ckbRef,
-		defaultChecked,
-		value,
-		disabled,
-		onClick,
-		onChange
-	});
+  const innerRef = useRef<HTMLDivElement>(null);
+  const ckbRef = useCombinedRefs<HTMLDivElement>(ref, innerRef);
+  const checked = useCheckbox({
+    ref: ckbRef,
+    defaultChecked,
+    value,
+    disabled,
+    onClick,
+    onChange,
+  });
 
-	return (
-		<Container
-			ref={ckbRef}
-			orientation="horizontal"
-			width="fit"
-			height="fit"
-			padding={padding}
-			style={{ cursor: disabled ? 'default' : 'pointer' }}
-			crossAlignment="center"
-			{...rest}
-		>
-			<IconWrapper $disabled={disabled} tabIndex={disabled ? -1 : 0} $iconColor={String(iconColor)}>
-				<Icon
-					icon={checked ? 'ToggleRight' : 'ToggleLeftOutline'}
-					size={size === 'medium' ? 'large' : 'medium'}
-					color={String(iconColor)}
-					disabled={disabled}
-				/>
-			</IconWrapper>
-			{label && (
-				<Padding left="small">
-					<CustomText
-						size={size === 'medium' ? 'medium' : 'small'}
-						weight="regular"
-						overflow="break-word"
-						color="gray0"
-						disabled={disabled}
-					>
-						{label}
-					</CustomText>
-				</Padding>
-			)}
-		</Container>
-	);
+  return (
+    <Container
+      ref={ckbRef}
+      orientation="horizontal"
+      width="fit"
+      height="fit"
+      padding={padding}
+      style={{ cursor: disabled ? 'default' : 'pointer' }}
+      crossAlignment="center"
+      {...rest}
+    >
+      <IconWrapper $disabled={disabled} tabIndex={disabled ? -1 : 0} $iconColor={String(iconColor)}>
+        <icon-wc
+          icon={checked ? 'ToggleRight' : 'ToggleLeftOutline'}
+          size={size === 'medium' ? 'large' : 'medium'}
+          color={String(iconColor)}
+          disabled={disabled || undefined}
+        />
+      </IconWrapper>
+      {label && (
+        <Padding left="small">
+          <CustomText
+            size={size === 'medium' ? 'medium' : 'small'}
+            weight="regular"
+            overflow="break-word"
+            color="gray0"
+            disabled={disabled}
+          >
+            {label}
+          </CustomText>
+        </Padding>
+      )}
+    </Container>
+  );
 };
 
 export { Switch };
