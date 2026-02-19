d30b3a105f767eb6c6d7fe736ad2c9c9e26a475d
Luca Stauble <luca.stauble@gmail.com>
2026-02-19 15:25:44 +0100

    Manage account total quota (#1074)

---
+++ apps/admin-ui-domains/src/services/get-account-quota.ts
@@ -0,0 +1,69 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import {
+  STORAGES_API_BASE_URL,
+  STORAGES_API_VERSION,
+  STORAGES_API_VERSION_HEADER,
+} from '../constants';
+
+export type GetAccountQuotaRawResponse = {
+  total: {
+    used: number;
+    computedLimit: number;
+  };
+  modules: {
+    mailbox: { used: number };
+    files: { used: number };
+    wsc: { used: number };
+  };
+};
+
+type GetAccountQuotaResponse =
+  | {
+      type: 'success';
+      totalComputedLimit: number;
+      totalUsed: number;
+    }
+  | {
+      type: 'error';
+      error: string;
+    };
+
+/**
+ * Returns the quota information for a specific account.
+ * @param accountId The ID of the account.
+ * @returns The quota information for the account.
+ */
+export const getAccountQuota = async (accountId: string): Promise<GetAccountQuotaResponse> => {
+  const url = `${STORAGES_API_BASE_URL}/quota/accounts/${accountId}`;
+  const headers = {
+    'Content-Type': 'application/json',
+    [STORAGES_API_VERSION_HEADER]: STORAGES_API_VERSION,
+  };
+
+  return fetch(url, { headers })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json() as Promise<GetAccountQuotaRawResponse>;
+    })
+    .then((data) => {
+      const { total } = data;
+      return {
+        type: 'success',
+        totalComputedLimit: total.computedLimit,
+        totalUsed: total.used,
+      } satisfies GetAccountQuotaResponse;
+    })
+    .catch((error) => {
+      return {
+        type: 'error',
+        error: error.message,
+      } satisfies GetAccountQuotaResponse;
+    });
+};
