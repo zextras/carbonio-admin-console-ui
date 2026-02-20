dd9c01518c1ca90314b9d3f5d8c45367efcff259
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-02-20 06:16:45 +0100

    chore[packages/test-utils] (utils): remove custom queries and cleanup
    Removed custom query helpers and related code from utils. Deleted
    test-utils.tsx from ui-components package.

--- packages/test-utils/src/jsdom/utils/utils.tsx
+++ packages/test-utils/src/jsdom/utils/utils.tsx
@@ -4,20 +4,8 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import {
-  type ByRoleMatcher,
-  type ByRoleOptions,
-  type GetAllBy,
-  queries,
-  queryHelpers,
-  render,
-  type RenderOptions,
-  type RenderResult,
-  screen,
-  within,
-} from '@testing-library/react';
+import { queries, render, type RenderOptions, type RenderResult } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { filter } from 'lodash-es';
 import React, { type ReactElement } from 'react';
 
 import { Wrapper, WrapperProps } from './wrapper';
@@ -33,96 +21,6 @@ export type UserEvent = ReturnType<(typeof userEvent)['setup']> & {
   readonly tab: (options?: { shift?: boolean }) => Promise<void>;
 };
 
-/**
- * Matcher function to search a string in more html elements and not just in a single element.
- */
-const queryAllByTextWithMarkup: GetAllBy<[string | RegExp]> = (container, text) =>
-  screen.queryAllByText((_content, element) => {
-    if (element && element instanceof HTMLElement) {
-      const hasText = (singleNode: Element): boolean => {
-        const regExp = RegExp(text);
-        return singleNode.textContent != null && regExp.test(singleNode.textContent);
-      };
-      const childrenDontHaveText = Array.from(element.children).every((child) => !hasText(child));
-      return hasText(element) && childrenDontHaveText;
-    }
-    return false;
-  });
-
-const getByTextWithMarkupMultipleError = (
-  container: Element | null,
-  text: string | RegExp,
-): string => `Found multiple elements with text: ${text}`;
-const getByTextWithMarkupMissingError = (
-  container: Element | null,
-  text: string | RegExp,
-): string => `Unable to find an element with text: ${text}`;
-
-type ByRoleWithIconOptions = ByRoleOptions & {
-  icon: string | RegExp;
-};
-/**
- * Matcher function to search an icon button through the icon data-testid
- */
-const queryAllByRoleWithIcon: GetAllBy<[ByRoleMatcher, ByRoleWithIconOptions]> = (
-  container,
-  role,
-  { icon, ...options },
-) =>
-  filter(
-    screen.queryAllByRole('button', options),
-    (element) => within(element).queryByTestId(icon) !== null,
-  );
-const getByRoleWithIconMultipleError = (
-  container: Element | null,
-  role: ByRoleMatcher,
-  options: ByRoleWithIconOptions,
-): string => `Found multiple elements with role ${role} and icon ${options.icon}`;
-const getByRoleWithIconMissingError = (
-  container: Element | null,
-  role: ByRoleMatcher,
-  options: ByRoleWithIconOptions,
-): string => `Unable to find an element with role ${role} and icon ${options.icon}`;
-
-const [
-  queryByTextWithMarkup,
-  getAllByTextWithMarkup,
-  getByTextWithMarkup,
-  findAllByTextWithMarkup,
-  findByTextWithMarkup,
-] = queryHelpers.buildQueries<[string | RegExp]>(
-  queryAllByTextWithMarkup,
-  getByTextWithMarkupMultipleError,
-  getByTextWithMarkupMissingError,
-);
-
-const [
-  queryByRoleWithIcon,
-  getAllByRoleWithIcon,
-  getByRoleWithIcon,
-  findAllByRoleWithIcon,
-  findByRoleWithIcon,
-] = queryHelpers.buildQueries<[ByRoleMatcher, ByRoleWithIconOptions]>(
-  queryAllByRoleWithIcon,
-  getByRoleWithIconMultipleError,
-  getByRoleWithIconMissingError,
-);
-
-const customQueries = {
-  // byTextWithMarkup
-  queryByTextWithMarkup,
-  getAllByTextWithMarkup,
-  getByTextWithMarkup,
-  findAllByTextWithMarkup,
-  findByTextWithMarkup,
-  // byRoleWithIcon
-  queryByRoleWithIcon,
-  getAllByRoleWithIcon,
-  getByRoleWithIcon,
-  findAllByRoleWithIcon,
-  findByRoleWithIcon,
-};
-
 function customRender(
   ui: React.ReactElement,
   {
@@ -131,12 +29,12 @@ function customRender(
   }: WrapperProps & {
     options?: Omit<RenderOptions, 'queries' | 'wrapper'>;
   } = {},
-): RenderResult<typeof queries & typeof customQueries> {
+): RenderResult<typeof queries> {
   return render(ui, {
     wrapper: ({ children }: Pick<WrapperProps, 'children'>) => (
       <Wrapper initialRouterEntries={initialRouterEntries}>{children}</Wrapper>
     ),
-    queries: { ...queries, ...customQueries },
+    queries: { ...queries },
     ...options,
   });
 }
