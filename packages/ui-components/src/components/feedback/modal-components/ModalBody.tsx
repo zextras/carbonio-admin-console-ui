70f41833c04a5981f486d30f142be01eac939c14
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-03-02 18:42:36 +0100

    feat(packages/ui-components): migrate ModalBody to CSS module
    Adds ModalBody.module.css and updates ModalBody to use CSS module
    styling, removing styled-components usage.

--- packages/ui-components/src/components/feedback/modal-components/ModalBody.tsx
+++ packages/ui-components/src/components/feedback/modal-components/ModalBody.tsx
@@ -3,10 +3,10 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import clsx from 'clsx';
 import React, { HTMLAttributes } from 'react';
-import styled, { css, SimpleInterpolation } from 'styled-components';
 
-import { With$Prefix } from '../../../types/utils';
+import styles from './ModalBody.module.css';
 
 type ModalBodyProps = HTMLAttributes<HTMLDivElement> & {
 	/** Max height of the body container */
@@ -16,41 +16,18 @@ type ModalBodyProps = HTMLAttributes<HTMLDivElement> & {
 	ref?: React.Ref<HTMLDivElement>;
 };
 
-const ModalBodyComponent = styled.div<With$Prefix<ModalBodyProps>>`
-	overflow-y: auto;
-	max-height: ${({ $maxHeight }): SimpleInterpolation => $maxHeight};
-	max-width: 100%;
-	box-sizing: border-box;
-	width: 100%;
-	padding-top: ${({ theme }): string => theme.sizes.padding.large};
-	padding-bottom: ${({ theme }): string => theme.sizes.padding.large};
-	${({ $centered }): SimpleInterpolation =>
-		$centered &&
-		css`
-			text-align: center;
-		`};
-	&::-webkit-scrollbar {
-		width: 0.5rem;
-	}
-
-	&::-webkit-scrollbar-track {
-		background-color: transparent;
-	}
-
-	&::-webkit-scrollbar-thumb {
-		background-color: ${({ theme }): string => theme.palette.gray3.regular};
-		border-radius: 0.25rem;
-	}
-	flex-grow: 1;
-`;
-
-const ModalBody = ({ maxHeight, centered, children, ref, ...rest }: ModalBodyProps) => {
+function ModalBody({ maxHeight, centered, children, ref, ...rest }: ModalBodyProps) {
 	return (
-		<ModalBodyComponent $centered={centered} $maxHeight={maxHeight} ref={ref} {...rest}>
+		<div
+			className={clsx(styles.modalBody, centered && styles.centered)}
+			style={{ maxHeight }}
+			ref={ref}
+			{...rest}
+		>
 			{children}
-		</ModalBodyComponent>
+		</div>
 	);
-};
+}
 
 export type { ModalBodyProps };
 export { ModalBody };
