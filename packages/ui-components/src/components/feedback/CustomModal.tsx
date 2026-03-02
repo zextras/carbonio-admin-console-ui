7bd12c65f9517fd61a563ff2b5164d535813710b
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-03-02 18:05:41 +0100

    fix(packages/ui-components): remove ThemeContext from CustomModal
    Removes ThemeContext usage from CustomModal and updates modal logic to
    use window.document directly.

--- packages/ui-components/src/components/feedback/CustomModal.tsx
+++ packages/ui-components/src/components/feedback/CustomModal.tsx
@@ -4,16 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import React, {
-	HTMLAttributes,
-	useCallback,
-	useContext,
-	useEffect,
-	useMemo,
-	useRef,
-	useState
-} from 'react';
-import { ThemeContext } from 'styled-components';
+import React, { HTMLAttributes, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 import { useCombinedRefs } from '../../hooks/useCombinedRefs';
 import { KeyboardPresetObj, useKeyboard } from '../../hooks/useKeyboard';
@@ -22,185 +13,176 @@ import { TIMERS } from '../constants';
 import { Portal } from '../utilities/Portal';
 import { Transition } from '../utilities/Transition';
 import {
-	getScrollbarSize,
-	isBodyOverflowing,
-	ModalContainer,
-	ModalContent,
-	ModalWrapper
+  getScrollbarSize,
+  isBodyOverflowing,
+  ModalContainer,
+  ModalContent,
+  ModalWrapper,
 } from './modal-components/ModalComponents';
 
 type BareModalProps = {
-	/** Modal background */
-	background?: AnyColor;
-	/** Modal size */
-	size?: 'extrasmall' | 'small' | 'medium' | 'large';
-	/** Boolean to show the modal */
-	open?: boolean;
-	/** Css property to handle the stack order of multiple modals */
-	zIndex?: number;
-	/** min height of the modal */
-	minHeight?: string;
-	/** max height of the modal */
-	maxHeight?: string;
-	/** Flag to disable the Portal implementation */
-	disablePortal?: boolean;
-	/** Content of the modal */
-	children?: React.ReactNode | React.ReactNode[];
-	/** Callback to close the Modal */
-	onClose?: (event: React.MouseEvent | KeyboardEvent) => void;
-	/**
-	 * The window where to insert the Portal's children.
-	 * The default value is 'windowObj' obtained from the ThemContext.
-	 * */
-	containerWindow?: Window;
-
-	ref?: React.Ref<HTMLDivElement>;
+  /** Modal background */
+  background?: AnyColor;
+  /** Modal size */
+  size?: 'extrasmall' | 'small' | 'medium' | 'large';
+  /** Boolean to show the modal */
+  open?: boolean;
+  /** Css property to handle the stack order of multiple modals */
+  zIndex?: number;
+  /** min height of the modal */
+  minHeight?: string;
+  /** max height of the modal */
+  maxHeight?: string;
+  /** Flag to disable the Portal implementation */
+  disablePortal?: boolean;
+  /** Content of the modal */
+  children?: React.ReactNode | React.ReactNode[];
+  /** Callback to close the Modal */
+  onClose?: (event: React.MouseEvent | KeyboardEvent) => void;
+  ref?: React.Ref<HTMLDivElement>;
 };
 
 type CustomModalProps = BareModalProps &
-	Omit<HTMLAttributes<HTMLDivElement>, keyof BareModalProps | 'title'>;
+  Omit<HTMLAttributes<HTMLDivElement>, keyof BareModalProps | 'title'>;
 
 const CustomModal = ({
-	background = 'gray6',
-	size = 'small',
-	open = false,
-	onClose,
-	children,
-	disablePortal = false,
-	minHeight,
-	maxHeight,
-	zIndex = 999,
-	onClick,
-	containerWindow,
-	ref,
-	...rest
+  background = 'gray6',
+  size = 'small',
+  open = false,
+  onClose,
+  children,
+  disablePortal = false,
+  minHeight,
+  maxHeight,
+  zIndex = 999,
+  onClick,
+  ref,
+  ...rest
 }: CustomModalProps) => {
-	const [delayedOpen, setDelayedOpen] = useState(false);
-	const { windowObj: themeWindowObj } = useContext(ThemeContext);
-	const windowObj = containerWindow ?? themeWindowObj;
+  const [delayedOpen, setDelayedOpen] = useState(false);
 
-	const modalRef = useCombinedRefs<HTMLDivElement>(ref);
-	const modalContentRef = useRef<HTMLDivElement>(null);
-	const startSentinelRef = useRef<HTMLDivElement>(null);
-	const endSentinelRef = useRef<HTMLDivElement>(null);
+  const modalRef = useCombinedRefs<HTMLDivElement>(ref);
+  const modalContentRef = useRef<HTMLDivElement>(null);
+  const startSentinelRef = useRef<HTMLDivElement>(null);
+  const endSentinelRef = useRef<HTMLDivElement>(null);
 
-	const onBackdropClick = useCallback(
-		(e: KeyboardEvent | React.MouseEvent<HTMLDivElement>) => {
-			if (e) {
-				e.stopPropagation();
-			}
-			if (
-				!e.defaultPrevented &&
-				modalContentRef.current &&
-				onClose &&
-				e.target instanceof Node &&
-				!modalContentRef.current.contains(e.target)
-			) {
-				onClose(e);
-			}
-		},
-		[onClose]
-	);
+  const onBackdropClick = useCallback(
+    (e: KeyboardEvent | React.MouseEvent<HTMLDivElement>) => {
+      if (e) {
+        e.stopPropagation();
+      }
+      if (
+        !e.defaultPrevented &&
+        modalContentRef.current &&
+        onClose &&
+        e.target instanceof Node &&
+        !modalContentRef.current.contains(e.target)
+      ) {
+        onClose(e);
+      }
+    },
+    [onClose],
+  );
 
-	const onStartSentinelFocus = useCallback(() => {
-		if (modalContentRef.current) {
-			const nodeList = modalContentRef.current.querySelectorAll<HTMLElement>('[tabindex]');
-			nodeList[nodeList.length - 1].focus();
-		}
-	}, []);
+  const onStartSentinelFocus = useCallback(() => {
+    if (modalContentRef.current) {
+      const nodeList = modalContentRef.current.querySelectorAll<HTMLElement>('[tabindex]');
+      nodeList[nodeList.length - 1].focus();
+    }
+  }, []);
 
-	const onEndSentinelFocus = useCallback(() => {
-		if (modalContentRef.current) {
-			const nodeEl = modalContentRef.current.querySelector<HTMLElement>('[tabindex]');
-			nodeEl && nodeEl.focus();
-		}
-	}, []);
+  const onEndSentinelFocus = useCallback(() => {
+    if (modalContentRef.current) {
+      const nodeEl = modalContentRef.current.querySelector<HTMLElement>('[tabindex]');
+      nodeEl?.focus();
+    }
+  }, []);
 
-	const escapeEvent = useMemo<KeyboardPresetObj[]>(
-		() =>
-			(onClose && [
-				{ type: 'keydown', callback: onClose, keys: [{ key: 'Escape', ctrlKey: false }] }
-			]) ||
-			[],
-		[onClose]
-	);
-	useKeyboard(modalRef, escapeEvent);
+  const escapeEvent = useMemo<KeyboardPresetObj[]>(
+    () =>
+      (onClose && [
+        { type: 'keydown', callback: onClose, keys: [{ key: 'Escape', ctrlKey: false }] },
+      ]) ||
+      [],
+    [onClose],
+  );
+  useKeyboard(modalRef, escapeEvent);
 
-	useEffect(() => {
-		if (open) {
-			const defaultOverflowY = windowObj.document.body.style.overflowY;
-			const defaultPaddingRight = windowObj.document.body.style.paddingRight;
+  useEffect(() => {
+    if (open) {
+      const defaultOverflowY = window.document.body.style.overflowY;
+      const defaultPaddingRight = window.document.body.style.paddingRight;
 
-			windowObj.document.body.style.overflowY = 'hidden';
-			isBodyOverflowing(modalRef, windowObj) &&
-				(windowObj.document.body.style.paddingRight = `${getScrollbarSize(windowObj)}px`);
+      window.document.body.style.overflowY = 'hidden';
+      isBodyOverflowing(modalRef, window) &&
+        (window.document.body.style.paddingRight = `${getScrollbarSize(window)}px`);
 
-			return (): void => {
-				windowObj.document.body.style.overflowY = defaultOverflowY;
-				windowObj.document.body.style.paddingRight = defaultPaddingRight;
-			};
-		}
-		return (): void => undefined;
-	}, [modalRef, open, windowObj]);
+      return (): void => {
+        window.document.body.style.overflowY = defaultOverflowY;
+        window.document.body.style.paddingRight = defaultPaddingRight;
+      };
+    }
+    return (): void => undefined;
+  }, [modalRef, open]);
 
-	useEffect(() => {
-		const focusedElement = windowObj.document.activeElement as HTMLElement;
-		const startSentinelRefSave = startSentinelRef.current;
-		const endSentinelRefSave = endSentinelRef.current;
+  useEffect(() => {
+    const focusedElement = window.document.activeElement as HTMLElement;
+    const startSentinelRefSave = startSentinelRef.current;
+    const endSentinelRefSave = endSentinelRef.current;
 
-		if (open) {
-			modalContentRef.current && modalContentRef.current.focus();
-			startSentinelRefSave && startSentinelRefSave.addEventListener('focus', onStartSentinelFocus);
-			endSentinelRefSave && endSentinelRefSave.addEventListener('focus', onEndSentinelFocus);
-		}
+    if (open) {
+      modalContentRef.current && modalContentRef.current.focus();
+      startSentinelRefSave && startSentinelRefSave.addEventListener('focus', onStartSentinelFocus);
+      endSentinelRefSave && endSentinelRefSave.addEventListener('focus', onEndSentinelFocus);
+    }
 
-		return (): void => {
-			startSentinelRefSave &&
-				startSentinelRefSave.removeEventListener('focus', onStartSentinelFocus);
-			endSentinelRefSave && endSentinelRefSave.removeEventListener('focus', onEndSentinelFocus);
-			open && focusedElement && focusedElement.focus();
-		};
-	}, [open, onStartSentinelFocus, onEndSentinelFocus, windowObj]);
+    return (): void => {
+      startSentinelRefSave &&
+        startSentinelRefSave.removeEventListener('focus', onStartSentinelFocus);
+      endSentinelRefSave && endSentinelRefSave.removeEventListener('focus', onEndSentinelFocus);
+      open && focusedElement && focusedElement.focus();
+    };
+  }, [open, onStartSentinelFocus, onEndSentinelFocus]);
 
-	useEffect(() => {
-		// delay the open of the modal after the show of the portal
-		// in order to make the transition visible
-		const timeout = setTimeout(() => setDelayedOpen(open), TIMERS.MODAL.DELAY_OPEN);
-		return (): void => {
-			clearTimeout(timeout);
-		};
-	}, [open]);
+  useEffect(() => {
+    // delay the open of the modal after the show of the portal
+    // in order to make the transition visible
+    const timeout = setTimeout(() => setDelayedOpen(open), TIMERS.MODAL.DELAY_OPEN);
+    return (): void => {
+      clearTimeout(timeout);
+    };
+  }, [open]);
 
-	return (
-		<Portal show={open} disablePortal={disablePortal} container={windowObj.document.body}>
-			<ModalContainer
-				ref={modalRef}
-				$open={delayedOpen}
-				$mounted={open}
-				onClick={onBackdropClick}
-				$zIndex={zIndex}
-				data-testid="modal"
-				{...rest}
-			>
-				<div tabIndex={0} ref={startSentinelRef} />
-				<Transition type="scale-in" apply={delayedOpen}>
-					<ModalWrapper>
-						<ModalContent
-							ref={modalContentRef}
-							background={background}
-							$size={size}
-							minHeight={minHeight}
-							maxHeight={maxHeight}
-							onClick={onClick}
-						>
-							{children}
-						</ModalContent>
-					</ModalWrapper>
-				</Transition>
-				<div tabIndex={0} ref={endSentinelRef} />
-			</ModalContainer>
-		</Portal>
-	);
+  return (
+    <Portal show={open} disablePortal={disablePortal} container={window.document.body}>
+      <ModalContainer
+        ref={modalRef}
+        $open={delayedOpen}
+        $mounted={open}
+        onClick={onBackdropClick}
+        $zIndex={zIndex}
+        data-testid="modal"
+        {...rest}
+      >
+        <div tabIndex={0} ref={startSentinelRef} />
+        <Transition type="scale-in" apply={delayedOpen}>
+          <ModalWrapper>
+            <ModalContent
+              ref={modalContentRef}
+              background={background}
+              $size={size}
+              minHeight={minHeight}
+              maxHeight={maxHeight}
+              onClick={onClick}
+            >
+              {children}
+            </ModalContent>
+          </ModalWrapper>
+        </Transition>
+        <div tabIndex={0} ref={endSentinelRef} />
+      </ModalContainer>
+    </Portal>
+  );
 };
 
 export type { CustomModalProps };
