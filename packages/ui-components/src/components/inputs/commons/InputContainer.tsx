1174bfd2067c041efdc56e96549fbb2089c16961
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-03-02 11:59:38 +0100

    feat(packages/ui-components): migrate InputContainer to CSS module
    Replaces styled-components with CSS module for InputContainer. Updates
    component to use theme variables and clsx for class management.

--- packages/ui-components/src/components/inputs/commons/InputContainer.tsx
+++ packages/ui-components/src/components/inputs/commons/InputContainer.tsx
@@ -3,26 +3,53 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import styled, { css, SimpleInterpolation } from 'styled-components';
+import clsx from 'clsx';
+import { CSSProperties, FC, HTMLAttributes, useMemo } from 'react';
 
-import { getColor, pseudoClasses } from '../../../theme/theme-utils';
+import { getThemeColorVar } from '../../../theme/theme-utils';
 import { Container, ContainerProps } from '../../layout/Container';
+import styles from './InputContainer.module.css';
 
-export const InputContainer = styled(Container)<{
-	background: NonNullable<ContainerProps['background']>;
-	$disabled?: boolean;
-}>`
-	${({ $disabled, background, theme }): SimpleInterpolation =>
-		$disabled
-			? css`
-					background: ${getColor(`${background}.disabled`, theme)};
-				`
-			: css`
-					cursor: text;
-					${pseudoClasses(theme, background)}
-				`};
-	min-height: calc(
-		${({ theme }): string => theme.sizes.font.medium} * 1.5 +
-			${({ theme }): string => theme.sizes.font.extrasmall} * 1.5 + 0.125rem
-	);
-`;
+type InputContainerOwnProps = {
+  background: NonNullable<ContainerProps['background']>;
+  $disabled?: boolean;
+};
+
+export type InputContainerProps = InputContainerOwnProps &
+  Omit<ContainerProps, 'background'> &
+  Omit<HTMLAttributes<HTMLDivElement>, keyof ContainerProps>;
+
+const COLOR_VARIANTS = ['regular', 'hover', 'focus', 'active', 'disabled'] as const;
+const COLOR_SPLIT_REGEXP = new RegExp(`.(${COLOR_VARIANTS.join('|')})`);
+
+function resolveColorVar(color: string, variant: string): string {
+  return getThemeColorVar(color.replace(COLOR_SPLIT_REGEXP, ''), variant);
+}
+
+export const InputContainer: FC<InputContainerProps> = ({
+  background,
+  $disabled,
+  className,
+  style,
+  ...rest
+}) => {
+  const containerStyle = useMemo<CSSProperties>(() => {
+    const baseColor = background.replace(COLOR_SPLIT_REGEXP, '');
+    return {
+      '--input-container-bg': resolveColorVar(background, 'regular'),
+      '--input-container-bg-hover': resolveColorVar(`${baseColor}.hover`, 'hover'),
+      '--input-container-bg-focus': resolveColorVar(`${baseColor}.focus`, 'focus'),
+      '--input-container-bg-active': resolveColorVar(`${baseColor}.active`, 'active'),
+      '--input-container-bg-disabled': resolveColorVar(`${baseColor}.disabled`, 'disabled'),
+      ...style,
+    } as CSSProperties;
+  }, [background, style]);
+
+  return (
+    <Container
+      className={clsx(styles.inputContainer, $disabled && styles.disabled, className)}
+      style={containerStyle}
+      {...rest}
+    />
+  );
+};
