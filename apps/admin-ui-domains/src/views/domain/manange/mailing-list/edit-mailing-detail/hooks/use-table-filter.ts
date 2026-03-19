329b249db03b2f4878828667aea577ae5ecb4584
Dhaval Dodiya <dhaval.dodiya@zextras.com>
2026-03-19 13:38:04 +0530

    chore: refactor edit mailing and created individual components for tabs

---
+++ apps/admin-ui-domains/src/views/domain/manange/mailing-list/edit-mailing-detail/hooks/use-table-filter.ts
@@ -0,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { ChangeEvent, useCallback, useState } from 'react';
+
+type UseTableFilterReturn = {
+  filterValue: string;
+  filteredRows: Array<any>;
+  handleFilterChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export function useTableFilter(sourceRows: Array<any>): UseTableFilterReturn {
+  const [filterValue, setFilterValue] = useState('');
+  const [filteredRows, setFilteredRows] = useState<Array<any>>([]);
+
+  const handleFilterChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>): void => {
+      const value = e.target.value;
+      if (value !== '') {
+        setFilterValue(value);
+        const filtered = sourceRows.filter((item: any) =>
+          item?.id?.toLowerCase().includes(value.toLowerCase()),
+        );
+        setFilteredRows(filtered);
+      } else {
+        setFilterValue('');
+        setFilteredRows(sourceRows);
+      }
+    },
+    [sourceRows],
+  );
+
+  return { filterValue, filteredRows, handleFilterChange };
+}
