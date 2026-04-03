fb65d721042fe39517d59907e2ba8b7a44ff80c2
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-04-03 05:09:46 +0200

    chore(root): update semantic-release config for git assets
    Include package.json and pnpm-lock.json in the assets for the
    @semantic-release/git plugin in release.config. Also update formatting
    for consistency.

--- release.config.mjs
+++ release.config.mjs
@@ -7,38 +7,38 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-	branches: [
-		'release',
-		{
-			name: 'devel',
-			prerelease: true
-		}
-	],
-	plugins: [
-		[
-			'@semantic-release/commit-analyzer',
-			{
-				preset: 'conventionalcommits',
-				releaseRules: [
-					{ type: 'refactor', release: 'patch' },
-					{ type: 'build', release: 'patch' }
-				]
-			}
-		],
-		[
-			'@semantic-release/release-notes-generator',
-			{
-				preset: 'conventionalcommits'
-			}
-		],
-		'@semantic-release/changelog',
-		[
-			'@semantic-release/git',
-			{
-				assets: ['CHANGELOG.md'],
-				message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-			}
-		],
-		'@semantic-release/github'
-	]
+  branches: [
+    'release',
+    {
+      name: 'devel',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { type: 'refactor', release: 'patch' },
+          { type: 'build', release: 'patch' },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'pnpm-lock.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
 };
