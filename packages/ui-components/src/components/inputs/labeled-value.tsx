8ae828c3f1f16938ae507a7f51d7c3a2eef0ce7e
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-04-09 15:15:10 +0200

    fix[packages/ui-components](labeled-value): restyle and simplify component
    Adjusted styles and props for labeled-value, updated color variables,
    and replaced InputContainer with styled divs.

--- packages/ui-components/src/components/inputs/labeled-value.tsx
+++ packages/ui-components/src/components/inputs/labeled-value.tsx
@@ -6,14 +6,13 @@
 
 import '../../web-components/divider-wc';
 
-import { useMemo } from 'react';
+import React, { useMemo } from 'react';
 
-import { resolveThemeColor } from '../../theme/theme-utils';
+import { getPaddingVar, resolveThemeColor } from '../../theme/theme-utils';
 import { AnyColor } from '../../types/utils';
-import { Container, ContainerProps } from '../layout/Container';
 import styles from './labeled-value.module.css';
 
-type InputProps = ContainerProps & {
+type LabeledValueProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> & {
   backgroundColor?: AnyColor;
   textColor?: AnyColor;
   label?: string;
@@ -27,43 +26,36 @@ export const LabeledValue = ({
   label,
   value,
   CustomIcon,
-}: InputProps) => {
-  const inputColor = useMemo(
-    () =>
-      ({
-        '--input-color': resolveThemeColor(textColor, 'regular'),
-      } as React.CSSProperties),
-    [textColor],
-  );
+}: LabeledValueProps) => {
+  const computedTextColor = {
+    '--text-color': resolveThemeColor(textColor, 'regular'),
+  } as React.CSSProperties;
 
   const wrapperStyle = useMemo(() => {
     return {
-      '--input-container-bg': resolveThemeColor(backgroundColor, 'regular'),
+      '--text-container-bg': resolveThemeColor(backgroundColor, 'regular'),
     } as React.CSSProperties;
   }, [backgroundColor]);
 
+  const innerStyle = {
+    ...computedTextColor,
+    padding: getPaddingVar({ vertical: label ? '0.0625rem' : '0.625rem' }),
+  } as React.CSSProperties;
+
   return (
-    <Container height="fit" width="fill" crossAlignment="flex-start">
+    <div className={styles.outerWrapper}>
       <div className={styles.fieldWrapper} style={wrapperStyle}>
-        <Container
-          className={styles.relativeContainer}
-          style={inputColor}
-          padding={{ vertical: label ? '0.0625rem' : '0.625rem' }}
-          mainAlignment={'flex-end'}
-          height={'fill'}
-          width={'fill'}
-          minHeight={'inherit'}
-        >
+        <div className={styles.relativeContainer} style={innerStyle}>
           <span className={styles.value}>{value}</span>
           {label && <span className={styles.label}>{label}</span>}
-        </Container>
+        </div>
         {CustomIcon && (
-          <span style={{ display: 'flex', alignItems: 'center' }}>
+          <span className={styles.iconWrapper}>
             <CustomIcon />
           </span>
         )}
       </div>
       <divider-wc color={'gray3'}></divider-wc>
-    </Container>
+    </div>
   );
 };
