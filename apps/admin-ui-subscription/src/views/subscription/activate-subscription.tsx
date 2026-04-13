11f6a211444a96dd56322120d2d10a825ccbcf14
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-04-13 17:50:11 +0200

    fix(subscription): update disclaimer text container (activate-subscription)
    Wraps the disclaimer Text component in a div for improved styling and
    structure.

--- apps/admin-ui-subscription/src/views/subscription/activate-subscription.tsx
+++ apps/admin-ui-subscription/src/views/subscription/activate-subscription.tsx
@@ -60,12 +60,14 @@ export const ActivateSubscription = (): React.JSX.Element => {
           </div>
         </div>
         <img src={subscription_logo} alt="logo" className={styles.logo} />
-        <Text color="gray0" style={{ ...styles.text }}>
-          {t(
-            'core.subscription.disclaimer',
-            "Seems like you don't have a subscription token active yet.\nFill the field above or contact a vendor to get a new one.",
-          )}
-        </Text>
+        <div className={styles.text}>
+          <Text color="gray0">
+            {t(
+              'core.subscription.disclaimer',
+              "Seems like you don't have a subscription token active yet.\nFill the field above or contact a vendor to get a new one.",
+            )}
+          </Text>
+        </div>
       </div>
     </div>
   );
