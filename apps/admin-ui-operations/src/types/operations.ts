963b6db71ac4663e82220de6882b04918d37be5e
Dhaval Dodiya <dhaval.dodiya@zextras.com>
2026-04-17 13:27:45 +0530

    chore: improve type safety in operations module

---
+++ apps/admin-ui-operations/src/types/operations.ts
@@ -0,0 +1,41 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export type OperationParameters = {
+	requesterAddress?: string;
+	additionalNotificationAddresses?: Array<string>;
+	createFakeBlob?: boolean;
+	isDeep?: boolean;
+};
+
+export type Operation = {
+	id: string;
+	name?: string;
+	host?: string;
+	serverName?: string;
+	serverId?: string;
+	module?: string;
+	state: string;
+	type?: string;
+	startTime?: number;
+	queuedTime?: number;
+	humanStartTime?: string;
+	parameters?: OperationParameters;
+};
+
+export type ManageOption = {
+	id: string;
+	name: string;
+	isSelected: boolean;
+};
+
+export type ZextrasRequestBody = {
+	Body?: {
+		zextras?: {
+			action?: string;
+		};
+	};
+};
