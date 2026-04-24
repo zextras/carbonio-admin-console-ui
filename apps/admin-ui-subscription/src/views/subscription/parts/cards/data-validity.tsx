33ea706c5b0fdd33029477d3c61d5d726fd7712b
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-04-24 15:33:56 +0200

    feat[subscription](data-validity): add DataValidity card component
    Add DataValidity card component and CSS module to display the data validity
    date using license info and date formatting.

---
+++ apps/admin-ui-subscription/src/views/subscription/parts/cards/data-validity.tsx
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { useLicenseInfo } from '@zextras/ui-shared';
+import { format } from 'date-fns';
+import { useTranslation } from 'react-i18next';
+
+import styles from './data-validity.module.css';
+
+const DATE_FORMAT = 'dd MMM yyyy';
+
+export const DataValidity = () => {
+  const { t } = useTranslation();
+
+  const title = t('core.subscription.data_validity', 'Data valid until');
+  const { data: licenseData } = useLicenseInfo();
+
+  const lastValidationDate = licenseData?.response?.nextValidationDeadline;
+
+  const dataValidityLabel = lastValidationDate ? format(lastValidationDate, DATE_FORMAT) : '';
+  return (
+    <div className={styles.card}>
+      <ds-text size="small" as="span" color="gray0">
+        {title}
+      </ds-text>
+      <ds-text weight="bold" color="gray0" style={{ fontSize: '1.5rem' }}>
+        {dataValidityLabel}
+      </ds-text>
+    </div>
+  );
+};
