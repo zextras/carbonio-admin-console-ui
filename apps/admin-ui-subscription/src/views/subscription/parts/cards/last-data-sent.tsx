ada504ccb5953bf44573471ec9caed1292daa4e6
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-04-24 15:29:17 +0200

    feat[subscription](last-data-sent): add LastDataSent card component
    Add LastDataSent card component and its CSS module for displaying the last
    time data was sent, using license info and date formatting.

---
+++ apps/admin-ui-subscription/src/views/subscription/parts/cards/last-data-sent.tsx
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { useLicenseInfo } from '@zextras/ui-shared';
+import { format } from 'date-fns';
+import { useTranslation } from 'react-i18next';
+
+import styles from './subscription-expiry.module.css';
+
+const DATE_FORMAT = 'dd MMM yyyy';
+
+export const LastDataSent = () => {
+  const { t } = useTranslation();
+
+  const title = t('core.subscription.last_data_sent', 'Last time data has been sent');
+  const { data: licenseData } = useLicenseInfo();
+
+  const lastValidationDate = licenseData?.response?.lastValidationCheck;
+
+  const expirationDateLabel = lastValidationDate ? format(lastValidationDate, DATE_FORMAT) : '';
+  return (
+    <div className={styles.card}>
+      <ds-text size="small" as="span" color="gray0">
+        {title}
+      </ds-text>
+      <ds-text weight="bold" color="gray0" style={{ fontSize: '1.5rem' }}>
+        {expirationDateLabel}
+      </ds-text>
+    </div>
+  );
+};
