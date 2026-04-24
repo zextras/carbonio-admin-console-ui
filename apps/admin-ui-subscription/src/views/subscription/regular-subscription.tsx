5c78afedbc966e8c184ba87b51cc3e2bd8f85826
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-04-24 16:31:18 +0200

    fix[subscription](regular-subscription): fix import order
    Reorder imports in RegularSubscription view to comply with style guidelines.

--- apps/admin-ui-subscription/src/views/subscription/regular-subscription.tsx
+++ apps/admin-ui-subscription/src/views/subscription/regular-subscription.tsx
@@ -6,8 +6,8 @@
 
 import { useTranslation } from 'react-i18next';
 
-import { SeatUtilization } from './parts/cards/seat-utilization';
 import { ActiveEdition } from './parts/cards/active-edition';
+import { SeatUtilization } from './parts/cards/seat-utilization';
 import { SubscriptionExpiry } from './parts/cards/subscription-expiry';
 import { SubscriptionStatus } from './parts/cards/subscription-status';
 import styles from './regular-subscription.module.css';
