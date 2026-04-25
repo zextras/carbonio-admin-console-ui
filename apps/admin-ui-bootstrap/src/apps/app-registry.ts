dd8291f67d98e5e9715bcd79ce44755ecd53e55c
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-04-25 10:51:22 +0200

    fix(a11y): associate InputLabel with control via children support

--- apps/admin-ui-bootstrap/src/apps/app-registry.ts
+++ apps/admin-ui-bootstrap/src/apps/app-registry.ts
@@ -15,8 +15,7 @@ import Operations from '@zextras/admin-ui-operations';
 import Privacy from '@zextras/admin-ui-privacy';
 import Storage from '@zextras/admin-ui-storage';
 import Subscription from '@zextras/admin-ui-subscription';
-import type { AppState } from '@zextras/ui-shared';
-import { registerApp } from '@zextras/ui-shared';
+import { type AppState, registerApp } from '@zextras/ui-shared';
 import { ComponentType } from 'react';
 import { StoreApi, UseBoundStore } from 'zustand';
 
