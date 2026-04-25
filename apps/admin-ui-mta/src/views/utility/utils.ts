21bed2dc51c89a44257a4b7275fdb28a4300cb8f
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-04-25 10:47:16 +0200

    fix(sonar): replace parseFloat with Number.parseFloat in admin-ui-mta utils

--- apps/admin-ui-mta/src/views/utility/utils.ts
+++ apps/admin-ui-mta/src/views/utility/utils.ts
@@ -50,7 +50,7 @@ export const validateIpAddress = (value: string): boolean => {
 };
 
 export function bytesToMB(bytes: number): number {
-  return parseFloat((bytes / 1024 / 1024).toFixed(2));
+  return Number.parseFloat((bytes / 1024 / 1024).toFixed(2));
 }
 
 export function mbToBytes(mb: number): number {
