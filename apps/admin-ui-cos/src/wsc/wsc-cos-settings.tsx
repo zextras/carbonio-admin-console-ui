09cb6b8607131b67b3224afb73638b06e5d25c14
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-06-03 09:33:10 +0200

    refactor(admin-ui-cos/wsc-cos-settings): introduce WscCosForm component
    Extract the WSC settings form logic into a new `WscCosForm` component.
    This simplifies `WscCosSettings` by delegating form management to the new component,
    improving separation of concerns and maintainability.

--- apps/admin-ui-cos/src/wsc/wsc-cos-settings.tsx
+++ apps/admin-ui-cos/src/wsc/wsc-cos-settings.tsx
@@ -4,132 +4,39 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { Container } from '@zextras/ui-components';
 import { useCurrentUserRights } from '@zextras/ui-shared';
-import { find, forEach, isEqual, size } from 'lodash-es';
-import { FC, useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { find } from 'lodash-es';
 import { useParams } from 'react-router';
 
-import { AccountType } from '../../types/account';
-import { Attribute } from '../../types/attribute';
-import { COS, ZIMBRA_ADMIN_URN } from '../constants';
-import { ModifyCosBody } from '../services/modify-cos-service';
+import { type AccountType } from '../../types/account';
+import { type Attribute } from '../../types/attribute';
+import { COS } from '../constants';
 import { useCosDetail } from '../services/use-cos-detail';
-import { useModifyCos } from '../services/use-modify-cos';
-import { FormPageLayout } from '../views/form-page-layout';
-import { WscSettings } from './wsc-settings';
+import { WscCosForm } from './wsc-cos-form';
 
-export const WscCosSettings: FC = () => {
-  const [t] = useTranslation();
+function buildCosData(cosInformation: Array<Attribute> | undefined): AccountType {
+  if (!cosInformation?.length) return {} as AccountType;
+  const obj: AccountType = {};
+  cosInformation.forEach((item) => {
+    obj[item?.n as keyof AccountType] = item._content;
+  });
+  return obj;
+}
+
+export const WscCosSettings = () => {
   const { cosId } = useParams();
-
-  const [zimbraId, setZimbraId] = useState<string | undefined>(undefined);
-  const [initCosData, setInitCosData] = useState<AccountType>({});
-  const [cosFeatures, setCosFeatures] = useState<AccountType>({});
-  const [isDirty, setIsDirty] = useState<boolean>(false);
-
   const { data: cosDetailData, isPending } = useCosDetail(cosId);
   const cosInformation = cosDetailData?.cos?.[0]?.a;
   const { data: rights = [] } = useCurrentUserRights();
-  const modifyCosMutation = useModifyCos(cosId);
 
-  const readonlyCOS = (() => {
-    const rightsConfig = find(rights, { type: COS }) || { all: [], type: COS };
-    return !rightsConfig?.all?.[0]?.setAttrs?.[0]?.all;
-  })();
+  const cosData = buildCosData(cosInformation);
 
-  const setSwitchOptionValue = (key: keyof AccountType, value: string | undefined): void => {
-    if (value) {
-      setInitCosData((prev) => ({ ...prev, [key]: value }));
-      setCosFeatures((prev) => ({ ...prev, [key]: value }));
-    }
-  };
-
-  const setInitialValues = (obj: AccountType) => {
-    if (obj) {
-      setSwitchOptionValue('carbonioFeatureWscEnabled', obj?.carbonioFeatureWscEnabled);
-      setSwitchOptionValue('carbonioWscShowMessageReads', obj?.carbonioWscShowMessageReads);
-      setSwitchOptionValue('carbonioWscShowUsersPresence', obj?.carbonioWscShowUsersPresence);
-      setSwitchOptionValue(
-        'carbonioWscVirtualBackgroundEnabled',
-        obj?.carbonioWscVirtualBackgroundEnabled,
-      );
-      setSwitchOptionValue('carbonioWscVideoCallEnabled', obj?.carbonioWscVideoCallEnabled);
-      setSwitchOptionValue('carbonioWscRecordingEnabled', obj?.carbonioWscRecordingEnabled);
-      setSwitchOptionValue('carbonioWscGroupChatCreation', obj?.carbonioWscGroupChatCreation);
-      setSwitchOptionValue('carbonioWscPrivateChatCreation', obj?.carbonioWscPrivateChatCreation);
-      setSwitchOptionValue('carbonioWscAttachmentUpload', obj?.carbonioWscAttachmentUpload);
-      setSwitchOptionValue(
-        'carbonioWscMessageDeleteTimeLimit',
-        obj?.carbonioWscMessageDeleteTimeLimit,
-      );
-      setSwitchOptionValue('carbonioWscMessageEditTimeLimit', obj?.carbonioWscMessageEditTimeLimit);
-      setSwitchOptionValue('carbonioWscMaxGroupMembers', obj?.carbonioWscMaxGroupMembers);
-      setSwitchOptionValue('carbonioWscMaxRoomPictureSize', obj?.carbonioWscMaxRoomPictureSize);
-      setSwitchOptionValue('carbonioWscMaxAttachmentSize', obj?.carbonioWscMaxAttachmentSize);
-    }
-  };
-
-  useEffect(() => {
-    if (size(cosInformation) > 0) {
-      const obj: AccountType = {};
-      forEach(cosInformation, (item: Attribute) => {
-        obj[item?.n as keyof AccountType] = item._content;
-      });
-      setZimbraId(obj?.zimbraId);
-      setInitialValues(obj);
-      setIsDirty(false);
-    }
-  }, [cosInformation]);
-
-  useEffect(() => {
-    if (zimbraId && !isEqual(cosFeatures, initCosData)) {
-      setIsDirty(true);
-    } else {
-      setIsDirty(false);
-    }
-  }, [cosFeatures, initCosData, zimbraId]);
-
-  const onSave = () => {
-    const body: ModifyCosBody = {
-      _jsns: ZIMBRA_ADMIN_URN,
-      id: {
-        _content: zimbraId,
-      },
-    } as ModifyCosBody;
-    body.a = Object.keys(cosFeatures).map((ele) => ({
-      n: ele,
-      _content: cosFeatures[ele as keyof AccountType] as string,
-    }));
-    modifyCosMutation.mutate(body);
-  };
-
-  const onCancel = () => {
-    setCosFeatures(initCosData);
-    setIsDirty(false);
-  };
+  const rightsConfig = find(rights, { type: COS }) || { all: [], type: COS };
+  const readonlyCOS = !rightsConfig?.all?.[0]?.setAttrs?.[0]?.all;
 
   if (isPending) {
-    return (
-      <Container crossAlignment="center" mainAlignment="center" height="fill">
-        <ds-spinner />
-      </Container>
-    );
+    return <ds-page-shimmer></ds-page-shimmer>;
   }
 
-  return (
-    <FormPageLayout
-      title={t('label.wsc', 'Chats')}
-      onSave={onSave}
-      onCancel={onCancel}
-      unsavedChanges={isDirty}
-    >
-      <WscSettings
-        featuresDetail={cosFeatures}
-        setFeaturesDetail={setCosFeatures}
-        readonlyFeatures={readonlyCOS}
-      />
-    </FormPageLayout>
-  );
+  return <WscCosForm cosData={cosData} readonlyCOS={readonlyCOS} />;
 };
