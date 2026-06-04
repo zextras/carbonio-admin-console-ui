67d3f679a524c3a32b871c4ea2051700b1c56998
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-06-04 14:34:30 +0200

    style(wsc/wsc-settings): reorder imports

--- apps/admin-ui-cos/src/wsc/tests/wsc-settings.browser.test.tsx
+++ apps/admin-ui-cos/src/wsc/tests/wsc-settings.browser.test.tsx
@@ -3,8 +3,8 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import { useSelector } from '@tanstack/react-store';
 import { QueryClient } from '@tanstack/react-query';
+import { useSelector } from '@tanstack/react-store';
 import {
 	advancedSupportedApiForBrowser,
 	createBrowserAPIInterceptor,
