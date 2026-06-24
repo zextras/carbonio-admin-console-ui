1c6377bb37025a01196ac5baf0aee9397f96f3a4
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-06-24 14:15:59 +0200

    test(subscription): add perpetual subscription browser tests
    Extract header to a dedicated component to support testing.

--- apps/admin-ui-subscription/src/views/subscription/perpetual-subscription.tsx
+++ apps/admin-ui-subscription/src/views/subscription/perpetual-subscription.tsx
@@ -4,37 +4,27 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { useTranslation } from 'react-i18next';
-
-import { ActiveEdition } from './parts/cards/active-edition';
 import { MaxVersion } from './parts/cards/max-version';
-import { SeatUtilization } from './parts/cards/seat-utilization';
 import { SubscriptionStatus } from './parts/cards/subscription-status';
 import { ActivationTokenSection } from './parts/sections/activation-token-section';
 import { ActiveEditionSection } from './parts/sections/active-edition-section';
 import { AddonsSection } from './parts/sections/addons-section';
 import { DetailsSection } from './parts/sections/details-section';
+import { SubscriptionsHeader } from './parts/sections/subscriptions-header';
 import styles from './subscription-layout.module.css';
 
 export const PerpetualSubscription = () => {
-  const { t } = useTranslation();
   return (
     <div className={styles.outer}>
-      <div className={styles.header}>
-        <ds-text as="h2" weight="bold" size="extralarge" color="gray0">
-          {t('label.subscriptions', 'Subscriptions')}
-        </ds-text>
-      </div>
+      <SubscriptionsHeader />
       <div className={styles.row}>
         <SubscriptionStatus />
-        <ActiveEdition />
-        <SeatUtilization />
         <MaxVersion />
       </div>
       <div className={styles.content}>
         <ActiveEditionSection />
         <AddonsSection />
-        <ds-divider style={{paddingBottom:'1.9rem', paddingTop:'0.4rem'}}></ds-divider>
+        <ds-divider style={{ paddingBottom: '1.9rem', paddingTop: '0.4rem' }}></ds-divider>
         <DetailsSection />
         <ActivationTokenSection />
       </div>
