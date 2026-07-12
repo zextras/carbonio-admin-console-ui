b64867be978e7391b69f83bffafe6d847885d463
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-07-12 17:24:54 +0200

    refactor(admin-ui-storage/create-mailstores-volume): extract wizard buttons into components
    Extract inline wizard step buttons into reusable components and use a
    WizardActionsContext to handle cancel and next callbacks, reducing
    duplication in the wizard steps definition.

--- apps/admin-ui-storage/src/views/bucket/server-specifics/volume/create-volume/advanced-create-volume/create-mailstores-volume.tsx
+++ apps/admin-ui-storage/src/views/bucket/server-specifics/volume/create-volume/advanced-create-volume/create-mailstores-volume.tsx
@@ -5,7 +5,7 @@
  */
 import { useForm } from '@tanstack/react-form';
 import { Button, HorizontalWizard, Section } from '@zextras/ui-components';
-import { type ReactElement, useState } from 'react';
+import { createContext, type FC, type ReactElement, useContext, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import type { CreateMailstoresVolumeProps, WizardInSectionProps } from '../../../../../../../types';
@@ -14,7 +14,17 @@ import { AdvancedMailstoresConfig } from './advanced-mailstores-config';
 import AdvancedMailstoresCreate from './advanced-mailstores-create';
 import AdvancedMailstoresDefinition from './advanced-mailstores-definition';
 import { AdvancedVolumeContext } from './create-advanced-volume-context';
-import type { AdvancedVolumeFormValues, WizardStep } from './types';
+import type { AdvancedVolumeFormValues, WizardButtonRenderProps, WizardStep } from './types';
+
+type WizardActions = {
+	onCancel: () => void;
+	onNextLocal: () => void;
+};
+
+export const WizardActionsContext = createContext<WizardActions>({
+	onCancel: () => {},
+	onNextLocal: () => {},
+});
 
 const WizardInSection = ({
 	wizard,
@@ -36,6 +46,84 @@ const WizardInSection = ({
 		>
 			{wizard}
 		</Section>
+	);
+};
+
+const WizardCancelButton: FC<WizardButtonRenderProps> = () => {
+	const { t } = useTranslation();
+	const { onCancel } = useContext(WizardActionsContext);
+	return (
+		<Button
+			type="outlined"
+			key="wizard-cancel"
+			label={t('label.volume_cancel_button', 'CANCEL')}
+			icon={'CloseOutline'}
+			iconPlacement="right"
+			color="secondary"
+			onClick={onCancel}
+		/>
+	);
+};
+
+const EmptyPrevButton: FC<WizardButtonRenderProps> = (): ReactElement => <></>;
+
+const DefinitionNextButton: FC<WizardButtonRenderProps> = ({ onClick, toggleNextBtn }) => {
+	const { t } = useTranslation();
+	const { onNextLocal } = useContext(WizardActionsContext);
+	return toggleNextBtn ? (
+		<Button
+			onClick={onNextLocal}
+			label={t('label.volume_next_step_button', 'NEXT STEP')}
+			icon={'ChevronRightOutline'}
+			iconPlacement="right"
+		/>
+	) : (
+		<Button
+			onClick={onClick}
+			label={t('label.volume_next_step_button', 'NEXT STEP')}
+			icon={'ChevronRightOutline'}
+			iconPlacement="right"
+		/>
+	);
+};
+
+const WizardPrevButton: FC<WizardButtonRenderProps> = ({ onClick, completeLoading }) => {
+	const { t } = useTranslation();
+	return (
+		<Button
+			onClick={onClick}
+			label={t('label.volume_back_button', 'BACK')}
+			icon={'ChevronLeftOutline'}
+			iconPlacement="left"
+			disabled={completeLoading}
+			color="secondary"
+		/>
+	);
+};
+
+const ConfigNextButton: FC<WizardButtonRenderProps> = ({ onClick, completeLoading }) => {
+	const { t } = useTranslation();
+	return (
+		<Button
+			onClick={onClick}
+			label={t('label.volume_next_button', 'NEXT')}
+			icon={'ChevronRightOutline'}
+			iconPlacement="right"
+			disabled={completeLoading}
+		/>
+	);
+};
+
+const CreateNextButton: FC<WizardButtonRenderProps> = ({ onClick, completeLoading }) => {
+	const { t } = useTranslation();
+	return (
+		<Button
+			onClick={onClick}
+			label={t('label.volume_create', 'CREATE')}
+			icon={'PowerOutline'}
+			iconPlacement="right"
+			disabled={completeLoading}
+		/>
 	);
 };
 
@@ -75,34 +163,9 @@ export const CreateMailstoresVolume = ({
 			icon: 'CubeOutline',
 			view: AdvancedMailstoresDefinition,
 			canGoNext: () => true,
-			CancelButton: () => (
-				<Button
-					type="outlined"
-					key="wizard-cancel"
-					label={t('label.volume_cancel_button', 'CANCEL')}
-					icon={'CloseOutline'}
-					iconPlacement="right"
-					color="secondary"
-					onClick={(): void => setToggleWizardExternal(false)}
-				/>
-			),
-			PrevButton: (): ReactElement => <></>,
-			NextButton: ({ onClick, toggleNextBtn }): ReactElement =>
-				!toggleNextBtn ? (
-					<Button
-						onClick={onClick}
-						label={t('label.volume_next_step_button', 'NEXT STEP')}
-						icon={'ChevronRightOutline'}
-						iconPlacement="right"
-					/>
-				) : (
-					<Button
-						onClick={(): void => setToggleWizardLocal(true)}
-						label={t('label.volume_next_step_button', 'NEXT STEP')}
-						icon={'ChevronRightOutline'}
-						iconPlacement="right"
-					/>
-				),
+			CancelButton: WizardCancelButton,
+			PrevButton: EmptyPrevButton,
+			NextButton: DefinitionNextButton,
 		},
 		{
 			name: 'config',
@@ -111,36 +174,9 @@ export const CreateMailstoresVolume = ({
 			view: AdvancedMailstoresConfig,
 			canGoNext: () => true,
 			clickDisabled: !!isAllocationToggle,
-			CancelButton: () => (
-				<Button
-					type="outlined"
-					key="wizard-cancel"
-					label={t('label.volume_cancel_button', 'CANCEL')}
-					icon={'CloseOutline'}
-					iconPlacement="right"
-					color="secondary"
-					onClick={(): void => setToggleWizardExternal(false)}
-				/>
-			),
-			PrevButton: ({ onClick, completeLoading }) => (
-				<Button
-					onClick={onClick}
-					label={t('label.volume_back_button', 'BACK')}
-					icon={'ChevronLeftOutline'}
-					iconPlacement="left"
-					disabled={completeLoading}
-					color="secondary"
-				/>
-			),
-			NextButton: ({ onClick, completeLoading }) => (
-				<Button
-					onClick={onClick}
-					label={t('label.volume_next_button', 'NEXT')}
-					icon={'ChevronRightOutline'}
-					iconPlacement="right"
-					disabled={completeLoading}
-				/>
-			),
+			CancelButton: WizardCancelButton,
+			PrevButton: WizardPrevButton,
+			NextButton: ConfigNextButton,
 		},
 		{
 			name: 'create',
@@ -149,36 +185,9 @@ export const CreateMailstoresVolume = ({
 			view: AdvancedMailstoresCreate,
 			canGoNext: () => true,
 			clickDisabled: !!isAllocationToggle,
-			CancelButton: () => (
-				<Button
-					type="outlined"
-					key="wizard-cancel"
-					label={t('label.volume_cancel_button', 'CANCEL')}
-					icon={'CloseOutline'}
-					iconPlacement="right"
-					color="secondary"
-					onClick={(): void => setToggleWizardExternal(false)}
-				/>
-			),
-			PrevButton: ({ onClick, completeLoading }) => (
-				<Button
-					onClick={onClick}
-					label={t('label.volume_back_button', 'BACK')}
-					icon={'ChevronLeftOutline'}
-					iconPlacement="left"
-					disabled={completeLoading}
-					color="secondary"
-				/>
-			),
-			NextButton: ({ onClick, completeLoading }) => (
-				<Button
-					onClick={onClick}
-					label={t('label.volume_create', 'CREATE')}
-					icon={'PowerOutline'}
-					iconPlacement="right"
-					disabled={completeLoading}
-				/>
-			),
+			CancelButton: WizardCancelButton,
+			PrevButton: WizardPrevButton,
+			NextButton: CreateNextButton,
 		},
 	];
 
@@ -202,15 +211,22 @@ export const CreateMailstoresVolume = ({
 	};
 
 	return (
-		<AdvancedVolumeContext.Provider value={{ form, isAllocationToggle, setIsAllocationToggle }}>
-			<HorizontalWizard
-				steps={wizardSteps}
-				Wrapper={WizardInSection}
-				onComplete={onComplete}
-				setToggleWizardSection={setToggleWizardExternal}
-				externalData={volName}
-			/>
-		</AdvancedVolumeContext.Provider>
+		<WizardActionsContext.Provider
+			value={{
+				onCancel: () => setToggleWizardExternal(false),
+				onNextLocal: () => setToggleWizardLocal(true),
+			}}
+		>
+			<AdvancedVolumeContext.Provider value={{ form, isAllocationToggle, setIsAllocationToggle }}>
+				<HorizontalWizard
+					steps={wizardSteps}
+					Wrapper={WizardInSection}
+					onComplete={onComplete}
+					setToggleWizardSection={setToggleWizardExternal}
+					externalData={volName}
+				/>
+			</AdvancedVolumeContext.Provider>
+		</WizardActionsContext.Provider>
 	);
 };
 
