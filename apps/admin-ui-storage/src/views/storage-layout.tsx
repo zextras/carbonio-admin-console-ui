c7e0f1ce953e7dbebaa21526455436a6ba77178b
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-07-13 12:59:09 +0200

    refactor(admin-ui-storage/storage-layout): use dynamic max width hook

--- apps/admin-ui-storage/src/views/storage-layout.tsx
+++ apps/admin-ui-storage/src/views/storage-layout.tsx
@@ -4,25 +4,30 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { Container } from '@zextras/ui-components';
+import { useDetailViewMaxWidth } from '@zextras/ui-shared';
 import { Outlet } from 'react-router';
 
 import BucketListPanel from './bucket/bucket-list-panel';
 
-export const StorageLayout = () => (
-  <Container orientation="horizontal" mainAlignment="flex-start">
-    <Container style={{ maxWidth: '265px' }}>
-      <BucketListPanel />
-    </Container>
-    <Container style={{ maxWidth: '100%' }}>
-      <Container
-        orientation="column"
-        crossAlignment="center"
-        mainAlignment="flex-start"
-        style={{ overflowY: 'hidden' }}
-        background="gray6"
-      >
-        <Outlet />
+export const StorageLayout = () => {
+  const detailViewMaxWidth = useDetailViewMaxWidth();
+
+  return (
+    <Container orientation="horizontal" mainAlignment="flex-start">
+      <Container style={{ maxWidth: '265px' }}>
+        <BucketListPanel />
+      </Container>
+      <Container style={{ maxWidth: '100%' }}>
+        <Container
+          orientation="column"
+          crossAlignment="center"
+          mainAlignment="flex-start"
+          style={{ maxWidth: detailViewMaxWidth, transition: 'width 300ms', overflowY: 'hidden' }}
+          background="gray6"
+        >
+          <Outlet />
+        </Container>
       </Container>
     </Container>
-  </Container>
-);
+  );
+};
