e7d2c1e92a3d688e3bb1108215d978c82850120f
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-07-14 12:51:34 +0200

    refactor(admin-ui-storage): extract bucket list table component
    move the bucket list table rendering, empty state, and associated
    configuration out of the s3-connector-list-panel into a dedicated
    bucket-list-table module.

---
+++ apps/admin-ui-storage/src/views/bucket/bucket-list-table.tsx
@@ -0,0 +1,210 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import {
+  Container,
+  CustomHeaderFactory,
+  HoverableRowFactory,
+  ListRow,
+  Padding,
+  Row,
+  Table,
+  Tooltip,
+} from '@zextras/ui-components';
+import { TFunction } from 'i18next';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { BucketConnectorRow } from '../../../types';
+import logo from '../../assets/ninja_robo.svg';
+
+type TableHeader = {
+  id: string;
+  label: string;
+  bold: boolean;
+  width?: string;
+};
+
+export type SingleSelection = [] | [string];
+
+type TextColumnConfig = {
+  key: string;
+  getValue: (volume: BucketConnectorRow) => string;
+  weight: 'light' | 'regular';
+  hasTooltip?: boolean;
+};
+
+const TEXT_COLUMNS: Array<TextColumnConfig> = [
+  { key: 'id', getValue: (v) => v.uuid, weight: 'light', hasTooltip: true },
+  { key: 'label', getValue: (v) => v.label, weight: 'regular' },
+  { key: 'bucket', getValue: (v) => v.bucketName, weight: 'light' },
+];
+
+const ACTION_BUTTON_STYLE: React.CSSProperties = {
+  background: 'none',
+  border: 'none',
+  cursor: 'pointer',
+  padding: '14px 0px 0px 14px',
+  display: 'inline-flex',
+  alignItems: 'center',
+};
+
+const headers = (t: TFunction): Array<TableHeader> => [
+  {
+    id: 'id',
+    label: t('label.id', 'ID'),
+    width: '35%',
+    bold: true,
+  },
+  {
+    id: 'label',
+    label: t('label.descriptive_name', 'Descriptive Name'),
+    width: '25%',
+    bold: true,
+  },
+  {
+    id: 'bucketName',
+    label: t('label.bucket_name', 'Bucket name'),
+    width: '25%',
+    bold: true,
+  },
+  {
+    id: 'actions',
+    label: t('label.actions', 'Actions'),
+    width: '10%',
+    bold: true,
+  },
+];
+
+function renderTextCell(
+  config: TextColumnConfig,
+  volume: BucketConnectorRow,
+  index: number,
+  onClick: (i: number) => void,
+  onDoubleClick: (i: number) => void,
+) {
+  const value = config.getValue(volume);
+  const cellKey = `${volume.uuid}-${config.key}`;
+
+  const row = (
+    <Row
+      key={cellKey}
+      onDoubleClick={(): void => {
+        onDoubleClick(index);
+      }}
+      onClick={(): void => {
+        onClick(index);
+      }}
+      style={{ textAlign: 'left', justifyContent: 'flex-start' }}
+    >
+      <ds-text as="span" size="small" weight={config.weight}>
+        {value}
+      </ds-text>
+    </Row>
+  );
+
+  if (config.hasTooltip) {
+    return (
+      <Tooltip placement="bottom" label={value} key={`${cellKey}-tip`}>
+        {row}
+      </Tooltip>
+    );
+  }
+
+  return row;
+}
+
+type BucketListTable = {
+  volumes: Array<BucketConnectorRow>;
+  selectedRows: SingleSelection;
+  onSelectionChange: (selected: string[]) => void;
+  onDoubleClick: (i: number) => void;
+  onClick: (i: number) => void;
+};
+
+export const BucketListTable = ({
+  volumes,
+  selectedRows,
+  onSelectionChange,
+  onDoubleClick,
+  onClick,
+}: BucketListTable) => {
+  const [t] = useTranslation();
+
+  const tableRows: Array<{
+    id: string;
+    columns: Array<string | React.ReactElement>;
+    clickable: boolean;
+  }> = volumes.map((v, i) => ({
+    id: v.uuid,
+    columns: [
+      ...TEXT_COLUMNS.map((config) => renderTextCell(config, v, i, onClick, onDoubleClick)),
+      <Row
+        key={`${v.uuid}-actions`}
+        orientation="vertical"
+        mainAlignment="center"
+        crossAlignment="flex-start"
+      >
+        <button
+          type="button"
+          onClick={(): void => {
+            onClick(i);
+          }}
+          style={ACTION_BUTTON_STYLE}
+        >
+          <ds-icon icon="ArrowForwardOutline" size="18px" color="primary" />
+        </button>
+      </Row>,
+    ],
+    clickable: true,
+  }));
+
+  return (
+    <Container mainAlignment="flex-start" crossAlignment="flex-start">
+      <ListRow>
+        <Container
+          orientation="horizontal"
+          mainAlignment="space-between"
+          crossAlignment="flex-start"
+          width="fill"
+          maxHeight="calc(100vh - 25rem)"
+          minHeight="auto"
+        >
+          <Table
+            headers={headers(t)}
+            rows={tableRows}
+            showCheckbox={false}
+            multiSelect={false}
+            selectedRows={selectedRows}
+            onSelectionChange={onSelectionChange}
+            RowFactory={HoverableRowFactory}
+            HeaderFactory={CustomHeaderFactory}
+          />
+        </Container>
+      </ListRow>
+      {tableRows.length === 0 && (
+        <Container crossAlignment="center" mainAlignment="flex-start" style={{ marginTop: '4rem' }}>
+          <img src={logo} alt="logo" />
+          <Padding all="medium" width="30.875rem">
+            <ds-text
+              as="p"
+              color="gray1"
+              overflow="break-word"
+              weight="regular"
+              size="large"
+              style={{ whiteSpace: 'pre-line', textAlign: 'center' }}
+            >
+              {t(
+                'select_bucket_or_create_bucket',
+                'It seems like you haven\'t setup a bucket type. \n Click on the "CREATE +" button to create a new one.',
+              )}
+            </ds-text>
+          </Padding>
+        </Container>
+      )}
+    </Container>
+  );
+};
--- apps/admin-ui-storage/src/views/bucket/s3-connector-list-panel.tsx
+++ apps/admin-ui-storage/src/views/bucket/s3-connector-list-panel.tsx
@@ -9,61 +9,24 @@ import {
   Banner,
   Button,
   Container,
-  CustomHeaderFactory,
-  HoverableRowFactory,
   Input,
-  ListRow,
   ModalOverlay,
   Padding,
   Row,
-  Table,
-  Tooltip,
   useSnackbar,
 } from '@zextras/ui-components';
-import { TFunction } from 'i18next';
-import React, { FC, useState } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { BucketConnectorRow, DeleteS3ConnectorRequest, S3Connector } from '../../../types';
-import logo from '../../assets/ninja_robo.svg';
 import { ZIMBRA_ADMIN_URN } from '../../constants';
 import { deleteS3Connector } from '../../services/bucket-service';
 import { bucketVolumeQueryKeys } from '../../services/bucket-volume-query-keys';
 import { useListS3Connectors } from '../../services/use-list-s3-connectors';
+import { BucketListTable, type SingleSelection } from './bucket-list-table';
 import BucketDeleteModel from './delete-bucket-model';
 import EditBucketDetailPanel from './edit-bucket-details-panel';
 import NewBucket from './new-bucket';
-
-type TableHeader = {
-  id: string;
-  label: string;
-  bold: boolean;
-  width?: string;
-};
-
-type SingleSelection = [] | [string];
-
-type TextColumnConfig = {
-  key: string;
-  getValue: (volume: BucketConnectorRow) => string;
-  weight: 'light' | 'regular';
-  hasTooltip?: boolean;
-};
-
-const TEXT_COLUMNS: Array<TextColumnConfig> = [
-  { key: 'id', getValue: (v) => v.uuid, weight: 'light', hasTooltip: true },
-  { key: 'label', getValue: (v) => v.label, weight: 'regular' },
-  { key: 'bucket', getValue: (v) => v.bucketName, weight: 'light' },
-];
-
-const ACTION_BUTTON_STYLE: React.CSSProperties = {
-  background: 'none',
-  border: 'none',
-  cursor: 'pointer',
-  padding: '14px 0px 0px 14px',
-  display: 'inline-flex',
-  alignItems: 'center',
-};
 
 export function resolveSelectedBucketConnector(
   bucketList: Array<BucketConnectorRow>,
@@ -80,155 +43,6 @@ export function resolveSelectedBucketConnector(
 
   return bucketList.find((bucket) => bucket.uuid === selectedValue || bucket.id === selectedValue);
 }
-
-const headers = (t: TFunction): Array<TableHeader> => [
-  {
-    id: 'id',
-    label: t('label.id', 'ID'),
-    width: '35%',
-    bold: true,
-  },
-  {
-    id: 'label',
-    label: t('label.descriptive_name', 'Descriptive Name'),
-    width: '25%',
-    bold: true,
-  },
-  {
-    id: 'bucketName',
-    label: t('label.bucket_name', 'Bucket name'),
-    width: '25%',
-    bold: true,
-  },
-  {
-    id: 'actions',
-    label: t('label.actions', 'Actions'),
-    width: '10%',
-    bold: true,
-  },
-];
-
-function renderTextCell(
-  config: TextColumnConfig,
-  volume: BucketConnectorRow,
-  index: number,
-  onClick: (i: number) => void,
-  onDoubleClick: (i: number) => void,
-): React.ReactElement {
-  const value = config.getValue(volume);
-  const cellKey = `${volume.uuid}-${config.key}`;
-
-  const row = (
-    <Row
-      key={cellKey}
-      onDoubleClick={(): void => {
-        onDoubleClick(index);
-      }}
-      onClick={(): void => {
-        onClick(index);
-      }}
-      style={{ textAlign: 'left', justifyContent: 'flex-start' }}
-    >
-      <ds-text as="span" size="small" weight={config.weight}>
-        {value}
-      </ds-text>
-    </Row>
-  );
-
-  if (config.hasTooltip) {
-    return (
-      <Tooltip placement="bottom" label={value} key={`${cellKey}-tip`}>
-        {row}
-      </Tooltip>
-    );
-  }
-
-  return row;
-}
-
-const BucketListTable: FC<{
-  volumes: Array<BucketConnectorRow>;
-  selectedRows: SingleSelection;
-  onSelectionChange: (selected: string[]) => void;
-  onDoubleClick: (i: number) => void;
-  onClick: (i: number) => void;
-}> = ({ volumes, selectedRows, onSelectionChange, onDoubleClick, onClick }) => {
-  const [t] = useTranslation();
-
-  const tableRows: Array<{
-    id: string;
-    columns: Array<string | React.ReactElement>;
-    clickable: boolean;
-  }> = volumes.map((v, i) => ({
-    id: v.uuid,
-    columns: [
-      ...TEXT_COLUMNS.map((config) => renderTextCell(config, v, i, onClick, onDoubleClick)),
-      <Row
-        key={`${v.uuid}-actions`}
-        orientation="vertical"
-        mainAlignment="center"
-        crossAlignment="flex-start"
-      >
-        <button
-          type="button"
-          onClick={(): void => {
-            onClick(i);
-          }}
-          style={ACTION_BUTTON_STYLE}
-        >
-          <ds-icon icon="ArrowForwardOutline" size="18px" color="primary" />
-        </button>
-      </Row>,
-    ],
-    clickable: true,
-  }));
-
-  return (
-    <Container mainAlignment="flex-start" crossAlignment="flex-start">
-      <ListRow>
-        <Container
-          orientation="horizontal"
-          mainAlignment="space-between"
-          crossAlignment="flex-start"
-          width="fill"
-          maxHeight="calc(100vh - 25rem)"
-          minHeight="auto"
-        >
-          <Table
-            headers={headers(t)}
-            rows={tableRows}
-            showCheckbox={false}
-            multiSelect={false}
-            selectedRows={selectedRows}
-            onSelectionChange={onSelectionChange}
-            RowFactory={HoverableRowFactory}
-            HeaderFactory={CustomHeaderFactory}
-          />
-        </Container>
-      </ListRow>
-      {tableRows.length === 0 && (
-        <Container crossAlignment="center" mainAlignment="flex-start" style={{ marginTop: '4rem' }}>
-          <img src={logo} alt="logo" />
-          <Padding all="medium" width="30.875rem">
-            <ds-text
-              as="p"
-              color="gray1"
-              overflow="break-word"
-              weight="regular"
-              size="large"
-              style={{ whiteSpace: 'pre-line', textAlign: 'center' }}
-            >
-              {t(
-                'select_bucket_or_create_bucket',
-                'It seems like you haven\'t setup a bucket type. \n Click on the "CREATE +" button to create a new one.',
-              )}
-            </ds-text>
-          </Padding>
-        </Container>
-      )}
-    </Container>
-  );
-};
 
 export const S3ConnectorListPanel = () => {
   const [t] = useTranslation();
