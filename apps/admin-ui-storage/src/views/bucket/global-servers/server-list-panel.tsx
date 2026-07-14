904400c436b8fbf24eda9fbbc9d8e52921401be1
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-07-14 11:04:57 +0200

    refactor(admin-ui-storage/server-list-panel): replace ui-components with css modules
    Switch from Container and Row components to native HTML elements
    styled with CSS modules for the server list panel layout.

--- apps/admin-ui-storage/src/views/bucket/global-servers/server-list-panel.tsx
+++ apps/admin-ui-storage/src/views/bucket/global-servers/server-list-panel.tsx
@@ -4,13 +4,14 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { Container, Input, Row, type THeader } from '@zextras/ui-components';
+import { Input, type THeader } from '@zextras/ui-components';
 import { useIsAdvanced, useMailstoreServers } from '@zextras/ui-shared';
 import { type ChangeEvent, type ReactElement, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { useServerVolumeSummary } from '../../../services/use-server-volume-summary';
 import { headerAdvanced } from '../../utility/utils';
+import styles from './server-list-panel.module.css';
 import { ServersListTable } from './server-list-table';
 
 export const ServerListPanel = () => {
@@ -46,60 +47,35 @@ export const ServerListPanel = () => {
   if (isLoading) return <ds-page-shimmer></ds-page-shimmer>;
 
   return (
-    <Container
-      orientation="column"
-      crossAlignment="flex-start"
-      mainAlignment="flex-start"
-      style={{ overflowY: 'auto', position: 'relative' }}
-      background="white"
-    >
-      <Row mainAlignment="flex-start" padding={{ all: 'large' }}>
+    <div className={styles.panel}>
+      <div className={styles.title}>
         <ds-text as="h2" weight="bold">
           {t('buckets.servers_list', 'Servers List')}
         </ds-text>
-      </Row>
+      </div>
       <ds-divider></ds-divider>
-      <Container
-        orientation="column"
-        crossAlignment="flex-start"
-        mainAlignment="flex-start"
-        width="100%"
-        height="calc(100vh - 200px)"
-        padding={{ top: 'extralarge', right: 'large', bottom: 'large', left: 'large' }}
-      >
-        <Row mainAlignment="flex-start" width="100%">
-          <Container height="fit" crossAlignment="flex-start" background="gray6">
-            <Row
-              orientation="horizontal"
-              mainAlignment="space-between"
-              crossAlignment="flex-start"
-              width="fill"
-              padding={{ top: 'small', bottom: 'large' }}
-            >
-              <Container>
-                <Input
-                  disabled={serversList.length === 0 && searchServer.length === 0}
-                  label={t('label.search_for_a_Server', `Search for a Server`)}
-                  backgroundColor="gray5"
-                  CustomIcon={(): ReactElement => (
-                    <ds-icon icon="FunnelOutline" size="large" color="primary"></ds-icon>
-                  )}
-                  value={searchServer}
-                  onChange={(e: ChangeEvent<HTMLInputElement>): void => {
-                    setSearchServer(e.target.value);
-                  }}
-                />
-              </Container>
-            </Row>
-          </Container>
-        </Row>
-        <Row width="100%">
+      <div className={styles.content}>
+        <div className={styles.searchBox}>
+          <Input
+            disabled={serversList.length === 0 && searchServer.length === 0}
+            label={t('label.search_for_a_Server', `Search for a Server`)}
+            backgroundColor="gray5"
+            CustomIcon={(): ReactElement => (
+              <ds-icon icon="FunnelOutline" size="large" color="primary"></ds-icon>
+            )}
+            value={searchServer}
+            onChange={(e: ChangeEvent<HTMLInputElement>): void => {
+              setSearchServer(e.target.value);
+            }}
+          />
+        </div>
+        <div className={styles.tableSection}>
           <ServersListTable
             volumes={serversList}
             headers={isAdvanced ? serverHeaderAdvanced : headerCE}
           />
-        </Row>
-      </Container>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 };
