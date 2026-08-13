b3ff414bad77d6ddbe723e76af068b52b3f07793
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-08-13 10:53:08 +0200

    refactor(admin-ui-backup/backup-config-header): replace ui-components with css

--- apps/admin-ui-backup/src/views/backup/components/backup/backup-config-header.tsx
+++ apps/admin-ui-backup/src/views/backup/components/backup/backup-config-header.tsx
@@ -3,53 +3,43 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import { Button, Container, Padding, Row } from '@zextras/ui-components';
+import { Button } from '@zextras/ui-components';
 import { useTranslation } from 'react-i18next';
 
+import styles from './backup-config-header.module.css';
+
 type BackupConfigHeaderProps = {
-  title: string;
-  isDirty: boolean;
-  onCancel: () => void;
-  onSave: () => void;
+	title: string;
+	isDirty: boolean;
+	onCancel: () => void;
+	onSave: () => void;
 };
 
 export const BackupConfigHeader = ({
-  title,
-  isDirty,
-  onCancel,
-  onSave,
+	title,
+	isDirty,
+	onCancel,
+	onSave,
 }: BackupConfigHeaderProps) => {
-  const [t] = useTranslation();
-  return (
-    <Row mainAlignment="flex-start" width="100%">
-      <Container orientation="vertical" mainAlignment="space-around" height="3.5rem">
-        <Row orientation="horizontal" width="100%">
-          <Row
-            padding={{ all: 'large' }}
-            mainAlignment="flex-start"
-            width="50%"
-            crossAlignment="center"
-          >
-            <ds-text as="h2" size="medium" weight="bold" color="gray0">
-              {title}
-            </ds-text>
-          </Row>
-          <Row
-            padding={{ all: 'large' }}
-            width="50%"
-            mainAlignment="flex-end"
-            crossAlignment="center"
-          >
-            <Padding right="small">
-              {isDirty && (
-                <Button label={t('label.cancel', 'Cancel')} color="secondary" onClick={onCancel} />
-              )}
-            </Padding>
-            {isDirty && <Button label={t('label.save', 'Save')} color="primary" onClick={onSave} />}
-          </Row>
-        </Row>
-      </Container>
-      <ds-divider></ds-divider>
-    </Row>
-  );
+	const [t] = useTranslation();
+	return (
+		<>
+			<div className={styles.header}>
+				<ds-text as="h2" size="medium" weight="bold" color="gray0">
+					{title}
+				</ds-text>
+				{isDirty && (
+					<div className={styles.actions}>
+						<Button
+							label={t('label.cancel', 'Cancel')}
+							color="secondary"
+							onClick={onCancel}
+						/>
+						<Button label={t('label.save', 'Save')} color="primary" onClick={onSave} />
+					</div>
+				)}
+			</div>
+			<ds-divider></ds-divider>
+		</>
+	);
 };
