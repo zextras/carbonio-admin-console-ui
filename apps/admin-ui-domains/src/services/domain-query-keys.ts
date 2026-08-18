48191fad2971ed89d5f2b456264ee41e2f9fb831
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-08-18 14:18:19 +0200

    feat(admin-ui-domains/domain-query-keys): add new query keys for security and services

--- apps/admin-ui-domains/src/services/domain-query-keys.ts
+++ apps/admin-ui-domains/src/services/domain-query-keys.ts
@@ -8,4 +8,9 @@ export const domainQueryKeys = {
   all: ['domain'] as const,
   list: () => [...domainQueryKeys.all, 'list'] as const,
   quota: (domainId: string) => [...domainQueryKeys.all, 'quota', domainId] as const,
+  twoFactorPolicies: (domain: string) =>
+    [...domainQueryKeys.all, 'two-factor-policies', domain] as const,
+  addressBookService: () => [...domainQueryKeys.all, 'address-book-service'] as const,
+  antiDosConfig: () => [...domainQueryKeys.all, 'anti-dos-config'] as const,
+  samlConfig: (domain: string) => [...domainQueryKeys.all, 'saml-config', domain] as const,
 } as const;
