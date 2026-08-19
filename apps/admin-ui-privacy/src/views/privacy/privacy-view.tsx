2f0533961a2a40ffcd334243bdd2cb44e9e6f81e
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-08-19 08:07:43 +0200

    refactor: use shared FormPageLayout in PrivacyView

--- apps/admin-ui-privacy/src/views/privacy/privacy-view.tsx
+++ apps/admin-ui-privacy/src/views/privacy/privacy-view.tsx
@@ -5,6 +5,8 @@
  */
 
 import { useForm } from '@tanstack/react-form';
+import { useSelector } from '@tanstack/react-store';
+import { FormPageLayout } from '@zextras/ui-components';
 import { useAllConfig, useCurrentUserRights } from '@zextras/ui-shared';
 import { find } from 'lodash-es';
 import { useTranslation } from 'react-i18next';
@@ -17,18 +19,8 @@ import {
   TRUE,
 } from '../../constants';
 import { useModifyPrivacyConfig } from '../../services/use-modify-privacy-config';
-import { FormButtons } from './parts/form-buttons';
 import { FormSwitch } from './parts/form-switch';
 import { SwitchDescription } from './parts/switch-description';
-
-const styles = {
-  root: 'flex flex-col justify-start items-center bg-gray6-regular h-full w-full',
-  header: 'flex flex-row justify-start items-center w-full h-[58px] p-lg box-border',
-  formContainer:
-    'flex flex-col items-center items-start justify-start w-full h-[calc(100vh-200px)] pt-xl overflow-auto bg-gray6-regular px-sm box-border',
-};
-
-const titleStyle = { flexBasis: '30%' };
 
 export function PrivacyView() {
   const [t] = useTranslation();
@@ -79,60 +71,48 @@ export function PrivacyView() {
     'We promise they will be fast, easy and very useful to understand how are we doing.',
   );
 
+  const isDirty = useSelector(form.store, (s) => !s.isDefaultValue);
+
   return (
-    <div className={styles.root}>
-      <form.Subscribe selector={(state) => !state.isDefaultValue}>
-        {(isDirty) => (
-          <>
-            <header className={styles.header}>
-              <ds-text as="h2" size="medium" weight="bold" color="gray0" style={titleStyle}>
-                {t('label.privacy', 'Privacy')}
-              </ds-text>
-              {isDirty && (
-                <div className="flex flex-row w-[70%] justify-end items-end">
-                  <FormButtons onCancel={() => form.reset()} onSave={() => form.handleSubmit()} />
-                </div>
-              )}
-            </header>
-            <ds-divider />
-            <div className={styles.formContainer}>
-              <form.Field name="sendFullError">
-                {(field) => (
-                  <FormSwitch
-                    label={t('privacy.send_full_error_data', 'Send full error data')}
-                    fieldValue={field.state.value}
-                    allowSetPrivacy={allowSetPrivacy}
-                    onClick={() => field.handleChange(!field.state.value)}
-                  />
-                )}
-              </form.Field>
-              <SwitchDescription label={privacyErrorDescription} />
-              <form.Field name="sendAnalytics">
-                {(field) => (
-                  <FormSwitch
-                    label={t('privacy.allow_data_analytics', 'Allow data analytics')}
-                    fieldValue={field.state.value}
-                    allowSetPrivacy={allowSetPrivacy}
-                    onClick={() => field.handleChange(!field.state.value)}
-                  />
-                )}
-              </form.Field>
-              <SwitchDescription label={privacyAnalyticsDescription} />
-              <form.Field name="allowFeedback">
-                {(field) => (
-                  <FormSwitch
-                    label={t('privacy.allow_live_survey_feedbacks', 'Allow live survey feedbacks')}
-                    fieldValue={field.state.value}
-                    allowSetPrivacy={allowSetPrivacy}
-                    onClick={() => field.handleChange(!field.state.value)}
-                  />
-                )}
-              </form.Field>
-              <SwitchDescription label={privacyFeedbackDescription} />
-            </div>
-          </>
+    <FormPageLayout
+      title={t('label.privacy', 'Privacy')}
+      unsavedChanges={isDirty}
+      onCancel={() => form.reset()}
+      onSave={() => form.handleSubmit()}
+    >
+      <form.Field name="sendFullError">
+        {(field) => (
+          <FormSwitch
+            label={t('privacy.send_full_error_data', 'Send full error data')}
+            fieldValue={field.state.value}
+            allowSetPrivacy={allowSetPrivacy}
+            onClick={() => field.handleChange(!field.state.value)}
+          />
         )}
-      </form.Subscribe>
-    </div>
+      </form.Field>
+      <SwitchDescription label={privacyErrorDescription} />
+      <form.Field name="sendAnalytics">
+        {(field) => (
+          <FormSwitch
+            label={t('privacy.allow_data_analytics', 'Allow data analytics')}
+            fieldValue={field.state.value}
+            allowSetPrivacy={allowSetPrivacy}
+            onClick={() => field.handleChange(!field.state.value)}
+          />
+        )}
+      </form.Field>
+      <SwitchDescription label={privacyAnalyticsDescription} />
+      <form.Field name="allowFeedback">
+        {(field) => (
+          <FormSwitch
+            label={t('privacy.allow_live_survey_feedbacks', 'Allow live survey feedbacks')}
+            fieldValue={field.state.value}
+            allowSetPrivacy={allowSetPrivacy}
+            onClick={() => field.handleChange(!field.state.value)}
+          />
+        )}
+      </form.Field>
+      <SwitchDescription label={privacyFeedbackDescription} />
+    </FormPageLayout>
   );
 }
