65e61c3b8310b3745e3e23832e35709dd222b676
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-08-20 21:54:07 +0200

    style(admin-ui-domains/app-view): adjust container overflow and layout styles

--- apps/admin-ui-domains/src/views/app-view.tsx
+++ apps/admin-ui-domains/src/views/app-view.tsx
@@ -18,7 +18,7 @@ export const AppView: FC = () => {
   return (
     <Container>
       <DomainPageHeader />
-      <Container orientation="horizontal" mainAlignment="flex-start" height="calc(100vh - 105px)">
+      <Container orientation="horizontal" mainAlignment="flex-start" style={{ overflow: 'hidden' }}>
         <Container style={{ maxWidth: '265px' }}>
           <Suspense fallback={<ds-spinner />}>
             <DomainListPanel />
