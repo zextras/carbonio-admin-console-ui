ef3d5f57233e6234de3db164a6855eca3b13d693
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-08-21 07:23:08 +0200

    refactor(global-administrators): replace isFetching with isLoading

--- apps/admin-ui-domains/src/views/domain/global/global-administrators/global-administrators.tsx
+++ apps/admin-ui-domains/src/views/domain/global/global-administrators/global-administrators.tsx
@@ -41,7 +41,7 @@ export const GlobalAdministrators = () => {
 	} | null>(null);
 	const [showEditAccountView, setShowEditAccountView] = useState<boolean>(false);
 
-	const { data, isFetching } = useAdminAccountList(offset, limit);
+	const { data, isLoading } = useAdminAccountList(offset, limit);
 	const accounts = data?.accounts ?? [];
 	const totalAccount = data?.total ?? 0;
 
@@ -104,19 +104,19 @@ export const GlobalAdministrators = () => {
 				</div>
 				<div className={styles.tableArea}>
 					<Table
-						rows={isFetching ? [] : accountList}
+						rows={isLoading ? [] : accountList}
 						headers={headers}
 						showCheckbox={false}
 						multiSelect={false}
 						RowFactory={HoverableRowFactory}
 						HeaderFactory={CustomHeaderFactory}
 					/>
-					{isFetching && (
+					{isLoading && (
 						<div className={styles.spinner}>
 							<ds-spinner></ds-spinner>
 						</div>
 					)}
-					{accountList.length === 0 && !isFetching && <AdministratorsEmptyState />}
+					{accountList.length === 0 && !isLoading && <AdministratorsEmptyState />}
 					{accountList.length !== 0 && (
 						<div className={styles.footer}>
 							<div className={styles.paging}>
