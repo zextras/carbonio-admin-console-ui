99fe0dab58402bfa0e81299b5110f7ecdf06fa1f
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2026-08-21 14:28:17 +0200

    chore(eslint): ignore global-two-factor-auth linting

--- eslint.config.js
+++ eslint.config.js
@@ -97,6 +97,7 @@ export default tseslint.config(
       'packages/ui-components/src/components/custom/breadcrumb.tsx',
       'apps/admin-ui-domains/src/views/domain/domain-list-panel.tsx',
       'apps/admin-ui-domains/src/views/domain/global/global-detail-panel.tsx',
+      'apps/admin-ui-domains/src/views/domain/global/global-two-factor-auth/**',
       'apps/admin-ui-domains/src/views/domain/global/global-administrators/**',
       'apps/admin-ui-domains/src/views/domain/global/global-white-label/**',
       'apps/admin-ui-domains/src/views/domain/edit-account/**',
